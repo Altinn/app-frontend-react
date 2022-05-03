443ec2fae087c6cbc6d91329e9b860f4c93f8a69
mjulstein <6736558+mjulstein@users.noreply.github.com>
2022-05-03 16:01:52 +0200

    summary component coverage

--- src/altinn-app-frontend/src/components/summary/SummaryComponent.test.tsx
+++ src/altinn-app-frontend/src/components/summary/SummaryComponent.test.tsx
@@ -1,56 +1,151 @@
 import * as React from 'react';
 import { renderWithProviders } from '../../../testUtils';
-import { screen } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
 
-import { SummaryComponent } from './SummaryComponent';
+import { ISummaryComponent, SummaryComponent } from './SummaryComponent';
+import { IValidations } from 'src/types';
+import {
+  FormLayoutActions,
+  ILayoutState,
+} from 'src/features/form/layout/formLayoutSlice';
+import { ILayoutComponent } from 'src/features/form/layout';
+import { getFormLayoutStateMock } from '../../../__mocks__/mocks';
 
 describe('SummaryComponent', () => {
-  const mockLayout = {
-    layouts: {
-      FormLayout: [
-        ...[
-          'group','Group','FileUpload','FileUploadWithTag','Checkboxes', 'default'
-        ].map(t => ({id: t, type: t, dataModelBindings:{}}))
-      ]
-    },
-    uiConfig: {
-      hiddenFields: []
-    },
-  } as any;
+  const defaultId = 'default';
+  const pageId = 'FormLayout';
+  const layoutMock = (): ILayoutState =>
+    getFormLayoutStateMock({
+      layouts: {
+        [pageId]: [
+          ...[
+            defaultId,
+            'group',
+            'Group',
+            'FileUpload',
+            'FileUploadWithTag',
+            'Checkboxes',
+          ].map(
+            (t) =>
+              ({
+                id: t,
+                type: t,
+                dataModelBindings: {},
+                textResourceBindings: {},
+                children: [],
+                maxCount: 0,
+              } as ILayoutComponent),
+          ),
+        ],
+      },
+    });
   test('should render group', () => {
-    renderHelper({componentRef: 'group'});
+    renderHelper({ componentRef: 'group' });
     expect(screen.getByTestId('summary-group-component')).toBeInTheDocument();
   });
   test('should render Group (spelled differently)', () => {
-    renderHelper({componentRef: 'Group'});
+    renderHelper({ componentRef: 'Group' });
     expect(screen.getByTestId('summary-group-component')).toBeInTheDocument();
   });
   test('should render file upload', () => {
-    renderHelper({componentRef: 'FileUpload'});
-    expect(screen.getByTestId('attachment-summary-component')).toBeInTheDocument();
+    renderHelper({ componentRef: 'FileUpload' });
+    expect(
+      screen.getByTestId('attachment-summary-component'),
+    ).toBeInTheDocument();
   });
   test('should render file upload with tag', () => {
-    renderHelper({componentRef: 'FileUploadWithTag'});
-    expect(screen.getByTestId('attachment-with-tag-summary')).toBeInTheDocument();
+    renderHelper({ componentRef: 'FileUploadWithTag' });
+    expect(
+      screen.getByTestId('attachment-with-tag-summary'),
+    ).toBeInTheDocument();
   });
   test('should render checkboxes', () => {
-    renderHelper({componentRef: 'Checkboxes'});
+    renderHelper({ componentRef: 'Checkboxes' });
     expect(screen.getByTestId('multiple-choice-summary')).toBeInTheDocument();
   });
   test('should render default', () => {
-    renderHelper({componentRef: 'default'});
+    renderHelper({ componentRef: defaultId });
     expect(screen.getByTestId('single-input-summary')).toBeInTheDocument();
   });
-
-  const renderHelper = ({componentRef}:{ componentRef: string }) => {
-    renderWithProviders(
-      <SummaryComponent
-        id={'test-id'}
-        pageRef={'FormLayout'}
-        componentRef={componentRef}
-      />,
+  test('should render with validation message', () => {
+    renderHelper(
+      { componentRef: defaultId },
       {
-        preloadedState: { formLayout: mockLayout },
+        [pageId]: {
+          [defaultId]: {
+            simpleBinding: {
+              errors: ['Error message'],
+              warnings: [],
+            },
+          },
+        },
+      },
+    );
+    expect(screen.getByText('Error message')).toBeInTheDocument();
+  });
+  test('should not render if hidden', () => {
+    const otherLayout = {
+      ...layoutMock(),
+    };
+    otherLayout.uiConfig.hiddenFields = [defaultId];
+    const { container } = renderHelper(
+      { componentRef: defaultId },
+      {},
+      otherLayout,
+    );
+    expect(container.firstChild).toBeNull();
+    expect(container.childElementCount).toBe(0);
+  });
+
+  test('should get title text from resource or default', () => {
+    const otherLayout = {
+      ...layoutMock(),
+    };
+    otherLayout.layouts[pageId][0].textResourceBindings = {
+      title: 'default title',
+    };
+    renderHelper({ componentRef: defaultId }, {}, otherLayout);
+    expect(screen.getByText('default title')).toBeInTheDocument();
+  });
+
+  test('should respond to on change click', () => {
+    const spy = jest.spyOn(FormLayoutActions, 'updateCurrentView');
+    const otherLayout = {
+      ...layoutMock(),
+    };
+    otherLayout.uiConfig.currentView = 'otherPage';
+    const theRender = renderHelper(
+      { componentRef: defaultId },
+      {},
+      otherLayout,
+    );
+    const button =
+      theRender.container.querySelector<HTMLButtonElement>('button');
+    fireEvent.click(button);
+    expect(spy).toHaveBeenCalledWith({
+      newView: pageId,
+      runValidations: null,
+      returnToView: 'otherPage',
+    });
+  });
+
+  const renderHelper = (
+    extendProps: Partial<ISummaryComponent>,
+    validations: IValidations = {},
+    mockLayout = layoutMock(),
+  ) => {
+    return renderWithProviders(
+      <SummaryComponent id={defaultId} pageRef={pageId} {...extendProps} />,
+      {
+        preloadedState: {
+          formLayout: mockLayout,
+          formValidations: {
+            validations,
+            error: null,
+            invalidDataTypes: [],
+            currentSingleFieldValidation: {},
+          },
+        },
       },
     );
   };
