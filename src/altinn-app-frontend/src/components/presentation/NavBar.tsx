e486244bc4ee6d2fe856c0086b8e9f41ea32e151
laklov <lasse.klovstad@ciber.no>
2022-05-04 16:03:39 +0200

    Legg til språk dropdown

--- src/altinn-app-frontend/src/components/presentation/NavBar.tsx
+++ src/altinn-app-frontend/src/components/presentation/NavBar.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { getLanguageFromKey } from 'altinn-shared/utils';
 import { ILanguage } from 'altinn-shared/types';
-import { AltinnCollapsableList } from 'altinn-shared/components';
-import { Grid, Typography } from '@material-ui/core';
+import { AltinnDropdown } from 'altinn-shared/components';
+import { Box } from '@material-ui/core';
 
 export interface INavBarProps {
   language: ILanguage;
@@ -17,17 +17,8 @@ export interface INavBarProps {
 }
 
 const NavBar = (props: INavBarProps) => {
-  const [subUnitsExpanded, setSubUnitsExpanded] =
-    React.useState<boolean>(false);
-  /*const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    props.onAppLanguageChange(event.target.value);
-  };*/
-  function expandSubUnits() {
-    setSubUnitsExpanded(!subUnitsExpanded);
-  }
-
   return (
-    <div className='a-modal-navbar'>
+    <Box mt={5} width={'100%'} display='flex' justifyContent={'space-between'}>
       {props.showBackArrow && (
         <button
           type='button'
@@ -43,71 +34,40 @@ const NavBar = (props: INavBarProps) => {
           </span>
         </button>
       )}
-      {
-        <AltinnCollapsableList
-          transition={true}
-          onClickExpand={expandSubUnits}
-          listHeader={
-            <Grid container={true} direction='row'>
-              <Grid container={true} direction='row'>
-                <div>
-                  <i
-                    className='ai ai-expand-circle'
-                    style={{
-                      WebkitTransition: '-webkit-transform 0.5s',
-                      transition: 'transform 0.5s',
-                      transform: subUnitsExpanded
-                        ? 'rotate(90deg)'
-                        : 'rotate(0deg)',
-                      WebkitTransform: subUnitsExpanded
-                        ? 'rotate(90deg)'
-                        : 'rotate(0deg)',
-                    }}
-                  />
-                </div>
-                <Typography>&nbsp;Språk</Typography>
-              </Grid>
-            </Grid>
-          }
-        >
-          {props.appLanguages.map((languageCode: string, index: number) => (
-            <Grid key={index} container={true} direction='column'>
-              <Grid
-                key={index}
-                container={true}
-                direction='column'
-                tabIndex={subUnitsExpanded ? 0 : undefined}
-              >
-                <Grid container={true} direction='row'>
-                  <Typography>{languageCode}</Typography>
-                </Grid>
-              </Grid>
-            </Grid>
-          ))}
-        </AltinnCollapsableList>
-      }
-      {!props.hideCloseButton && (
-        <button
-          type='button'
-          className='a-modal-close a-js-tabable-popover'
-          aria-label={getLanguageFromKey(
-            'general.close_schema',
-            props.language,
-          )}
-          onClick={props.handleClose}
-        >
-          <span className='ai-stack'>
-            <i
-              className='ai-stack-1x ai ai-exit  a-modal-close-icon'
-              aria-hidden='true'
-            />
-          </span>
-          <span className='hidden-button-text'>
-            {getLanguageFromKey('general.close_schema', props.language)}
-          </span>
-        </button>
-      )}
-    </div>
+
+      <Box display='flex'>
+        <Box mr={1}>
+          <AltinnDropdown
+            options={props.appLanguages.map((l) => ({ label: l, value: l }))}
+            onChange={(ev) => props.onAppLanguageChange(ev.target.value)}
+            value={props.selectedAppLanguage}
+            id='app-language-select'
+          />
+        </Box>
+
+        {!props.hideCloseButton && (
+          <button
+            type='button'
+            className='a-modal-close a-js-tabable-popover'
+            aria-label={getLanguageFromKey(
+              'general.close_schema',
+              props.language,
+            )}
+            onClick={props.handleClose}
+          >
+            <span className='ai-stack'>
+              <i
+                className='ai-stack-1x ai ai-exit  a-modal-close-icon'
+                aria-hidden='true'
+              />
+            </span>
+            <span className='hidden-button-text'>
+              {getLanguageFromKey('general.close_schema', props.language)}
+            </span>
+          </button>
+        )}
+      </Box>
+    </Box>
   );
 };
 
