77baeed3f7fe1502a7d3bf858434edfacd2b062d
Kylstad, Nina <nkylstad@gmail.com>
2022-05-05 11:37:16 +0200

    update selector

--- src/altinn-app-frontend/src/selectors/getAllowAnonymous.ts
+++ src/altinn-app-frontend/src/selectors/getAllowAnonymous.ts
@@ -2,40 +2,32 @@ import type { IRuntimeState } from 'src/types';
 import { createSelector } from 'reselect';
 import { getDataTypeByLayoutSetId, isStatelessApp } from 'src/utils/appMetadata';
 
-const getAllowAnonymousSelector = (state: IRuntimeState) => {
-  const application = state.applicationMetadata?.applicationMetadata;
-
-  // Require application metadata
-  if (
-    !application
-    || !application.dataTypes
-    || application.dataTypes.length === 0){
-    return undefined;
-  }
-
-  if (!isStatelessApp(application)) return false;
-  // Require layout sets for stateless apps
-  if (!state.formLayout.layoutsets || state.formLayout.layoutsets.sets.length === 0) {
-    return undefined;
-  }
-
-  const dataTypeId = getDataTypeByLayoutSetId(application.onEntry.show, state.formLayout.layoutsets);
-  if (!dataTypeId) return undefined;
-
-  const dataType = application.dataTypes.find(d => d.id === dataTypeId);
-  if (dataType
-      && dataType.appLogic
-      && dataType.appLogic.allowAnonymousOnStateless !== undefined) {
-    return dataType.appLogic.allowAnonymousOnStateless;
-  }
-
-  return false;
-}
+const getApplicationMetadata = (state: IRuntimeState) => state.applicationMetadata?.applicationMetadata;
+const getLayoutSets = (state: IRuntimeState) => state.formLayout.layoutsets;
 
 const getAllowAnonymous = () => {
   return createSelector(
-    [getAllowAnonymousSelector],
-    (allowAnonymous: boolean) => allowAnonymous,
+    [getApplicationMetadata, getLayoutSets],
+    (application, layoutsets) => {
+      // Require application metadata - return undefined if not yet loaded
+      if (!application || !application.dataTypes) {
+        return undefined;
+      }
+
+      if (!isStatelessApp(application)) return false;
+      // Require layout sets for stateless apps - return undefined if not yet loaded
+      if (!layoutsets?.sets) {
+        return undefined;
+      }
+
+      const dataTypeId = getDataTypeByLayoutSetId(application.onEntry.show, layoutsets);
+      const dataType = application.dataTypes.find(d => d.id === dataTypeId);
+      if (dataType?.appLogic?.allowAnonymousOnStateless !== undefined) {
+        return dataType.appLogic.allowAnonymousOnStateless;
+      }
+
+      return false;
+    },
   );
 };
 
