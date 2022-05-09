7ef50f154997dfeae0040165837f65e3410389c2
laklov <lasse.klovstad@ciber.no>
2022-05-09 15:41:36 +0200

    Fix from review

--- src/altinn-app-frontend/src/components/presentation/NavBar.tsx
+++ src/altinn-app-frontend/src/components/presentation/NavBar.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { getLanguageFromKey } from 'altinn-shared/utils';
 import { ILanguage, IAppLanguage } from 'altinn-shared/types';
-import { AltinnDropdown } from 'altinn-shared/components';
+import { Select } from 'altinn-shared/components';
 import { Box } from '@material-ui/core';
 
 export interface INavBarProps {
@@ -73,7 +73,7 @@ const NavBar = (props: INavBarProps) => {
                 )?.dropdownLabel
               }
             </label>
-            <AltinnDropdown
+            <Select
               options={props.appLanguages.map((l) => ({
                 value: l.language,
                 label: l.languageDescription,
