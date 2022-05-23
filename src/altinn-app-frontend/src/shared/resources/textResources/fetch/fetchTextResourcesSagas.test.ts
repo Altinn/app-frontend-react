1dc08f957cb216352b1140de11bc2b77daa0a6c4
Lasse Klovstad <lasse.klovstad@gmail.com>
2022-05-23 15:44:31 +0200

    REU-466; Fix after rebase

--- src/altinn-app-frontend/src/shared/resources/textResources/fetch/fetchTextResourcesSagas.test.ts
+++ src/altinn-app-frontend/src/shared/resources/textResources/fetch/fetchTextResourcesSagas.test.ts
@@ -1,14 +1,19 @@
-import { all, call, select, take } from 'redux-saga/effects';
+import { all, call, select, take, takeLatest } from 'redux-saga/effects';
 import { expectSaga } from 'redux-saga-test-plan';
 
-import { watchFetchTextResourcesSaga,
+import {
+  watchFetchTextResourcesSaga,
   fetchTextResources,
-  allowAnonymousSelector } from './fetchTextResourcesSagas';
+  allowAnonymousSelector,
+} from './fetchTextResourcesSagas';
 import { FormLayoutActions } from 'src/features/form/layout/formLayoutSlice';
 import { FETCH_TEXT_RESOURCES } from './fetchTextResourcesActionTypes';
 import { FETCH_APPLICATION_METADATA_FULFILLED } from 'src/shared/resources/applicationMetadata/actions/types';
 import { FETCH_PROFILE_FULFILLED } from '../../profile/fetch/fetchProfileActionTypes';
-import { profileStateSelector } from 'src/selectors/simpleSelectors';
+import {
+  appLanguageState,
+  profileStateSelector,
+} from 'src/selectors/simpleSelectors';
 import { IProfile } from 'altinn-shared/types';
 import { get } from 'src/utils/networking';
 import { textResourcesUrl } from 'src/utils/appUrlHelper';
@@ -21,12 +26,15 @@ describe('fetchTextResourcesSagas', () => {
       all([
         take(FormLayoutActions.fetchLayoutSetsFulfilled),
         take(FETCH_APPLICATION_METADATA_FULFILLED),
-        take(FETCH_TEXT_RESOURCES)
+        take(FETCH_TEXT_RESOURCES),
       ]),
     );
     expect(generator.next().value).toEqual(select(allowAnonymousSelector));
     expect(generator.next().value).toEqual(take(FETCH_PROFILE_FULFILLED));
     expect(generator.next().value).toEqual(call(fetchTextResources));
+    expect(generator.next().value).toEqual(
+      takeLatest(FETCH_TEXT_RESOURCES, fetchTextResources),
+    );
     expect(generator.next().done).toBeTruthy();
   });
   it('should fetch text resources using default language when allowAnonymous is true', () => {
@@ -40,13 +48,18 @@ describe('fetchTextResourcesSagas', () => {
       ],
     };
     expectSaga(fetchTextResources)
-    .provide([
-      [select(allowAnonymousSelector), true],
-      [call(get, textResourcesUrl('nb')), mockTextResource],
-    ])
-    .call(TextResourcesActions.fetchTextResourcesFulfilled, 'nb', mockTextResource.resources)
-    .run();
-  })
+      .provide([
+        [select(appLanguageState), ''],
+        [select(allowAnonymousSelector), true],
+        [call(get, textResourcesUrl('nb')), mockTextResource],
+      ])
+      .call(
+        TextResourcesActions.fetchTextResourcesFulfilled,
+        'nb',
+        mockTextResource.resources,
+      )
+      .run();
+  });
   it('should run fetch text resources using profile language', () => {
     const profileMock: IProfile = {
       userId: 1,
@@ -58,7 +71,7 @@ describe('fetchTextResourcesSagas', () => {
         doNotPromptForParty: false,
         language: 'en',
         preSelectedPartyId: 0,
-      }
+      },
     };
     const mockTextResource = {
       language: 'en',
@@ -70,12 +83,53 @@ describe('fetchTextResourcesSagas', () => {
       ],
     };
     return expectSaga(fetchTextResources)
-    .provide([
-      [select(allowAnonymousSelector), false],
-      [select(profileStateSelector), profileMock],
-      [call(get, textResourcesUrl('en')), mockTextResource]
-    ])
-    .call(TextResourcesActions.fetchTextResourcesFulfilled, 'en', mockTextResource.resources)
-    .run();
-  })
-})
+      .provide([
+        [select(appLanguageState), ''],
+        [select(allowAnonymousSelector), false],
+        [select(profileStateSelector), profileMock],
+        [call(get, textResourcesUrl('en')), mockTextResource],
+      ])
+      .call(
+        TextResourcesActions.fetchTextResourcesFulfilled,
+        'en',
+        mockTextResource.resources,
+      )
+      .run();
+  });
+  it('should run fetch text resources using app language', () => {
+    const profileMock: IProfile = {
+      userId: 1,
+      userName: '',
+      partyId: 1234,
+      party: null,
+      userType: 1,
+      profileSettingPreference: {
+        doNotPromptForParty: false,
+        language: 'nb',
+        preSelectedPartyId: 0,
+      },
+    };
+    const mockTextResource = {
+      language: 'en',
+      resources: [
+        {
+          id: 'id1',
+          value: 'This is a text',
+        },
+      ],
+    };
+    return expectSaga(fetchTextResources)
+      .provide([
+        [select(appLanguageState), 'en'],
+        [select(allowAnonymousSelector), false],
+        [select(profileStateSelector), profileMock],
+        [call(get, textResourcesUrl('en')), mockTextResource],
+      ])
+      .call(
+        TextResourcesActions.fetchTextResourcesFulfilled,
+        'en',
+        mockTextResource.resources,
+      )
+      .run();
+  });
+});
