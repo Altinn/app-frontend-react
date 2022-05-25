d8cfbe63237b516213cc4111bb0a4f4cae1cdae6
Håkon Carson Bogsrud <hakon.carson.bogsrud@digdir.no>
2022-05-25 14:43:57 +0200

    chore: explicitly require path module from node

---
+++ test/cypress/e2e/plugins/index.js
@@ -0,0 +1,23 @@
+const path = require('node:path');
+const fs = require('fs-extra');
+
+function getConfigurationByFile(pathToFile, file) {
+  const pathToJsonDataFile = path.resolve(pathToFile, `${file}.json`);
+  return fs.readJson(pathToJsonDataFile);
+}
+
+module.exports = (on, config) => {
+  var pathToConfig = 'e2e/config';
+  switch (config.env.environment) {
+    case 'local':
+      return getConfigurationByFile(pathToConfig, 'local');
+    case 'at21':
+      return getConfigurationByFile(pathToConfig, 'at21');
+    case 'at22':
+      return getConfigurationByFile(pathToConfig, 'at22');
+    case 'tt02':
+      return getConfigurationByFile(pathToConfig, 'tt02');
+  }
+
+  return config;
+};
