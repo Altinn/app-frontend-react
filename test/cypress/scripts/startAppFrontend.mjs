43631442794caf1126dba3bcf8321912fad87eb3
Ole Martin Handeland <git@olemartin.org>
2022-05-30 16:53:57 +0200

    Removing spaces on the end of the working directory options. This caused an undescriptive ENOENT on my end, indicating that yarn could not be found. Took me way too long stumbling around and suspecting corepack bofore I saw this.

--- test/cypress/scripts/startAppFrontend.mjs
+++ test/cypress/scripts/startAppFrontend.mjs
@@ -3,8 +3,8 @@ import { runCommand, getEnvVariableOrExit } from './utils.mjs';
 const init = async () => {
   const appFrontendPath = getEnvVariableOrExit('APP_FRONTEND_PATH');
 
-  await runCommand('yarn --immutable', { cwd: `${appFrontendPath}/src ` });
-  await runCommand('yarn start', { cwd: `${appFrontendPath}/src/altinn-app-frontend ` });
+  await runCommand('yarn --immutable', { cwd: `${appFrontendPath}/src` });
+  await runCommand('yarn start', { cwd: `${appFrontendPath}/src/altinn-app-frontend` });
 };
 
 init();
