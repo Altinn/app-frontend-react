a465732d4b01f4402cf1a68eedb774097a3dad21
EKEBERG Steffen <steffen.ekeberg@soprasteria.com>
2022-06-07 13:51:06 +0200

    reafactor: context to decide use of fullWidthWrapper

--- src/altinn-app-frontend/src/components/GenericComponent.tsx
+++ src/altinn-app-frontend/src/components/GenericComponent.tsx
@@ -56,43 +56,49 @@ const useStyles = makeStyles((theme) => ({
   },
   xs: {
     'border-bottom': '1px dashed #949494',
-    '& > div:nth-child(2)':{
-      paddingLeft: theme.spacing(3/2) // Half the spacing of <Grid in <Form
+    '& > div:nth-child(2)': {
+      paddingLeft: theme.spacing(3 / 2) // Half the spacing of <Grid in <Form
     }
   },
   sm: {
     [theme.breakpoints.up('sm')]: {
       'border-bottom': '1px dashed #949494',
-      '& > div:nth-child(2)':{
-        paddingLeft: theme.spacing(3/2)
+      '& > div:nth-child(2)': {
+        paddingLeft: theme.spacing(3 / 2)
       }
     },
   },
   md: {
     [theme.breakpoints.up('md')]: {
       'border-bottom': '1px dashed #949494',
-      '& > div:nth-child(2)':{
-        paddingLeft: theme.spacing(3/2)
+      '& > div:nth-child(2)': {
+        paddingLeft: theme.spacing(3 / 2)
       }
     },
   },
   lg: {
     [theme.breakpoints.up('lg')]: {
       'border-bottom': '1px dashed #949494',
-      '& > div:nth-child(2)':{
-        paddingLeft: theme.spacing(3/2)
+      '& > div:nth-child(2)': {
+        paddingLeft: theme.spacing(3 / 2)
       }
     },
   },
   xl: {
     [theme.breakpoints.up('xl')]: {
       'border-bottom': '1px dashed #949494',
-      '& > div:nth-child(2)':{
-        paddingLeft: theme.spacing(3/2)
+      '& > div:nth-child(2)': {
+        paddingLeft: theme.spacing(3 / 2)
       }
     },
   },
 }));
+
+export interface IComponentContext {
+  grid?: IGrid;
+}
+
+export const ComponentContext = React.createContext<IComponentContext>({});
 
 export function GenericComponent(props: IGenericComponentProps) {
   const { id, ...passThroughProps } = props;
@@ -305,64 +311,66 @@ export function GenericComponent(props: IGenericComponentProps) {
   ];
 
   return (
-    <Grid
-      item={true}
-      container={true}
-      xs={props.grid?.xs || 12}
-      sm={props.grid?.sm || false}
-      md={props.grid?.md || false}
-      lg={props.grid?.lg || false}
-      xl={props.grid?.xl || false}
-      key={`grid-${props.id}`}
-      className={classNames(
-        'form-group',
-        'a-form-group',
-        classes.container,
-        gridToHiddenProps(props.grid?.labelGrid, classes),
-      )}
-      alignItems='baseline'
-    >
-      {!noLabelComponents.includes(props.type) && (
-        <Grid
-          item={true}
-          xs={props.grid?.labelGrid?.xs || 12}
-          sm={props.grid?.labelGrid?.sm || false}
-          md={props.grid?.labelGrid?.md || false}
-          lg={props.grid?.labelGrid?.lg || false}
-          xl={props.grid?.labelGrid?.xl || false}
-        >
-          <RenderLabelScoped
-            props={props}
-            passThroughProps={passThroughProps}
-            language={language}
-            texts={texts}
-          />
-          <RenderDescription key={`description-${props.id}`} />
-        </Grid>
-      )}
+    <ComponentContext.Provider value={{ grid: props.grid }}>
       <Grid
-        key={`form-content-${props.id}`}
         item={true}
-        id={`form-content-${props.id}`}
-        xs={props.grid?.innerGrid?.xs || 12}
-        sm={props.grid?.innerGrid?.sm || false}
-        md={props.grid?.innerGrid?.md || false}
-        lg={props.grid?.innerGrid?.lg || false}
-        xl={props.grid?.innerGrid?.xl || false}
+        container={true}
+        xs={props.grid?.xs || 12}
+        sm={props.grid?.sm || false}
+        md={props.grid?.md || false}
+        lg={props.grid?.lg || false}
+        xl={props.grid?.xl || false}
+        key={`grid-${props.id}`}
+        className={classNames(
+          'form-group',
+          'a-form-group',
+          classes.container,
+          gridToHiddenProps(props.grid?.labelGrid, classes),
+        )}
+        alignItems='baseline'
       >
-        <RenderComponent.Tag {...componentProps} />
+        {!noLabelComponents.includes(props.type) && (
+          <Grid
+            item={true}
+            xs={props.grid?.labelGrid?.xs || 12}
+            sm={props.grid?.labelGrid?.sm || false}
+            md={props.grid?.labelGrid?.md || false}
+            lg={props.grid?.labelGrid?.lg || false}
+            xl={props.grid?.labelGrid?.xl || false}
+          >
+            <RenderLabelScoped
+              props={props}
+              passThroughProps={passThroughProps}
+              language={language}
+              texts={texts}
+            />
+            <RenderDescription key={`description-${props.id}`} />
+          </Grid>
+        )}
+        <Grid
+          key={`form-content-${props.id}`}
+          item={true}
+          id={`form-content-${props.id}`}
+          xs={props.grid?.innerGrid?.xs || 12}
+          sm={props.grid?.innerGrid?.sm || false}
+          md={props.grid?.innerGrid?.md || false}
+          lg={props.grid?.innerGrid?.lg || false}
+          xl={props.grid?.innerGrid?.xl || false}
+        >
+          <RenderComponent.Tag {...componentProps} />
 
-        {componentValidationsHandledByGenericComponent(
-          props.dataModelBindings,
-          props.type,
-        ) &&
-          hasValidationMessages &&
-          renderValidationMessagesForComponent(
-            componentValidations?.simpleBinding,
-            props.id,
-          )}
+          {componentValidationsHandledByGenericComponent(
+            props.dataModelBindings,
+            props.type,
+          ) &&
+            hasValidationMessages &&
+            renderValidationMessagesForComponent(
+              componentValidations?.simpleBinding,
+              props.id,
+            )}
+        </Grid>
       </Grid>
-    </Grid>
+    </ComponentContext.Provider>
   );
 }
 
--- src/altinn-app-frontend/src/features/form/components/SoftValidations.tsx
+++ src/altinn-app-frontend/src/features/form/components/SoftValidations.tsx
@@ -2,58 +2,74 @@ import { Panel, PanelVariant } from '@altinn/altinn-design-system';
 import type { ILanguage, ITextResource } from 'altinn-shared/types';
 import React from 'react';
 import { useAppSelector } from 'src/common/hooks';
+import { ComponentContext } from 'src/components/GenericComponent';
 import { getTextFromAppOrDefault } from 'src/utils/textResource';
 import { FullWidthWrapper } from './FullWidthWrapper';
 
 export interface ISoftValidationProps {
-    children: React.ReactNode;
-    variant: SoftValidationVariant;
+  children: React.ReactNode;
+  variant: SoftValidationVariant;
 }
 
 export type SoftValidationVariant = 'warning' | 'info' | 'success';
 
 const getPanelVariant = (messageType: SoftValidationVariant) => {
-    switch (messageType) {
-        case 'warning':
-            return PanelVariant.Warning;
-        case 'info':
-            return PanelVariant.Info;
-        case 'success':
-            return PanelVariant.Success;
-    }
+  switch (messageType) {
+    case 'warning':
+      return PanelVariant.Warning;
+    case 'info':
+      return PanelVariant.Info;
+    case 'success':
+      return PanelVariant.Success;
+  }
 };
 
 interface IGetPanelTitleProps {
-    variant: SoftValidationVariant;
-    textResources: ITextResource[];
-    language: ILanguage;
+  variant: SoftValidationVariant;
+  textResources: ITextResource[];
+  language: ILanguage;
 }
 
 export const getPanelTitle = ({ variant, textResources, language }: IGetPanelTitleProps) => {
-    switch (variant) {
-        case 'warning':
-            return getTextFromAppOrDefault('soft_validation.warning_title', textResources, language, undefined, true);
-        case 'info':
-            return getTextFromAppOrDefault('soft_validation.info_title', textResources, language, undefined, true);
-        case 'success':
-            return getTextFromAppOrDefault('soft_validation.success_title', textResources, language, undefined, true);
-    }
+  switch (variant) {
+    case 'warning':
+      return getTextFromAppOrDefault('soft_validation.warning_title', textResources, language, undefined, true);
+    case 'info':
+      return getTextFromAppOrDefault('soft_validation.info_title', textResources, language, undefined, true);
+    case 'success':
+      return getTextFromAppOrDefault('soft_validation.success_title', textResources, language, undefined, true);
+  }
 };
 
-export function SoftValidations({ children, variant }: ISoftValidationProps) {
-    const language = useAppSelector(state => state.language.language);
-    const textResources = useAppSelector(state => state.textResources.resources);
+const ValidationPanel = ({ variant, children }: ISoftValidationProps) => {
+  const language = useAppSelector(state => state.language.language);
+  const textResources = useAppSelector(state => state.textResources.resources);
 
+  return (
+    <Panel
+      variant={getPanelVariant(variant)}
+      showPointer
+      showIcon
+      title={getPanelTitle({ variant, textResources, language })}
+    >
+      {children}
+    </Panel>
+  )
+}
+
+export function SoftValidations(props: ISoftValidationProps) {
+  const { grid } = React.useContext(ComponentContext);
+  const shouldHaveFullWidth = !grid;
+
+  if (shouldHaveFullWidth) {
     return (
-        <FullWidthWrapper>
-            <Panel
-                variant={getPanelVariant(variant)}
-                showPointer
-                showIcon
-                title={getPanelTitle({ variant, textResources, language })}
-            >
-                {children}
-            </Panel>
-        </FullWidthWrapper>
+      <FullWidthWrapper>
+        <ValidationPanel {...props} />
+      </FullWidthWrapper>
     );
+  } else {
+    return (
+      <ValidationPanel {...props} />
+    )
+  }
 }
