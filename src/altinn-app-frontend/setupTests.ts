3c3e7ae0a3f37e03ab714db979208eca81c7b5be
Håkon Carson Bogsrud <hakon.carson.bogsrud@digdir.no>
2022-06-10 16:19:25 +0200

    chore: remove duplicate matchMedia mock

--- src/altinn-app-frontend/setupTests.ts
+++ src/altinn-app-frontend/setupTests.ts
@@ -27,21 +27,6 @@ altinnWindow.app = 'test';
 // Increase timeout for longer tests
 jest.setTimeout(10000);
 
-// https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: jest.fn().mockImplementation(query => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(), // deprecated
-    removeListener: jest.fn(), // deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
-});
-
 Enzyme.configure({
   adapter: new Adapter(),
 });
