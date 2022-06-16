e7bd5c2422ccfc6cfb0d8dd81e1deee6ded34628
Martin Joakim Ulstein <mjulstein@users.noreply.github.com>
2022-06-16 17:05:34 +0200

    unit-tests

---
+++ src/altinn-app-frontend/src/components/base/PrintButtonComponent.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { PrintButtonComponent } from 'src/components/base/PrintButtonComponent';
+import { renderWithProviders } from '../../../testUtils';
+import { IComponentProps } from 'src/components';
+
+const render = (
+  props: Partial<IComponentProps> = {},
+  preloadedState = undefined,
+) => {
+  renderWithProviders(<PrintButtonComponent {...props} />, { preloadedState });
+};
+
+describe('PrintButton', () => {
+  it('should should display the default text if no textResource binding key is found', async () => {
+    render();
+    expect(screen.getByText('Print')).toBeInTheDocument();
+  });
+  it('should display the resource binding key if the text resource is not defined', async () => {
+    render({ textResourceBindings: { text: 'root.printButton.text' } });
+    expect(screen.getByText('root.printButton.text')).toBeInTheDocument();
+  });
+  it('should display the resource if the resource is defined', async () => {
+    render(
+      { textResourceBindings: { text: 'root.printButton.text' } },
+      {
+        textResources: {
+          resources: [{ id: 'root.printButton.text', value: 'Skriv ut' }],
+        },
+      },
+    );
+    expect(screen.getByText('Skriv ut')).toBeInTheDocument();
+  });
+});
--- src/altinn-app-frontend/src/components/base/PrintButtonComponent.tsx
+++ src/altinn-app-frontend/src/components/base/PrintButtonComponent.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
-import {IComponentProps} from 'src/components';
-import {AltinnButton} from 'altinn-shared/components';
-import {getTextFromAppOrDefault} from "src/utils/textResource";
-import {useAppSelector} from "src/common/hooks";
+import { IComponentProps } from 'src/components';
+import { AltinnButton } from 'altinn-shared/components';
+import { getTextFromAppOrDefault } from 'src/utils/textResource';
+import { useAppSelector } from 'src/common/hooks';
 
-
-export const PrintButtonComponent = ({textResourceBindings, language}: IComponentProps) => {
+export const PrintButtonComponent = ({
+  textResourceBindings,
+  language,
+}: Partial<IComponentProps>) => {
   const textResources = useAppSelector(
     (state) => state.textResources.resources,
   );
   return (
-    <AltinnButton btnText={getTextFromAppOrDefault(
-      textResourceBindings.text || 'Print',
-      textResources,
-      language,
-      null,
-      true,
-    )} secondaryButton onClickFunction={window.print}/>
-  )
-}
+    <AltinnButton
+      btnText={getTextFromAppOrDefault(
+        textResourceBindings?.text || 'Print',
+        textResources,
+        language,
+        null,
+        true,
+      )}
+      secondaryButton
+      onClickFunction={window.print}
+    />
+  );
+};
