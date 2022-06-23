6ddb9c4da9011b7a57d450004938567e2e15a426
Martin Joakim Ulstein <mjulstein@users.noreply.github.com>
2022-06-23 14:41:05 +0200

    rewrite TextAreaComponent tests

--- src/altinn-app-frontend/src/components/base/TextAreaComponent.test.tsx
+++ src/altinn-app-frontend/src/components/base/TextAreaComponent.test.tsx
@@ -1,16 +1,14 @@
 import * as React from 'react';
-
-import { mount } from 'enzyme';
 import { TextAreaComponent } from './TextAreaComponent';
 import { render, screen, fireEvent } from '@testing-library/react';
 import type { IComponentProps } from 'src/components';
 
 describe('TextAreaComponent.tsx', () => {
-    const mockId = 'mock-id';
-    const mockHandleDataChange: (value: any) => void = jest.fn();
-    const mockIsValid = true;
-    const mockReadOnly = false;
-    const mockFormData = {};
+  const mockId = 'mock-id';
+  const mockHandleDataChange: (value: any) => void = jest.fn();
+  const mockIsValid = true;
+  const mockReadOnly = false;
+  const mockFormData = {};
 
   it('should set formdata on change', () => {
     const onDataChanged = jest.fn();
@@ -24,7 +22,7 @@ describe('TextAreaComponent.tsx', () => {
   });
 
   it('should render editable component when readOnly is false', () => {
-    const wrapper = mount(
+    render(
       <TextAreaComponent
         id={mockId}
         formData={mockFormData}
@@ -34,12 +32,16 @@ describe('TextAreaComponent.tsx', () => {
         {...({} as IComponentProps)}
       />,
     );
-    expect(wrapper.find('textarea').hasClass('disabled')).toBe(false);
-    expect(wrapper.find('textarea').prop('readOnly')).toBe(false);
+    (document.getElementById(mockId) as HTMLTextAreaElement).value =
+      'something';
+    expect(screen.getByDisplayValue('something')).not.toHaveClass('disabled');
+    expect(screen.getByDisplayValue('something')).not.toHaveAttribute(
+      'readOnly',
+    );
   });
 
   it('should render un-editable component when readOnly is true', () => {
-    const wrapper = mount(
+    render(
       <TextAreaComponent
         id={mockId}
         formData={mockFormData}
@@ -49,16 +51,21 @@ describe('TextAreaComponent.tsx', () => {
         {...({} as IComponentProps)}
       />,
     );
-    expect(wrapper.find('textarea').hasClass('disabled')).toBe(true);
-    expect(wrapper.find('textarea').prop('readOnly')).toBe(true);
+    (document.getElementById(mockId) as HTMLTextAreaElement).value =
+      'something';
+    expect(screen.getByDisplayValue('something')).toHaveClass('disabled');
+    expect(screen.getByDisplayValue('something')).toHaveAttribute('readOnly');
   });
 
   it('should have aria-describedby if textResourceBindings.description is not present', () => {
     renderTextAreaComponent({
-      textResourceBindings: { description: 'description'}
+      textResourceBindings: { description: 'description' },
     });
     const inputComponent = screen.getByTestId(mockId);
-    expect(inputComponent).toHaveAttribute('aria-describedby', 'description-mock-id');
+    expect(inputComponent).toHaveAttribute(
+      'aria-describedby',
+      'description-mock-id',
+    );
   });
 
   it('should not have aria-describedby if textResourceBindings.description is not present', () => {
