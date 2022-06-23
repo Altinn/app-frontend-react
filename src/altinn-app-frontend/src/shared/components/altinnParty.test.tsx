40d3a11152c3ec25d60db2d486a0cd9faddab2a1
Martin Joakim Ulstein <mjulstein@users.noreply.github.com>
2022-06-23 16:37:58 +0200

    altinnParty

--- src/altinn-app-frontend/src/shared/components/altinnParty.test.tsx
+++ src/altinn-app-frontend/src/shared/components/altinnParty.test.tsx
@@ -1,19 +1,14 @@
-import { mount, ReactWrapper } from 'enzyme';
 import * as React from 'react';
-import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
-
 import type { IParty } from 'altinn-shared/types';
 
 import AltinnParty from './altinnParty';
+import { renderWithProviders } from '../../../testUtils';
+import userEvent from '@testing-library/user-event';
 
 describe('altinnParty', () => {
-  let mountedComponent: ReactWrapper;
   let mockParty: IParty;
   let selectedParty: IParty;
   let onSelectPartyMock: (party: IParty) => void;
-  let createStore: any;
-  let mockStore: any;
 
   beforeEach(() => {
     mockParty = {
@@ -29,89 +24,31 @@ describe('altinnParty', () => {
     };
     selectedParty = null;
     onSelectPartyMock = (party: IParty) => (selectedParty = party);
-    createStore = configureStore();
-    mockStore = createStore({
-      language: {
-        language: [],
-      },
-    });
-    mountedComponent = mount(
-      <Provider store={mockStore}>
-        <AltinnParty
-          party={mockParty}
-          onSelectParty={onSelectPartyMock}
-          showSubUnits={true}
-        />
-      </Provider>,
-    );
   });
-
-  it('should use callback to select party', () => {
-    mountedComponent
-      .findWhere((n: ReactWrapper<any, any>) => {
-        if (n.type() === 'div') {
-          if (n.getDOMNode().id === `party-${mockParty.partyId}`) {
-            return true;
-          }
-          return false;
-        }
-        return false;
-      })
-      .simulate('click');
+  const render = () => {
+    return renderWithProviders(
+      <AltinnParty
+        party={mockParty}
+        onSelectParty={onSelectPartyMock}
+        showSubUnits={true}
+      />,
+    );
+  };
+  it('should use callback to select party', async () => {
+    const { container } = render();
+    const element = container.querySelector(`#party-${mockParty.partyId}`);
+    await userEvent.click(element);
     expect(selectedParty).toEqual(mockParty);
   });
-
   describe('should render with correct icon based on what kind of party it is', () => {
     it("should render with class 'fa fa-private' if party is a person", () => {
-      mockParty = {
-        childParties: [],
-        partyId: 'partyId',
-        partyTypeName: 1,
-        orgNumber: null,
-        ssn: 'ssn',
-        unitType: 'test',
-        name: 'Testing Testing',
-        isDeleted: false,
-        onlyHierarchyElementWithNoAccess: false,
-      };
-      mountedComponent = mount(
-        <Provider store={mockStore}>
-          <AltinnParty
-            party={mockParty}
-            onSelectParty={onSelectPartyMock}
-            showSubUnits={true}
-          />
-        </Provider>,
-      );
-      mountedComponent.containsMatchingElement(
-        <i className={'partyIcon fa fa-private'} />,
-      );
+      const { container } = render();
+      expect(container.querySelector('i.fa.fa-private')).toBeInTheDocument();
     });
-
     it("should render with class 'fa fa-corp' if party is a organisation", () => {
-      mockParty = {
-        childParties: [],
-        partyId: 'partyId',
-        partyTypeName: 1,
-        orgNumber: 1000000,
-        ssn: 'ssn',
-        unitType: 'test',
-        name: 'Testing Testing',
-        isDeleted: false,
-        onlyHierarchyElementWithNoAccess: false,
-      };
-      mountedComponent = mount(
-        <Provider store={mockStore}>
-          <AltinnParty
-            party={mockParty}
-            onSelectParty={onSelectPartyMock}
-            showSubUnits={true}
-          />
-        </Provider>,
-      );
-      mountedComponent.containsMatchingElement(
-        <i className={'partyIcon fa fa-corp'} />,
-      );
+      mockParty.orgNumber = 1000000;
+      const { container } = render();
+      expect(container.querySelector('i.fa.fa-corp')).toBeInTheDocument();
     });
   });
 });
