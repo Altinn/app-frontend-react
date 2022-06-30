8a6b15ed5c8f485d2b66f424157b9bc980e85acc
Håkon Carson Bogsrud <hakon.carson.bogsrud@digdir.no>
2022-06-30 13:20:21 +0200

    refactor: simplify paragraph test and remove snapshot test

--- src/altinn-app-frontend/src/components/base/ParagraphComponent.test.tsx
+++ src/altinn-app-frontend/src/components/base/ParagraphComponent.test.tsx
@@ -1,62 +1,42 @@
 import * as React from 'react';
-import { render, screen } from '@testing-library/react';
-import type { ITextResourceBindings } from 'src/features/form/layout';
+import { render as rtlRender, screen } from '@testing-library/react';
 import type { IComponentProps } from 'src/components';
 
 import { ParagraphComponent } from './ParagraphComponent';
 
 describe('ParagraphComponent', () => {
-  const mockId = 'mock-id';
-  const mockText = 'Here goes a paragraph';
-  const mockGetTextResource = (key: string) => key;
-  const mockLanguage: any = {};
-  const mockTextResourceBindings: ITextResourceBindings = {
-    tile: mockText,
-  };
+  it('should render with supplied text', () => {
+    const textContent = 'paragraph text content';
+    render({ text: textContent });
 
-  it('should match snapshot', () => {
-    const { container } = render(
-      <ParagraphComponent
-        id={mockId}
-        text={mockText}
-        language={mockLanguage}
-        getTextResource={mockGetTextResource}
-        textResourceBindings={mockTextResourceBindings}
-        {...({} as IComponentProps)}
-      />,
-    );
-    expect(container.firstChild).toMatchSnapshot();
-  });
-
-  it('should have correct text', () => {
-    render(
-      <ParagraphComponent
-        id={mockId}
-        text={'some other text'}
-        language={mockLanguage}
-        getTextResource={mockGetTextResource}
-        textResourceBindings={mockTextResourceBindings}
-        {...({} as IComponentProps)}
-      />,
-    );
-    expect(screen.getByText('some other text')).toBeInTheDocument();
+    expect(screen.getByText(textContent)).toBeInTheDocument();
   });
 
   it('should render help text if help text is supplied', () => {
-    render(
-      <ParagraphComponent
-        id={mockId}
-        text={mockText}
-        language={mockLanguage}
-        getTextResource={mockGetTextResource}
-        textResourceBindings={{ help: 'this is the help text' }}
-        {...({} as IComponentProps)}
-      />,
-    );
+    render({
+      textResourceBindings: { help: 'this is the help text' },
+    });
+
     expect(
       screen.getByRole('button', {
         name: /popover\.popover_button_helptext/i,
       }),
     ).toBeInTheDocument();
   });
+
+  it('should not render help text if no help text is supplied', () => {
+    render();
+
+    expect(screen.queryByRole('button')).not.toBeInTheDocument();
+  });
 });
+
+const render = (props: Partial<IComponentProps> = {}) => {
+  const allProps = {
+    text: 'paragraph text content',
+    getTextResource: (key: string) => key,
+    ...props,
+  } as IComponentProps;
+
+  rtlRender(<ParagraphComponent {...allProps} />);
+};
