1b58da8404dfeff629acb54587f8888fb7fa09ea
Håkon Carson Bogsrud <hakon.carson.bogsrud@digdir.no>
2022-07-04 14:21:09 +0200

    chore: remove unused components

--- src/shared/src/components/index.ts
+++ src/shared/src/components/index.ts
@@ -1,10 +1,8 @@
 export { default as AltinnButton } from './AltinnButton';
 export { default as AltinnCheckBox } from './AltinnCheckBox';
 export { default as AltinnCollapsableList } from './AltinnCollapsableList';
-export { default as AltinnColumnLayout } from './AltinnColumnLayout';
 export { default as AltinnIcon } from './AltinnIcon';
 export { default as AltinnInput } from './AltinnInput';
-export { default as AltinnLink } from './AltinnLink';
 export { default as AltinnLogo } from './AltinnLogo';
 export { default as AltinnPopover } from './AltinnPopover';
 export { default as AltinnSpinner } from './AltinnSpinner';
