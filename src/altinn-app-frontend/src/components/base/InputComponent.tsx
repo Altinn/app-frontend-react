29599d03c791e123a62aef73bc5307a946b4dfdd
Magnus Revheim Martinsen <mrmartinsen.96@gmail.com>
2022-07-06 16:23:11 +0200

    implement text field from deisgn system

--- src/altinn-app-frontend/src/components/base/InputComponent.tsx
+++ src/altinn-app-frontend/src/components/base/InputComponent.tsx
@@ -1,79 +1,18 @@
 import * as React from 'react';
-import classNames from 'classnames';
+import { TextField } from '@altinn/altinn-design-system';
+import type { ReadOnlyVariant } from '@altinn/altinn-design-system';
 import type { NumberFormatProps } from 'react-number-format';
-import NumberFormat from 'react-number-format';
-import { Input, makeStyles } from '@material-ui/core';
-
-import '../../styles/shared.css';
 import type { IComponentProps } from '..';
-
-export interface IInputBaseProps {
-  id: string;
-  readOnly: boolean;
-  required: boolean;
-  handleDataChange: (value: any) => void;
-  inputRef?: ((el: HTMLInputElement) => void) | React.Ref<any>;
-}
+import '../../styles/shared.css';
 
 export interface IInputFormatting {
   number?: NumberFormatProps;
   align?: 'right' | 'center' | 'left';
 }
 
-export interface IInputProps extends IComponentProps {
+export interface IInputProps extends Omit<IComponentProps, 'readOnly'> {
   formatting?: IInputFormatting;
-}
-
-export interface IBasicInputProps extends IInputBaseProps {
-  onDataChangeSubmit: () => void;
-  onDataChanged: (e: any) => void;
-  value: string;
-}
-
-export interface IFormattedNumberInputProps extends IInputBaseProps {
-  name: any;
-  onChange: (e: any) => void;
-  formatting?: IInputFormatting;
-}
-
-const useStyles = makeStyles({
-  input: {
-    boxSizing: 'border-box',
-  },
-});
-
-function NumberFormatCustom(props: IFormattedNumberInputProps) {
-  const { inputRef, onChange, formatting, ...rest } = props;
-
-  return (
-    <NumberFormat
-      {...rest}
-      getInputRef={inputRef}
-      data-testid={`${props.id}-formatted-number`}
-      onValueChange={(values) => {
-        onChange({
-          target: {
-            name: props.name,
-            value: values.value,
-          },
-        });
-      }}
-      isNumericString={true}
-      {...formatting.number}
-    />
-  );
-}
-
-export function BasicInputComponent({ inputRef, ...rest }: IBasicInputProps) {
-  return (
-    <>
-      <input
-        data-testid={rest.id}
-        ref={inputRef}
-        {...rest}
-      />
-    </>
-  );
+  readOnly?: boolean | ReadOnlyVariant;
 }
 
 export function InputComponent({
@@ -86,7 +25,6 @@ export function InputComponent({
   handleDataChange,
   textResourceBindings,
 }: IInputProps) {
-  const classes = useStyles();
   const [value, setValue] = React.useState(formData?.simpleBinding ?? '');
 
   React.useEffect(() => {
@@ -102,32 +40,19 @@ export function InputComponent({
   };
 
   return (
-    <Input
+    <TextField
       key={`input_${id}`}
       id={id}
       onBlur={onDataChangeSubmit}
       onChange={onDataChanged}
       readOnly={readOnly}
+      isValid={isValid}
       required={required}
-      fullWidth={true}
-      disableUnderline={true}
       value={value}
       aria-describedby={
         textResourceBindings?.description ? `description-${id}` : undefined
       }
-      inputComponent={
-        formatting?.number ? NumberFormatCustom : BasicInputComponent
-      }
-      inputProps={{
-        formatting,
-        className: classNames('form-control', classes.input, {
-          'validation-error': !isValid,
-          disabled: readOnly,
-        }),
-        style: {
-          textAlign: formatting?.align,
-        },
-      }}
+      formatting={formatting}
     />
   );
 }
