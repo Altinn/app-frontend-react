3f8a8b3958d987b9d4a70c2af2b422562a29b360
Ole Martin Handeland <git@olemartin.org>
2022-07-12 15:05:51 +0200

    Rewriting instantiationSlice.ts to createSagaSlice()

--- src/altinn-app-frontend/src/sagas/index.ts
+++ src/altinn-app-frontend/src/sagas/index.ts
@@ -1,7 +1,6 @@
 import type { SagaIterator } from 'redux-saga';
 import { fork } from 'redux-saga/effects';
 
-import InstantiationSagas from '../features/instantiate/instantiation/sagas';
 import ApplicationMetadataSagas from '../shared/resources/applicationMetadata/sagas';
 import Attachments from '../shared/resources/attachments/attachmentSagas';
 import { sagaMiddleware } from 'src/store';
@@ -10,7 +9,6 @@ import { rootSagas } from 'src/shared/resources/utils/sagaSlice';
 function* root(): SagaIterator {
   yield fork(Attachments);
   yield fork(ApplicationMetadataSagas);
-  yield fork(InstantiationSagas);
 
   for (const sliceSaga of rootSagas) {
     yield fork(sliceSaga);
