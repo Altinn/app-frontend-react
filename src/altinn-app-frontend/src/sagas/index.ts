180e7b7622abd37aa100d940b04d4a1f6acaceea
Nina Kylstad <nkylstad@gmail.com>
2022-07-12 16:02:46 +0200

    Rewriting attachmentSlice.ts to createSagaSlice()

--- src/altinn-app-frontend/src/sagas/index.ts
+++ src/altinn-app-frontend/src/sagas/index.ts
@@ -1,13 +1,10 @@
 import type { SagaIterator } from 'redux-saga';
 import { fork } from 'redux-saga/effects';
 
-import Attachments from '../shared/resources/attachments/attachmentSagas';
 import { sagaMiddleware } from 'src/store';
 import { rootSagas } from 'src/shared/resources/utils/sagaSlice';
 
 function* root(): SagaIterator {
-  yield fork(Attachments);
-
   for (const sliceSaga of rootSagas) {
     yield fork(sliceSaga);
   }
