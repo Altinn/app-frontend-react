b8a9be675a033b9cd3bdc79cab510965b2182f5b
lorang92 <steffen.ekeberg@gmail.com>
2022-07-14 10:23:16 +0200

    chore: disable no hot in githib workflow

--- test/cypress/scripts/startAppFrontend.mjs
+++ test/cypress/scripts/startAppFrontend.mjs
@@ -4,7 +4,7 @@ const init = async () => {
   const appFrontendPath = getEnvVariableOrExit('APP_FRONTEND_PATH');
 
   await runCommand('yarn --immutable', { cwd: `${appFrontendPath}/src` });
-  await runCommand('yarn start --no-hot', { cwd: `${appFrontendPath}/src/altinn-app-frontend` });
+  await runCommand('yarn start', { cwd: `${appFrontendPath}/src/altinn-app-frontend` });
 };
 
 init();
