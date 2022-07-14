8618c787a53806b9806bd9e409baf74795224f2d
Håkon Carson Bogsrud <hakon.carson.bogsrud@digdir.no>
2022-07-14 14:17:36 +0200

    test: fix incorrect merge

--- src/altinn-app-frontend/src/utils/dateHelpers.test.ts
+++ src/altinn-app-frontend/src/utils/dateHelpers.test.ts
@@ -21,7 +21,7 @@ describe('dateHelpers', () => {
   });
 
   describe('getISOString', () => {
-    test.each(['', undefined, null, 'abcdef'])(
+    test.each(['', undefined, null])(
       'should return undefined if input date is %p',
       (date) => {
         expect(getISOString(date)).toBeUndefined();
