5d4b478db4230afe34ebd12385cbcad4df26ca45
Martin Joakim Ulstein <mjulstein@users.noreply.github.com>
2022-07-23 02:16:59 +0200

    fix failing test and assert correct usage of reg-exp function

--- src/altinn-app-frontend/src/utils/appMetadata.ts
+++ src/altinn-app-frontend/src/utils/appMetadata.ts
@@ -95,9 +95,10 @@ export function getCurrentDataTypeForApplication({
 }
 
 export function isStatelessApp(application: IApplication) {
-  if (
-    window.location.href.match(getInstanceIdRegExp({ prefix: '/instance/' }))
-  ) {
+  const url = window.location.href; // This should probably be reconsidered when changing router.
+  const expr = getInstanceIdRegExp({ prefix: '/instance' });
+  const match = url.match(expr);
+  if (match) {
     // app can be setup as stateless but then go over to a statefull app
     return false;
   }
