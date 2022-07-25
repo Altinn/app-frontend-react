3b498945c287af3c4778471396ccff81cd18f0f7
Ole Martin Handeland <github@olemartin.org>
2022-07-25 11:59:09 +0200

    Update src/altinn-app-frontend/src/components/message/ErrorReport.tsx
    Co-authored-by: Håkon <2082481+haakemon@users.noreply.github.com>

--- src/altinn-app-frontend/src/components/message/ErrorReport.tsx
+++ src/altinn-app-frontend/src/components/message/ErrorReport.tsx
@@ -17,10 +17,9 @@ export interface IErrorReportProps {
   components: ILayout;
 }
 
-const ArrowForwardIcon =
-  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" style="position: relative; top: 2px">' +
-  '<path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"></path>' +
-  '</svg>';
+const ArrowForwardIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" style="position: relative; top: 2px">
+  <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"></path>
+</svg>`;
 
 const useStyles = makeStyles((theme) => ({
   errorList: {
