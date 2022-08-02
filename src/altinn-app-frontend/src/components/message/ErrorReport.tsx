4bae799c3d4e33d04c52f89549175240f99e1a0d
Ole Martin Handeland <git@olemartin.org>
2022-08-02 16:02:24 +0200

    Removing CSS-calculation

--- src/altinn-app-frontend/src/components/message/ErrorReport.tsx
+++ src/altinn-app-frontend/src/components/message/ErrorReport.tsx
@@ -25,7 +25,7 @@ const ArrowForwardIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2
 const useStyles = makeStyles((theme) => ({
   errorList: {
     listStylePosition: 'outside',
-    marginLeft: `calc(${iconSize}px + ${theme.spacing(1)}px)`,
+    marginLeft: iconSize + theme.spacing(1),
     listStyleImage: `url("data:image/svg+xml,${encodeURIComponent(
       ArrowForwardIcon,
     )}")`,
