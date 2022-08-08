14c301854173c6abd33466b9f5f5f2f73ca8d990
Ole Martin Handeland <git@olemartin.org>
2022-08-08 11:25:58 +0200

    Removing thrown error used to test exported artifacts from cypress testing via github actions

--- src/altinn-app-frontend/src/components/hooks/useDelayedSavedState.ts
+++ src/altinn-app-frontend/src/components/hooks/useDelayedSavedState.ts
@@ -48,11 +48,6 @@ export function useDelayedSavedState(
   return {
     value: immediateState,
     setValue: (newValue, saveImmediately) => {
-      if (newValue === 'a') {
-        // TODO: Remove this, obviously
-        throw new Error('blergh');
-      }
-
       setImmediateState(newValue);
       if (newValue !== formValue) {
         if (saveImmediately) {
