70c60c3c818342507a3cf389d5b0e9ed56d4796d
Ole Martin Handeland <git@olemartin.org>
2022-08-09 15:20:37 +0200

    Removing unused code in cypress tests

--- test/cypress/e2e/support/index.d.ts
+++ test/cypress/e2e/support/index.d.ts
@@ -8,19 +8,6 @@ declare namespace Cypress {
     getTokenForOrg(orgname: string): Chainable<Element>;
 
     /**
-     * Custom command to upload an attachment to an app instance
-     * @example cy.uploadAttachment(ttd, app, "512345", "fc56bb33-eb24-4583-967d-7cf2c2d5fa53", "attachment", "jwttoken")
-     */
-    uploadAttachment(
-      orgname: string,
-      appname: string,
-      partyId: string,
-      instanceId: string,
-      attachmentId: string,
-      token: string,
-    ): Chainable<Element>;
-
-    /**
      * Custom command to navigate to change name layout in task_2 in app: frontend-test
      * @example cy.navigateToChangeName()
      */
