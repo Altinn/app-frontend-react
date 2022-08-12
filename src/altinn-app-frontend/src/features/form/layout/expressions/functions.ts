4beaa5ffe22749af135c40968df6d84b8418a1e9
Ole Martin Handeland <git@olemartin.org>
2022-08-12 11:30:57 +0200

    Squashing types, as formData only stores strings. When comparing true and 'true', my equals function failed without this.

--- src/altinn-app-frontend/src/features/form/layout/expressions/functions.ts
+++ src/altinn-app-frontend/src/features/form/layout/expressions/functions.ts
@@ -1,8 +1,33 @@
+// TODO: Integrate this behaviour into the tests
+function toString(arg: boolean | string): string {
+  if (typeof arg === 'boolean' || typeof arg === 'number') {
+    return JSON.stringify(arg);
+  }
+  if (arg === null || typeof arg === 'undefined') {
+    return 'null';
+  }
+
+  return arg;
+}
+
+function toNumber(arg: any): number {
+  if (typeof arg === 'string') {
+    if (arg.match(/^\d+$/)) {
+      return parseInt(arg, 10);
+    }
+    if (arg.match(/^[\d.]+$/)) {
+      return parseFloat(arg);
+    }
+  }
+
+  return 0;
+}
+
 export const layoutExpressionFunctions = {
-  equals: (arg1, arg2) => arg1 == arg2,
-  notEquals: (arg1, arg2) => arg1 != arg2,
-  greaterThan: (arg1, arg2) => arg1 > arg2,
-  greaterThanEq: (arg1, arg2) => arg1 >= arg2,
-  lessThan: (arg1, arg2) => arg1 < arg2,
-  lessThanEq: (arg1, arg2) => arg1 <= arg2,
+  equals: (arg1, arg2) => toString(arg1) == toString(arg2),
+  notEquals: (arg1, arg2) => toString(arg1) != toString(arg2),
+  greaterThan: (arg1, arg2) => toNumber(arg1) > toNumber(arg2),
+  greaterThanEq: (arg1, arg2) => toNumber(arg1) >= toNumber(arg2),
+  lessThan: (arg1, arg2) => toNumber(arg1) < toNumber(arg2),
+  lessThanEq: (arg1, arg2) => toNumber(arg1) <= toNumber(arg2),
 };
