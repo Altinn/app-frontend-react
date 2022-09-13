b06802cacd8a0063c795e009e4ab13bb2d5f7dc1
Ole Martin Handeland <git@olemartin.org>
2022-09-13 13:58:27 +0200

    Revert "Outputting external source maps in production build"
    This reverts commit 16999ad8e777b1015ae2988ca665105cbe406e45.

--- src/altinn-app-frontend/webpack.config.production.js
+++ src/altinn-app-frontend/webpack.config.production.js
@@ -5,7 +5,7 @@ const common = require('./webpack.common');
 module.exports = {
   ...common,
   mode: 'production',
-  devtool: 'source-map',
+  devtool: false,
   performance: {
     hints: false,
   },
