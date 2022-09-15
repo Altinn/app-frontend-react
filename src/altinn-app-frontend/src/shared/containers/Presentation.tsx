3413b214659bdbf1c3e0b46852a0da1a2a11f4fb
laklov <lasse.klovstad@gmail.com>
2022-09-15 12:20:03 +0200

    try to fix cypress tests

--- src/altinn-app-frontend/src/shared/containers/Presentation.tsx
+++ src/altinn-app-frontend/src/shared/containers/Presentation.tsx
@@ -36,10 +36,6 @@ const style = {
   marginBottom: '1rem',
 };
 
-// Configure JSS
-// https://v4.mui.com/guides/right-to-left/#right-to-left
-const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
-
 const PresentationComponent = (props: IPresentationProvidedProps) => {
   const dispatch = useAppDispatch();
   const party = useAppSelector((state) => state.party?.selectedParty);
@@ -49,6 +45,11 @@ const PresentationComponent = (props: IPresentationProvidedProps) => {
   const textResources = useAppSelector((state) => state.textResources);
   const isRtl = textResources.rtlLanguageDirection;
   const direction = isRtl ? 'rtl' : 'ltr';
+  // Configure JSS
+  // https://v4.mui.com/guides/right-to-left/#right-to-left
+  const jss = create({
+    plugins: [...jssPreset().plugins, rtl({ enabled: isRtl })],
+  });
   const previousFormPage: string = useAppSelector((state) =>
     getNextView(
       state.formLayout.uiConfig.navigationConfig[
