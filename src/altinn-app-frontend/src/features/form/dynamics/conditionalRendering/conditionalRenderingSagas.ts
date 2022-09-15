975520894d6a907e021bd80363a3a4f14a33899c
Ole Martin Handeland <git@olemartin.org>
2022-09-15 11:01:31 +0200

    Running layout expressions to calculate hidden fields and populate the redux state as well

--- src/altinn-app-frontend/src/features/form/dynamics/conditionalRendering/conditionalRenderingSagas.ts
+++ src/altinn-app-frontend/src/features/form/dynamics/conditionalRendering/conditionalRenderingSagas.ts
@@ -4,20 +4,22 @@ import type { SagaIterator } from 'redux-saga';
 import { FormLayoutActions } from 'src/features/form/layout/formLayoutSlice';
 import { ValidationActions } from 'src/features/form/validation/validationSlice';
 import { runConditionalRenderingRules } from 'src/utils/conditionalRendering';
+import { resolvedLayoutsFromState } from 'src/utils/layout/hierarchy';
 import type { IFormData } from 'src/features/form/data';
 import type { IConditionalRenderingRules } from 'src/features/form/dynamics';
 import type { IRuntimeState, IUiConfig, IValidations } from 'src/types';
+import type { LayoutRootNodeCollection } from 'src/utils/layout/hierarchy';
 
-export const ConditionalRenderingSelector: (store: IRuntimeState) => any = (
-  store: IRuntimeState,
-) => store.formDynamics.conditionalRendering;
-export const FormDataSelector: (store: IRuntimeState) => IFormData = (store) =>
-  store.formData.formData;
-export const UiConfigSelector: (store: IRuntimeState) => IUiConfig = (store) =>
-  store.formLayout.uiConfig;
-export const FormValidationSelector: (store: IRuntimeState) => IValidations = (
-  store,
-) => store.formValidations.validations;
+export const ConditionalRenderingSelector = (store: IRuntimeState) =>
+  store.formDynamics.conditionalRendering;
+export const FormDataSelector = (state: IRuntimeState) =>
+  state.formData.formData;
+export const UiConfigSelector = (state: IRuntimeState) =>
+  state.formLayout.uiConfig;
+export const FormValidationSelector = (state: IRuntimeState) =>
+  state.formValidations.validations;
+export const ResolvedNodesSelector = (state: IRuntimeState) =>
+  resolvedLayoutsFromState(state);
 
 export function* checkIfConditionalRulesShouldRunSaga(): SagaIterator {
   try {
@@ -27,12 +29,18 @@ export function* checkIfConditionalRulesShouldRunSaga(): SagaIterator {
     const formData: IFormData = yield select(FormDataSelector);
     const formValidations: IValidations = yield select(FormValidationSelector);
     const uiConfig: IUiConfig = yield select(UiConfigSelector);
+    const resolvedNodes: LayoutRootNodeCollection<'resolved'> = yield select(
+      ResolvedNodesSelector,
+    );
+
     const present = new Set(uiConfig.hiddenFields);
     const future = runConditionalRenderingRules(
       conditionalRenderingState,
       formData,
       uiConfig.repeatingGroups,
     );
+
+    runLayoutExpressionRules(resolvedNodes, present, future);
 
     if (shouldHiddenFieldsUpdate(present, future)) {
       yield put(
@@ -57,6 +65,20 @@ export function* checkIfConditionalRulesShouldRunSaga(): SagaIterator {
     }
   } catch (err) {
     yield call(console.error, err);
+  }
+}
+
+function runLayoutExpressionRules(
+  layouts: LayoutRootNodeCollection<'resolved'>,
+  present: Set<string>,
+  future: Set<string>,
+) {
+  for (const layout of Object.values(layouts.all())) {
+    for (const node of layout.flat(true)) {
+      if (node.item.hidden === true) {
+        future.add(node.item.id);
+      }
+    }
   }
 }
 
