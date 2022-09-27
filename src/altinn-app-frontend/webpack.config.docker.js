facc7229cd54617da9ba85606ea572e73c6e2bdd
Ivar <ivar.nesje@finanstilsynet.no>
2022-09-27 09:14:11 +0200

    ignore more paths for watch

--- src/altinn-app-frontend/webpack.config.docker.js
+++ src/altinn-app-frontend/webpack.config.docker.js
@@ -7,7 +7,7 @@ module.exports = {
     host: '0.0.0.0', // docker needs to bind to public interface in container
   },
   watchOptions: {
-    ignored: '**/node_modules',
+    ignored: ['**/node_modules', '**/.git', '**/dist'],
     poll: 60000,
   },
 };
