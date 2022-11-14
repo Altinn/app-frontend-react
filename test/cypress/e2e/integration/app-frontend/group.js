1ba1eaa7bcb1099b87cb1ea2f65ca496be2e05a9
Ole Martin Handeland <git@olemartin.org>
2022-11-14 14:40:22 +0100

    Restructuring the cypress code to clean up all the navigateTo*() and completeTaskXForm() functions. Combining all this functionality into common ones, and adding support for short-circuiting a task and skipping straight to the next one without really filling out any form fields. This should make running all the tests quite a bit faster (I hope), and possibly also reduce failures in one early task to spill over into failing other tests that relied on completing an earlier task before getting to their own task. Removing some unused code as well.

--- test/cypress/e2e/integration/app-frontend/group.js
+++ test/cypress/e2e/integration/app-frontend/group.js
@@ -10,7 +10,7 @@ const mui = new Common();
 
 describe('Group', () => {
   const init = () => {
-    cy.navigateToTask3();
+    cy.goto('group');
     cy.contains(mui.button, texts.next).click();
     cy.get(appFrontend.group.showGroupToContinue).should('be.visible');
   };
