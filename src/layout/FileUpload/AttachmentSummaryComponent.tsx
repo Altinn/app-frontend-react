0d8e2d92dd74dcdd788192b629913d99b661ebdf
= <bjosttveit@gmail.com>
2023-01-04 10:03:05 +0100

    add empty for attachments

--- src/layout/FileUpload/AttachmentSummaryComponent.tsx
+++ src/layout/FileUpload/AttachmentSummaryComponent.tsx
@@ -1,23 +1,46 @@
 import * as React from 'react';
 
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, makeStyles, Typography } from '@material-ui/core';
 
 import { useAppSelector } from 'src/common/hooks';
+import { getLanguageFromKey } from 'src/utils/sharedUtils';
 import type { IAttachment } from 'src/shared/resources/attachments';
 
 export interface IAttachmentSummaryComponent {
   componentRef: string;
 }
 
+const useStyles = makeStyles({
+  label: {
+    fontWeight: 500,
+    fontSize: '1.8rem',
+    '& p': {
+      fontWeight: 500,
+      fontSize: '1.8rem',
+    },
+  },
+});
+
 export function AttachmentSummaryComponent({ componentRef }: IAttachmentSummaryComponent) {
+  const classes = useStyles();
   const attachments: IAttachment[] | undefined = useAppSelector((state) => state.attachments.attachments[componentRef]);
+  const language = useAppSelector((state) => state.language.language);
+  const isEmpty = !attachments || attachments.length < 1;
   return (
     <Grid
       item
       xs={12}
       data-testid={'attachment-summary-component'}
     >
-      {attachments &&
+      {isEmpty ? (
+        <Typography
+          variant='body1'
+          className={classes.label}
+          component='p'
+        >
+          {getLanguageFromKey('general.empty_summary', language || {})}
+        </Typography>
+      ) : (
         attachments.map((attachment) => {
           return (
             <Typography
@@ -27,7 +50,8 @@ export function AttachmentSummaryComponent({ componentRef }: IAttachmentSummaryC
               {attachment.name}
             </Typography>
           );
-        })}
+        })
+      )}
     </Grid>
   );
 }
