c0199402755f06d4467cd67ef70180684d2bd829
= <bjosttveit@gmail.com>
2023-02-02 12:46:00 +0100

    Footer icon

--- src/features/footer/components/shared/FooterIcon.tsx
+++ src/features/footer/components/shared/FooterIcon.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
+import { Email, Information, Telephone } from '@navikt/ds-icons';
+
 import type { IFooterIcon } from 'src/features/footer/components/types';
 
 interface FooterIconProps {
   icon: IFooterIcon;
 }
 
-export const FooterIcon = ({ icon }: FooterIconProps) => <span>This is an icon :P</span>;
+type IFooterLinkMap = {
+  [K in IFooterIcon]: React.ComponentType;
+};
+const FooterLinkMap: IFooterLinkMap = {
+  email: Email,
+  information: Information,
+  phone: Telephone,
+};
+
+export const FooterIcon = ({ icon }: FooterIconProps) => {
+  const IconComponent = FooterLinkMap[icon];
+  return <IconComponent aria-hidden={true} />;
+};
