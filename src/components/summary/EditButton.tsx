1e776e90461c176d5d764983f17978fc65af524b
lassopicasso <lars.walderhaug@digdir.no>
2023-02-13 13:16:51 +0100

    Solved tab accessibility

--- src/components/summary/EditButton.tsx
+++ src/components/summary/EditButton.tsx
@@ -1,45 +1,22 @@
 import * as React from 'react';
 
-import { IconButton, makeStyles, Typography } from '@material-ui/core';
+import { Button, ButtonColor, ButtonVariant } from '@digdir/design-system-react';
+import { Edit } from '@navikt/ds-icons';
 
 export interface IEditButtonProps {
   onClick: () => void;
   editText: string | null;
 }
 
-const useStyles = makeStyles({
-  editButton: {
-    color: 'black',
-    padding: 0,
-  },
-  editIcon: {
-    paddingLeft: '6px',
-    fontSize: '1.125rem !important',
-  },
-  change: {
-    fontSize: '1.125rem',
-    cursor: 'pointer',
-  },
-});
-
 export function EditButton(props: IEditButtonProps) {
-  const classes = useStyles();
-
   return (
-    <IconButton
-      onClick={props.onClick}
-      className={classes.editButton}
+    <Button
+      variant={ButtonVariant.Quiet}
+      color={ButtonColor.Secondary}
+      icon={<Edit aria-hidden={true} />}
+      iconPlacement='right'
     >
-      <Typography
-        variant='body1'
-        className={classes.change}
-      >
-        <span>{props.editText}</span>
-        <i
-          aria-hidden='true'
-          className={`fa fa-editing-file ${classes.editIcon}`}
-        />
-      </Typography>
-    </IconButton>
+      {props.editText}
+    </Button>
   );
 }
