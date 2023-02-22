2ceecd09bd344622c7958fac290cee062789483c
= <bjosttveit@gmail.com>
2023-02-22 09:54:03 +0100

    simulate feature state in cypress

--- test/e2e/integration/app-frontend/footer.ts
+++ test/e2e/integration/app-frontend/footer.ts
@@ -1,10 +1,23 @@
 import { AppFrontend } from 'test/e2e/pageobjects/app-frontend';
 import { mockFooter } from 'test/e2e/support/footer';
 
+import type { IBackendFeaturesState } from 'src/shared/resources/applicationMetadata';
+
 const appFrontend = new AppFrontend();
 
 describe('Footer', () => {
+  function simulateFooterFeature(value: boolean | undefined) {
+    cy.intercept('GET', '**/applicationmetadata', (req) => {
+      req.on('response', (res) => {
+        res.body.features = {
+          footer: value,
+        } as IBackendFeaturesState;
+      });
+    });
+  }
+
   it('Renders footer when app has it implemented', () => {
+    simulateFooterFeature(true);
     cy.intercept('GET', '**/api/v1/footer', { statusCode: 200, body: mockFooter });
     cy.gotoAndComplete('message');
     cy.get('footer').should('exist').and('be.visible');
@@ -19,10 +32,18 @@ describe('Footer', () => {
   });
   [204, 404].forEach((statusCode) => {
     it(`Does not render footer when backend returns ${statusCode}`, () => {
+      simulateFooterFeature(true);
       cy.intercept('GET', '**/api/v1/footer', { statusCode, body: null });
       cy.gotoAndComplete('message');
       cy.get(appFrontend.sendinButton).should('exist').and('be.visible'); // Make sure the page loads correctly
       cy.get('footer').should('not.exist');
     });
+  });
+  it('Does not fetch and render footer when featureflag is not set', () => {
+    simulateFooterFeature(undefined);
+    cy.intercept('GET', '**/api/v1/footer', { statusCode: 200, body: mockFooter });
+    cy.gotoAndComplete('message');
+    cy.get(appFrontend.sendinButton).should('exist').and('be.visible'); // Make sure the page loads correctly
+    cy.get('footer').should('not.exist');
   });
 });
