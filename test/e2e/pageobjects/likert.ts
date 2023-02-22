d89a1d2b89727afa8c6f63dc8789cb3a9ffabe37
Magnus Revheim Martinsen <mrmartinsen.96@gmail.com>
2023-02-22 10:22:23 +0100

    get radios by type radio

--- test/e2e/pageobjects/likert.ts
+++ test/e2e/pageobjects/likert.ts
@@ -28,13 +28,13 @@ export class Likert {
 
   selectRadio(question, option) {
     cy.findByRole('row', { name: question }).within(() => {
-      cy.findByRole('label', { name: new RegExp(option) }).click();
+      cy.findByRole('radio', { name: new RegExp(option) }).click({ force: true });
     });
   }
 
   selectRadioInMobile(question, option) {
     cy.findByRole('group', { name: question }).within(() => {
-      cy.findByRole('label', { name: new RegExp(option) }).click();
+      cy.findByRole('radio', { name: new RegExp(option) }).click({ force: true });
     });
   }
 
