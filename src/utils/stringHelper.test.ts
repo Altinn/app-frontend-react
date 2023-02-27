84fbbe8a680736f3bb0f1684aecf4536ec8fa8c8
Magnus Revheim Martinsen <mrmartinsen.96@gmail.com>
2023-02-27 16:21:33 +0100

    add test for HelpTextContainer and helper function

--- src/utils/stringHelper.test.ts
+++ src/utils/stringHelper.test.ts
@@ -1,4 +1,5 @@
-import { capitalizeName } from 'src/utils/stringHelper';
+import { getParsedLanguageFromText } from 'src/language/sharedLanguage';
+import { capitalizeName, getLabelFromChildren } from 'src/utils/stringHelper';
 
 describe('stringHelper', () => {
   it('can capitalize a name', () => {
@@ -11,5 +12,20 @@ describe('stringHelper', () => {
     expect(capitalizeName('robert conner, jr.')).toEqual('Robert Conner, Jr.');
     expect(capitalizeName('léonardo di caprio')).toEqual('Léonardo Di Caprio');
     expect(capitalizeName('" \'')).toEqual('" \'');
+  });
+
+  it('can get the inner child of an element as a string', () => {
+    expect(getLabelFromChildren(getParsedLanguageFromText('<h1>This is my message</h1>'))).toEqual(
+      'This is my message',
+    );
+    expect(getLabelFromChildren(getParsedLanguageFromText('<span>This is my message</span>'))).toEqual(
+      'This is my message',
+    );
+    expect(getLabelFromChildren(getParsedLanguageFromText('<div><span>This is my message</span></div>'))).toEqual(
+      'This is my message',
+    );
+
+    const myReactNode: React.ReactNode = getParsedLanguageFromText('<div><span>This is my message</span></div>');
+    expect(getLabelFromChildren(myReactNode)).toEqual('This is my message');
   });
 });
