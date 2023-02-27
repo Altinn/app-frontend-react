69ba2cff65077f77425f6801185fd9a967e0bde9
Magnus Revheim Martinsen <mrmartinsen.96@gmail.com>
2023-02-27 16:48:07 +0100

    add padding to helptext button

--- src/features/form/components/HelpTextContainer.tsx
+++ src/features/form/components/HelpTextContainer.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import { HelpText } from '@digdir/design-system-react';
 
+import classes from 'src/features/form/components/HelpTextContainer.module.css';
 import { getLanguageFromKey } from 'src/language/sharedLanguage';
 import type { ILanguage } from 'src/types/shared';
-
 export interface IHelpTextContainerProps {
   language: ILanguage;
   helpText: React.ReactNode;
@@ -13,14 +13,16 @@ export interface IHelpTextContainerProps {
 
 export function HelpTextContainer({ language, helpText, title }: IHelpTextContainerProps) {
   return (
-    <HelpText
-      title={
-        title
-          ? `${getLanguageFromKey('helptext.button_title_prefix', language)} ${title}`
-          : getLanguageFromKey('helptext.button_title', language)
-      }
-    >
-      {helpText}
-    </HelpText>
+    <div className={classes.helpTextContainer}>
+      <HelpText
+        title={
+          title
+            ? `${getLanguageFromKey('helptext.button_title_prefix', language)} ${title}`
+            : getLanguageFromKey('helptext.button_title', language)
+        }
+      >
+        {helpText}
+      </HelpText>
+    </div>
   );
 }
