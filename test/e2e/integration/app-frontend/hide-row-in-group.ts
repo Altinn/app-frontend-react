52791009f0d32b74308b83a98ce36cec36b9decd
LAPTOP-S0QU96IH\david <david@framit.no>
2023-03-23 11:34:12 +0100

    unstable test

--- test/e2e/integration/app-frontend/hide-row-in-group.ts
+++ test/e2e/integration/app-frontend/hide-row-in-group.ts
@@ -4,10 +4,12 @@ const appFrontend = new AppFrontend();
 
 it('should be possible to hide rows when "Endre fra" is greater or equals to', () => {
   cy.goto('group');
-  cy.get(appFrontend.group.prefill.liten).click().blur();
+  cy.get(appFrontend.group.prefill.liten).click({ force: true }).blur();
   cy.get(appFrontend.nextButton).click();
-  cy.get(appFrontend.group.showGroupToContinue).find('input').check();
+  cy.get(appFrontend.group.showGroupToContinue).find('input').check({ force: true });
   cy.get(appFrontend.group.edit).should('be.visible');
   cy.get(appFrontend.group.hideRepeatingGroupRow).clear().type('1');
   cy.get(appFrontend.group.edit).should('not.exist');
+  cy.get(appFrontend.group.hideRepeatingGroupRow).clear().type('1000000');
+  cy.get(appFrontend.group.edit).should('exist');
 });
