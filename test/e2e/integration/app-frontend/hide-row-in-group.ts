50deb7684292768a35f127a1b04d8fd02e8fe7da
LAPTOP-S0QU96IH\david <david@framit.no>
2023-03-23 10:47:24 +0100

    small refac of cypress test

--- test/e2e/integration/app-frontend/hide-row-in-group.ts
+++ test/e2e/integration/app-frontend/hide-row-in-group.ts
@@ -6,8 +6,6 @@ it('should be possible to hide rows when "Endre fra" is greater or equals to', (
   cy.goto('group');
   cy.get(appFrontend.group.prefill.liten).click().blur();
   cy.get(appFrontend.nextButton).click();
-  cy.get(appFrontend.group.hideRepeatingGroupRow).should('be.visible');
-  cy.get(appFrontend.group.hideRepeatingGroupRow).clear().type('400');
   cy.get(appFrontend.group.showGroupToContinue).find('input').check();
   cy.get(appFrontend.group.edit).should('be.visible');
   cy.get(appFrontend.group.hideRepeatingGroupRow).clear().type('1');
