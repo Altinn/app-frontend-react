f9c8cdbb56a3a7d0fdd4263156958216d199507a
= <bjosttveit@gmail.com>
2023-03-24 16:08:19 +0100

    rename

--- src/shared/resources/process/getTasks/getTasksSagas.ts
+++ src/shared/resources/process/getTasks/getTasksSagas.ts
@@ -9,8 +9,8 @@ import { getProcessNextUrl } from 'src/utils/urls/appUrlHelper';
 import type { IGetTasksFulfilled } from 'src/shared/resources/process';
 import type { IProcess } from 'src/types/shared';
 
-export function* getTasksSaga({ payload: { taskType: processStep } }: PayloadAction<IGetTasksFulfilled>): SagaIterator {
-  if (processStep === ProcessTaskType.Archived) {
+export function* getTasksSaga({ payload: { taskType } }: PayloadAction<IGetTasksFulfilled>): SagaIterator {
+  if (taskType === ProcessTaskType.Archived) {
     yield put(
       ProcessActions.getTasksFulfilled({
         tasks: [],
