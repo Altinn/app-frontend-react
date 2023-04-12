f2da040584e9d932b41c50ff00d522f3c9cda4c2
Ole Martin Handeland <git@olemartin.org>
2023-04-12 14:51:39 +0200

    Only allowing header/readOnly props for rows, not individual cells

--- src/layout/Grid/types.d.ts
+++ src/layout/Grid/types.d.ts
@@ -3,22 +3,19 @@ import type { ILayoutCompBase, ILayoutComponent } from 'src/layout/layout';
 import type { LayoutNode } from 'src/utils/layout/hierarchy';
 import type { AnyItem } from 'src/utils/layout/hierarchy.types';
 
-export interface GridCellOptions {
-  header?: boolean; // TODO: Support expressions here?
-  readOnly?: boolean; // TODO: Support expressions here?
-}
-
-export interface GridComponentRef extends GridCellOptions {
+export interface GridComponentRef {
   component: string;
 }
 
-export interface GridText extends GridCellOptions {
-  text: string; // TODO: Support expressions here
+export interface GridText {
+  text: string;
 }
 
 export type GridCell<C = GridComponentRef> = C | GridText | null;
 
 export interface GridRow<C = GridComponentRef> extends GridCellOptions {
+  header?: boolean;
+  readOnly?: boolean;
   cells: GridCell<C>[];
 }
 
@@ -26,7 +23,6 @@ export interface ILayoutCompGrid<C = GridComponentRef> extends ILayoutCompBase<'
   rows: GridRow<C>[];
 }
 
-// TODO: Restrict this to only allow component types that work inside a grid
 export type ComponentInGrid = ExprResolved<Exclude<ILayoutComponent, ILayoutCompGrid>>;
 
 export type ILayoutGridHierarchy = ExprResolved<ILayoutCompGrid<LayoutNode<Exclude<AnyItem, { type: 'Grid' }>>>>;
