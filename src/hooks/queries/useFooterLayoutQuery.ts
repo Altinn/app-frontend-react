064f9af0a73a55d82a5abd9a20456e820dea6587
LAPTOP-S0QU96IH\david <david@framit.no>
2023-04-24 11:10:29 +0200

    createed a new type that re-exports AxiosError, then its easier to change HttpClient within the feature if needed

--- src/hooks/queries/useFooterLayoutQuery.ts
+++ src/hooks/queries/useFooterLayoutQuery.ts
@@ -1,10 +1,16 @@
 import { useQuery } from '@tanstack/react-query';
 import type { UseQueryResult } from '@tanstack/react-query';
+import type { AxiosError } from 'axios';
 
 import { useAppQueriesContext } from 'src/contexts/appQueriesContext';
 import { FooterLayoutActions } from 'src/features/footer/data/footerLayoutSlice';
 import { useAppDispatch } from 'src/hooks/useAppDispatch';
 import type { IFooterLayout } from 'src/features/footer/types';
+import type { AppDispatch } from 'src/redux/store';
+
+const handleErrorAsSuccessful = (dispatch: AppDispatch): void => {
+  dispatch(FooterLayoutActions.fetchFulfilled({ footerLayout: null }));
+};
 
 enum ServerStateCacheKey {
   FetchFooterLayout = 'fetchFooterLayout',
@@ -19,7 +25,13 @@ export const useFooterLayoutQuery = (enabled?: boolean): UseQueryResult<IFooterL
       // Update the Redux Store ensures that legacy code has access to the data without using the Tanstack Query Cache
       dispatch(FooterLayoutActions.fetchFulfilled({ footerLayout }));
     },
-    onError: (error: Error) => {
+    onError: (error: AxiosError) => {
+      // 404 is a valid response for this query, so we handle it as a successful response. TODO rewrite the backend to return 204 instead of 404.
+      const errorStatusCodeToBeTreatedAsOK = [404];
+      if (errorStatusCodeToBeTreatedAsOK.includes(error?.response?.status || 0)) {
+        handleErrorAsSuccessful(dispatch);
+        return;
+      }
       // Update the Redux Store ensures that legacy code has access to the data without using the Tanstack Query Cache
       dispatch(FooterLayoutActions.fetchRejected({ error }));
     },
