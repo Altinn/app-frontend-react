02b71514f2cca1011524877833b323bf0008c802
= <bjosttveit@gmail.com>
2023-04-24 16:31:30 +0200

    ignore resizeobserver error in cypress

--- test/e2e/support/index.ts
+++ test/e2e/support/index.ts
@@ -49,3 +49,11 @@ afterEach(function () {
     });
   }
 });
+
+const resizeObserverLoopErrRe = /^[^(ResizeObserver loop limit exceeded)]/;
+Cypress.on('uncaught:exception', (err) => {
+  /* returning false here prevents Cypress from failing the test */
+  if (resizeObserverLoopErrRe.test(err.message)) {
+    return false;
+  }
+});
