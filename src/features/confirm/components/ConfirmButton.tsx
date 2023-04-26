e1035642b8bda096d46263205a796a16864eaaf2
= <bjosttveit@gmail.com>
2023-04-26 11:11:14 +0200

    add action to default confirm

--- src/features/confirm/components/ConfirmButton.tsx
+++ src/features/confirm/components/ConfirmButton.tsx
@@ -15,32 +15,47 @@ import type { ILanguage } from 'src/types/shared';
 
 export const ConfirmButton = (props: Omit<BaseButtonProps, 'onClick'> & { id: string; language: ILanguage }) => {
   const textResources = useAppSelector((state) => state.textResources.resources);
+  const processActionsFeature = useAppSelector(
+    (state) => state.applicationMetadata.applicationMetadata?.features?.processActions,
+  );
+  const { actions } = useAppSelector((state) => state.process);
+  const disabled = processActionsFeature && !actions?.confirm;
+
   const dispatch = useAppDispatch();
   const { instanceId } = window as Window as IAltinnWindow;
   const [busyWithId, setBusyWithId] = useState('');
+
   const handleConfirmClick = () => {
-    setBusyWithId(props.id);
-    httpGet(getValidationUrl(instanceId))
-      .then((data: any) => {
-        const mappedValidations = mapDataElementValidationToRedux(data, {}, textResources);
-        dispatch(
-          ValidationActions.updateValidations({
-            validations: mappedValidations,
-          }),
-        );
-        if (data.length === 0) {
-          dispatch(ProcessActions.complete());
-        }
-      })
-      .finally(() => {
-        setBusyWithId('');
-      });
+    if (!disabled) {
+      setBusyWithId(props.id);
+      httpGet(getValidationUrl(instanceId))
+        .then((data: any) => {
+          const mappedValidations = mapDataElementValidationToRedux(data, {}, textResources);
+          dispatch(
+            ValidationActions.updateValidations({
+              validations: mappedValidations,
+            }),
+          );
+          if (data.length === 0) {
+            if (processActionsFeature) {
+              dispatch(ProcessActions.complete({ action: 'confirm' }));
+            } else {
+              dispatch(ProcessActions.complete());
+            }
+          }
+        })
+        .finally(() => {
+          setBusyWithId('');
+        });
+    }
   };
+
   return (
     <SubmitButton
       {...props}
       busyWithId={busyWithId}
       onClick={handleConfirmClick}
+      disabled={disabled}
     >
       {getTextFromAppOrDefault('confirm.button_text', textResources, props.language)}
     </SubmitButton>
