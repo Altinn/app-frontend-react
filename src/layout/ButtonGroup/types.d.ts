b6e7802a5039676c046497b700b72383c9993b83
Ole Martin Handeland <git@olemartin.org>
2023-04-27 11:23:10 +0200

    Fixing typescript errors. It's a bit cumbersome right now, as these need to be injected manually - but in time we should make a more generic system for this.

--- src/layout/ButtonGroup/types.d.ts
+++ src/layout/ButtonGroup/types.d.ts
@@ -1,4 +1,10 @@
+import type { ExprResolved } from 'src/features/expressions/types';
 import type { ILayoutCompBase } from 'src/layout/layout';
+import type { LayoutNode } from 'src/utils/layout/LayoutNode';
+
+export interface ILayoutCompButtonGroupInHierarchy extends ExprResolved<ILayoutCompBase<'ButtonGroup'>> {
+  childComponents: LayoutNode[];
+}
 
 export interface ILayoutCompButtonGroup extends ILayoutCompBase<'ButtonGroup'> {
   children: string[];
