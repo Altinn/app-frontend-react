c2f0d5285630800fe737ec4a314cb6cacd3593be
= <bjosttveit@gmail.com>
2023-04-27 11:55:25 +0200

    fixed buttongroup child type checking

--- src/layout/ButtonGroup/hierarchy.ts
+++ src/layout/ButtonGroup/hierarchy.ts
@@ -1,22 +1,29 @@
 import { ComponentHierarchyGenerator } from 'src/utils/layout/HierarchyGenerator';
-import type { ComponentTypes } from 'src/layout/layout';
+import type { ButtonGroupChildType } from 'src/layout/ButtonGroup/types';
 import type { ChildFactory, HierarchyContext, UnprocessedItem } from 'src/utils/layout/HierarchyGenerator';
 import type { LayoutNode } from 'src/utils/layout/LayoutNode';
 
-const ButtonComponentTypes: ComponentTypes[] = ['Button', 'NavigationButtons', 'InstantiationButton', 'PrintButton'];
+type IButtonComponentTypes = { [key in ButtonGroupChildType]: true };
+
+const ButtonComponentTypes: IButtonComponentTypes = {
+  Button: true,
+  NavigationButtons: true,
+  InstantiationButton: true,
+  PrintButton: true,
+};
 
 export class ButtonGroupHierarchyGenerator extends ComponentHierarchyGenerator<'ButtonGroup'> {
   private isButton(childId: string, outputWarning = true): boolean {
     const prototype = this.generator.prototype(childId);
 
-    if (outputWarning && prototype && !ButtonComponentTypes.includes(prototype.type)) {
+    if (outputWarning && prototype && !ButtonComponentTypes[prototype.type]) {
       console.warn(
         `ButtonGroup component included a component '${childId}', which ` +
           `is a '${prototype.type}' and cannot be rendered in a button group.`,
       );
     }
 
-    return typeof prototype !== 'undefined' && ButtonComponentTypes.includes(prototype.type);
+    return typeof prototype !== 'undefined' && ButtonComponentTypes[prototype.type];
   }
 
   stage1(item: UnprocessedItem<'ButtonGroup'>): void {
--- src/layout/ButtonGroup/types.d.ts
+++ src/layout/ButtonGroup/types.d.ts
@@ -1,6 +1,11 @@
 import type { ExprResolved } from 'src/features/expressions/types';
-import type { ILayoutCompBase } from 'src/layout/layout';
+import type { ComponentTypes, ILayoutCompBase } from 'src/layout/layout';
 import type { LayoutNode } from 'src/utils/layout/LayoutNode';
+
+export type ButtonGroupChildType = Extract<
+  ComponentTypes,
+  'Button' | 'NavigationButtons' | 'InstantiationButton' | 'PrintButton'
+>;
 
 export interface ILayoutCompButtonGroupInHierarchy extends ExprResolved<ILayoutCompBase<'ButtonGroup'>> {
   childComponents: LayoutNode[];
