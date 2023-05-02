260cebe133daae285ca00244918c79f67bc275bf
lassopicasso <lars.walderhaug@digdir.no>
2023-05-02 16:07:02 +0200

    dynamic formatting of displayData

--- src/layout/Input/index.tsx
+++ src/layout/Input/index.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 import { formatNumericText } from '@altinn/altinn-design-system';
 
 import { useAppSelector } from 'src/hooks/useAppSelector';
+import { useMapToReactNumberConfig } from 'src/hooks/useMapToReactNumberConfig';
 import { InputComponent } from 'src/layout/Input/InputComponent';
 import { FormComponent } from 'src/layout/LayoutComponent';
 import { SummaryItemSimple } from 'src/layout/Summary/SummaryItemSimple';
 import type { PropsFromGenericComponent } from 'src/layout';
-import type { NumberFormatProps } from 'src/layout/layout';
+import type { IInputFormatting, NumberFormatProps } from 'src/layout/layout';
 import type { SummaryRendererProps } from 'src/layout/LayoutComponent';
 import type { LayoutNodeFromType } from 'src/utils/layout/hierarchy.types';
 
@@ -23,7 +24,17 @@ export class Input extends FormComponent<'Input'> {
     }
 
     const text = formData[node.item.dataModelBindings.simpleBinding] || '';
-    const numberFormatting = node.item.formatting?.number as NumberFormatProps | undefined;
+
+    // if(node.item.formatting.currency || node.item.formatting.unit) {
+    //   useMapToReactNumberConfig()
+    // }
+    const itemFormatting = useMapToReactNumberConfig(text, node.item.formatting as IInputFormatting);
+    console.log(itemFormatting);
+    // const numberFormatting = node.item.formatting?.number as NumberFormatProps | undefined;
+    const numberFormatting = itemFormatting?.number as NumberFormatProps | undefined;
+    // const numberIntlFormatting = useMapToReactNumberConfig(text, numberFormatting);
+
+    console.log(numberFormatting);
     if (numberFormatting) {
       return formatNumericText(text, numberFormatting);
     }
