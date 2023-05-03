4abd4820c6b3a50119ec8578ca6eb768a9da3bbf
lassopicasso <lars.walderhaug@digdir.no>
2023-05-03 14:52:33 +0200

    cleanup of dynamic formatting

--- src/layout/Input/index.tsx
+++ src/layout/Input/index.tsx
@@ -8,7 +8,7 @@ import { InputComponent } from 'src/layout/Input/InputComponent';
 import { FormComponent } from 'src/layout/LayoutComponent';
 import { SummaryItemSimple } from 'src/layout/Summary/SummaryItemSimple';
 import type { PropsFromGenericComponent } from 'src/layout';
-import type { IInputFormatting, NumberFormatProps } from 'src/layout/layout';
+import type { IInputFormatting } from 'src/layout/layout';
 import type { SummaryRendererProps } from 'src/layout/LayoutComponent';
 import type { LayoutNodeFromType } from 'src/utils/layout/hierarchy.types';
 
@@ -25,18 +25,10 @@ export class Input extends FormComponent<'Input'> {
 
     const text = formData[node.item.dataModelBindings.simpleBinding] || '';
 
-    // if(node.item.formatting.currency || node.item.formatting.unit) {
-    //   useMapToReactNumberConfig()
-    // }
-    const itemFormatting = useMapToReactNumberConfig(text, node.item.formatting as IInputFormatting);
-    console.log(itemFormatting);
-    // const numberFormatting = node.item.formatting?.number as NumberFormatProps | undefined;
-    const numberFormatting = itemFormatting?.number as NumberFormatProps | undefined;
-    // const numberIntlFormatting = useMapToReactNumberConfig(text, numberFormatting);
+    const numberFormatting = useMapToReactNumberConfig(text, node.item.formatting as IInputFormatting);
 
-    console.log(numberFormatting);
-    if (numberFormatting) {
-      return formatNumericText(text, numberFormatting);
+    if (numberFormatting?.number) {
+      return formatNumericText(text, numberFormatting.number);
     }
 
     return text;
