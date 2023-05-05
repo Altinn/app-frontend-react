eb4580db83794179dee77e76f5ac1d192b844074
= <bjosttveit@gmail.com>
2023-05-05 09:28:36 +0200

    made regular panel consume top/bottom padding

--- src/layout/Panel/PanelGroupContainer.tsx
+++ src/layout/Panel/PanelGroupContainer.tsx
@@ -9,7 +9,7 @@ import { getVariant } from 'src/components/form/Panel';
 import { useAppSelector } from 'src/hooks/useAppSelector';
 import { GenericComponent } from 'src/layout/GenericComponent';
 import { CustomIcon } from 'src/layout/Panel/CustomPanelIcon';
-import classes from 'src/layout/Panel/PanelGroupContainer.module.css';
+import classes from 'src/layout/Panel/Panel.module.css';
 import { selectComponentTexts } from 'src/utils/formComponentUtils';
 import { LayoutPage } from 'src/utils/layout/LayoutPage';
 import type { LayoutNodeFromType } from 'src/utils/layout/hierarchy.types';
@@ -24,6 +24,8 @@ export const PanelGroupContainer = ({ node }: PanelGroupConatinerProps) => {
   const { iconUrl, iconAlt } = node.item.panel || {};
   const fullWidth = node.parent instanceof LayoutPage;
   const isOnBottom = node.parent.children().indexOf(node) === node.parent.children().length - 1;
+  const isOnTop = node.parent.children().indexOf(node) === 0;
+
   return (
     <Grid
       item={true}
@@ -36,6 +38,7 @@ export const PanelGroupContainer = ({ node }: PanelGroupConatinerProps) => {
           <FullWidthWrapper
             className={classes.panelPadding}
             isOnBottom={isOnBottom}
+            isOnTop={isOnTop}
           >
             {child}
           </FullWidthWrapper>
