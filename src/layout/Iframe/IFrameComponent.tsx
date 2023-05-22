ed889860afa624870ce8b9c9e725132dd2ecbe00
LAPTOP-S0QU96IH\david <david@framit.no>
2023-05-22 20:18:51 +0200

    Work in progress. Now possible to render valgkort

---
+++ src/layout/Iframe/IFrameComponent.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import type { PropsFromGenericComponent } from 'src/layout';
+
+export type IFrameComponentProps = PropsFromGenericComponent<'IFrame'>;
+
+export const IFrameComponent = ({ node, getTextResourceAsString }: IFrameComponentProps): JSX.Element => {
+  const { textResourceBindings } = node.item;
+
+  const HTMLString = getTextResourceAsString(textResourceBindings?.title) ?? '';
+  const iFrameTitle = getTextResourceAsString(textResourceBindings?.iFrameTitle) ?? '';
+
+  // Resize the iframe to fit the content thats loaded inside it
+  const adjustIFrameSize = (iframe: React.BaseSyntheticEvent): void => {
+    iframe.target.style.height = `${iframe.target.contentWindow.document.documentElement.scrollHeight}px`;
+  };
+
+  return (
+    <iframe
+      scrolling='no'
+      width='100%'
+      srcDoc={HTMLString}
+      title={iFrameTitle}
+      onLoad={adjustIFrameSize}
+      sandbox='allow-same-origin'
+    />
+  );
+};
