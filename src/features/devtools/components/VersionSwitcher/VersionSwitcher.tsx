6ce0ecc4edd39ee32db3e653bed1fc39ed0d9c84
= <bjosttveit@gmail.com>
2023-05-22 13:10:33 +0200

    add message and localhost option

--- src/features/devtools/components/VersionSwitcher/VersionSwitcher.tsx
+++ src/features/devtools/components/VersionSwitcher/VersionSwitcher.tsx
@@ -15,7 +15,13 @@ export const VersionSwitcher = () => {
   } = useQuery({
     queryKey: ['frontendVersions'],
     queryFn: () => axios.get(frontendVersionsCDN).then((res) => res.data),
-    select: (data: string[]) => data.slice().reverse(),
+    select: (data: string[]) => [
+      { label: 'localhost:8080', value: 'http://localhost:8080' },
+      ...data
+        .slice()
+        .reverse()
+        .map((v) => ({ label: v, value: `${appFrontendCDNPath}/${v}` })),
+    ],
   });
 
   const {
@@ -29,14 +35,8 @@ export const VersionSwitcher = () => {
 
   const onClick = () => {
     const newDoc = html
-      .replace(
-        /src=".*\/altinn-app-frontend.js"/,
-        `src="${appFrontendCDNPath}/${selectedVersion}/altinn-app-frontend.js"`,
-      )
-      .replace(
-        /href=".*\/altinn-app-frontend.css"/,
-        `href="${appFrontendCDNPath}/${selectedVersion}/altinn-app-frontend.css"`,
-      );
+      .replace(/src=".*\/altinn-app-frontend.js"/, `src="${selectedVersion}/altinn-app-frontend.js"`)
+      .replace(/href=".*\/altinn-app-frontend.css"/, `href="${selectedVersion}/altinn-app-frontend.css"`);
     document.open();
     document.write(newDoc);
     document.close();
@@ -57,10 +57,12 @@ export const VersionSwitcher = () => {
     >
       <Select
         value={selectedVersion}
-        options={versions.map((v) => ({ label: v, value: v }))}
+        options={versions}
         onChange={(value) => setSelectedVersion(value)}
       />
+      {selectedVersion && <span>Last inn siden på nytt for å gå tilbake til opprinnelig versjon.</span>}
       <Button
+        id='version-switcher-button'
         style={{ width: '100%' }}
         disabled={!selectedVersion}
         onClick={onClick}
