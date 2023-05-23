4f526dc9961fcbaa0847abdb326dfda4a3b551de
Ole Martin Handeland <git@olemartin.org>
2023-05-23 14:28:50 +0200

    Refactoring mobile test to separate out all the different process steps into separate functions

--- test/e2e/integration/app-frontend/mobile.ts
+++ test/e2e/integration/app-frontend/mobile.ts
@@ -12,34 +12,53 @@ describe('Mobile', () => {
   });
 
   it('is possible to submit app instance from mobile', () => {
-    cy.goto('changename');
-    cy.get(appFrontend.changeOfName.oldFullName).parents().eq(2).should('have.css', 'max-width', '100%');
-    cy.gotoAndComplete('changename');
-    cy.intercept('**/api/layoutsettings/group').as('getLayoutGroup');
-    cy.get(appFrontend.sendinButton).should('be.visible');
-    cy.sendIn();
-    cy.wait('@getLayoutGroup');
-    cy.get(appFrontend.nextButton).click();
-    cy.get(appFrontend.group.showGroupToContinue).find('input').dsCheck();
-    cy.addItemToGroup(1, 2, 'automation');
-    cy.get(appFrontend.nextButton).click();
-    cy.get(appFrontend.group.sendersName).type('automation');
-    cy.get(appFrontend.navMenu).should('not.exist');
-    cy.get(appFrontend.group.navigationBarButton).should('have.attr', 'aria-expanded', 'false').click();
-    cy.get(appFrontend.group.navigationBarButton).should('have.attr', 'aria-expanded', 'true');
-    cy.get(appFrontend.navMenu).should('be.visible');
-    cy.get(appFrontend.navMenu).find('li > button').last().click();
-    cy.get(appFrontend.navMenu).should('not.exist');
-    cy.sendIn();
-    likertPage.selectRequiredRadiosInMobile();
-    cy.sendIn();
-    cy.get(dataListPage.tableBody).contains('Caroline').parent('div').parent('td').parent('tr').click();
-    cy.get(appFrontend.nextButton).click();
-    cy.sendIn();
-
-    cy.get(appFrontend.confirm.sendIn).click();
-    cy.get(appFrontend.confirm.sendIn).should('not.exist');
-    cy.get(appFrontend.receipt.container).should('be.visible');
-    cy.get(appFrontend.receipt.linkToArchive).should('be.visible');
+    testChangeName();
+    testGroup();
+    testLikert();
+    testList();
+    testConfirm();
   });
 });
+
+function testChangeName() {
+  cy.goto('changename');
+  cy.get(appFrontend.changeOfName.oldFullName).parents().eq(2).should('have.css', 'max-width', '100%');
+  cy.gotoAndComplete('changename');
+  cy.intercept('**/api/layoutsettings/group').as('getLayoutGroup');
+  cy.get(appFrontend.sendinButton).should('be.visible');
+  cy.sendIn();
+}
+
+function testGroup() {
+  cy.wait('@getLayoutGroup');
+  cy.navPage('repeating').click();
+  cy.get(appFrontend.group.showGroupToContinue).find('input').dsCheck();
+  cy.addItemToGroup(1, 2, 'automation');
+  cy.navPage('hide').click();
+  cy.get(appFrontend.group.sendersName).type('automation');
+  cy.get(appFrontend.navMenu).should('not.exist');
+  cy.get(appFrontend.group.navigationBarButton).should('have.attr', 'aria-expanded', 'false').click();
+  cy.get(appFrontend.group.navigationBarButton).should('have.attr', 'aria-expanded', 'true');
+  cy.get(appFrontend.navMenu).should('be.visible');
+  cy.get(appFrontend.navMenu).find('li > button').last().click();
+  cy.get(appFrontend.navMenu).should('not.exist');
+  cy.sendIn();
+}
+
+function testLikert() {
+  likertPage.selectRequiredRadiosInMobile();
+  cy.sendIn();
+}
+
+function testList() {
+  cy.get(dataListPage.tableBody).contains('Caroline').parent('div').parent('td').parent('tr').click();
+  cy.get(appFrontend.nextButton).click();
+  cy.sendIn();
+}
+
+function testConfirm() {
+  cy.get(appFrontend.confirm.sendIn).click();
+  cy.get(appFrontend.confirm.sendIn).should('not.exist');
+  cy.get(appFrontend.receipt.container).should('be.visible');
+  cy.get(appFrontend.receipt.linkToArchive).should('be.visible');
+}
