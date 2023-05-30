27698eaa65bb6f51d2ac76789cbe9af402d368be
= <bjosttveit@gmail.com>
2023-05-30 16:37:18 +0200

    added cypress test

--- test/e2e/integration/app-frontend/reportee-selection.ts
+++ test/e2e/integration/app-frontend/reportee-selection.ts
@@ -34,3 +34,78 @@ describe('Reportee selection', () => {
     cy.get(appFrontend.reporteeSelection.reportee).should('have.length', 1).contains('DDG');
   });
 });
+
+describe('doNotPromptForParty preference', () => {
+  [true, false].forEach((doNotPromptForParty) => {
+    it(`${
+      doNotPromptForParty ? 'Does not prompt' : 'Prompts'
+    } for party when doNotPromptForParty = ${doNotPromptForParty}, on instantiation with multiple possible parties`, () => {
+      // Intercept active instances
+      cy.intercept('**/active', []).as('noActiveInstances');
+
+      // Intercept profile preference
+      cy.intercept('GET', '**/api/v1/profile/user', (req) => {
+        req.on('response', (res) => {
+          res.body.profileSettingPreference.doNotPromptForParty = doNotPromptForParty;
+        });
+      });
+
+      cy.startAppInstance(appFrontend.apps.frontendTest);
+      cy.get(appFrontend.reporteeSelection.appHeader).should('be.visible');
+
+      if (!doNotPromptForParty) {
+        cy.get('[id*="party-"]').should('be.visible');
+        cy.findByRole('heading', { name: 'Appen for test av app frontend' }).should('not.exist');
+        cy.get('[id*="party-"]').eq(0).click();
+      }
+
+      cy.findByRole('heading', { name: 'Appen for test av app frontend' }).should('be.visible');
+      cy.get('[id*="party-"]').should('not.exist');
+
+      // Test that it goes straight in when accessing an existing instance
+      cy.reloadAndWait();
+
+      cy.findByRole('heading', { name: 'Appen for test av app frontend' }).should('be.visible');
+      cy.get('[id*="party-"]').should('not.exist');
+    });
+  });
+
+  [true, false].forEach((doNotPromptForParty) => {
+    it(`Does not prompt for party when doNotPromptForParty = ${doNotPromptForParty}, on instantiation with only one possible party`, () => {
+      // Intercept active instances
+      cy.intercept('**/active', []).as('noActiveInstances');
+
+      // Intercept profile preference
+      cy.intercept('GET', '**/api/v1/profile/user', (req) => {
+        req.on('response', (res) => {
+          res.body.profileSettingPreference.doNotPromptForParty = doNotPromptForParty;
+        });
+      });
+
+      // Intercept allowed parties
+      cy.intercept('GET', `**/api/v1/parties?allowedtoinstantiatefilter=true`, {
+        body: [
+          {
+            partyId: 50085642,
+            partyTypeName: 1,
+            orgNumber: '',
+            ssn: '23033600534',
+            unitType: null,
+            name: 'RISHAUG JULIUS',
+            isDeleted: false,
+            onlyHierarchyElementWithNoAccess: false,
+            person: null,
+            organization: null,
+            childParties: null,
+          },
+        ],
+      });
+
+      cy.startAppInstance(appFrontend.apps.frontendTest);
+      cy.get(appFrontend.reporteeSelection.appHeader).should('be.visible');
+
+      cy.findByRole('heading', { name: 'Appen for test av app frontend' }).should('be.visible');
+      cy.get('[id*="party-"]').should('not.exist');
+    });
+  });
+});
