abb9012fe822d2e3a6c45a03e8ed117de75bd7d8
= <bjosttveit@gmail.com>
2023-06-08 15:26:57 +0200

    use extended attachment validation

--- src/features/attachments/upload/uploadAttachmentSagas.ts
+++ src/features/attachments/upload/uploadAttachmentSagas.ts
@@ -1,18 +1,21 @@
+import { AxiosError } from 'axios';
 import { call, put, select } from 'redux-saga/effects';
 import type { PayloadAction } from '@reduxjs/toolkit';
-import type { AxiosRequestConfig } from 'axios';
+import type { AxiosRequestConfig, AxiosResponse } from 'axios';
 import type { SagaIterator } from 'redux-saga';
 
 import { AttachmentActions } from 'src/features/attachments/attachmentSlice';
 import { FormDataActions } from 'src/features/formData/formDataSlice';
 import { ValidationActions } from 'src/features/validation/validationSlice';
+import { Severity } from 'src/types';
 import { getFileUploadComponentValidations } from 'src/utils/formComponentUtils';
 import { httpPost } from 'src/utils/network/networking';
+import { getTextFromAppOrDefault } from 'src/utils/textResource';
 import { fileUploadUrl } from 'src/utils/urls/appUrlHelper';
 import { customEncodeURI } from 'src/utils/urls/urlHelper';
 import type { IAttachment } from 'src/features/attachments';
 import type { IUploadAttachmentAction } from 'src/features/attachments/upload/uploadAttachmentActions';
-import type { IRuntimeState } from 'src/types';
+import type { IComponentValidations, IRuntimeState, IValidationIssue } from 'src/types';
 import type { ILanguage } from 'src/types/shared';
 
 export function* uploadAttachmentSaga({
@@ -20,6 +23,8 @@ export function* uploadAttachmentSaga({
 }: PayloadAction<IUploadAttachmentAction>): SagaIterator {
   const currentView: string = yield select((s: IRuntimeState) => s.formLayout.uiConfig.currentView);
   const language: ILanguage = yield select((s: IRuntimeState) => s.language.language);
+  const textResources = yield select((s: IRuntimeState) => s.textResources.resources);
+  const backendFeatures = yield select((s: IRuntimeState) => s.applicationMetadata.applicationMetadata?.features);
 
   try {
     // Sets validations to empty.
@@ -33,7 +38,7 @@ export function* uploadAttachmentSaga({
     );
 
     const fileUploadLink = fileUploadUrl(attachmentType);
-    let contentType;
+    let contentType: string;
 
     if (!file.type) {
       contentType = `application/octet-stream`;
@@ -50,7 +55,7 @@ export function* uploadAttachmentSaga({
       },
     };
 
-    const response: any = yield call(httpPost, fileUploadLink, config, file);
+    const response: AxiosResponse = yield call(httpPost, fileUploadLink, config, file);
 
     if (response.status === 201) {
       const attachment: IAttachment = {
@@ -83,7 +88,29 @@ export function* uploadAttachmentSaga({
         );
       }
     } else {
-      const validations = getFileUploadComponentValidations('upload', language);
+      throw new AxiosError('Error uploading file', undefined, undefined, undefined, response);
+    }
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      let validations: IComponentValidations = {};
+
+      if (backendFeatures?.jsonObjectInDataResponse && err.response?.data?.result) {
+        const validationIssues: IValidationIssue[] = err.response.data.result;
+
+        validations = {
+          simpleBinding: {
+            errors: validationIssues
+              .filter((v) => v.severity === Severity.Error)
+              .map((v) => getTextFromAppOrDefault(v.code, textResources, language, undefined, true)),
+            warnings: validationIssues
+              .filter((v) => v.severity === Severity.Warning)
+              .map((v) => getTextFromAppOrDefault(v.code, textResources, language, undefined, true)),
+          },
+        };
+      } else {
+        validations = getFileUploadComponentValidations('upload', language);
+      }
+
       yield put(
         ValidationActions.updateComponentValidations({
           componentId,
@@ -94,26 +121,13 @@ export function* uploadAttachmentSaga({
       yield put(
         AttachmentActions.uploadAttachmentRejected({
           componentId,
-          attachmentId: tmpAttachmentId,
           attachmentType,
+          attachmentId: tmpAttachmentId,
         }),
       );
+    } else {
+      // Ukjent feil?
+      console.error('File upload failed for an unknown reason');
     }
-  } catch (err) {
-    const validations = getFileUploadComponentValidations('upload', language);
-    yield put(
-      ValidationActions.updateComponentValidations({
-        componentId,
-        layoutId: currentView,
-        validations,
-      }),
-    );
-    yield put(
-      AttachmentActions.uploadAttachmentRejected({
-        componentId,
-        attachmentType,
-        attachmentId: tmpAttachmentId,
-      }),
-    );
   }
 }
