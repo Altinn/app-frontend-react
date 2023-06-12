3623bcc587e7ce8fc50a3290c117f980c9bd4923
Ole Martin Handeland <git@olemartin.org>
2023-06-12 15:04:43 +0200

    Fixes after merge from main

--- src/layout/FileUpload/shared/handleRejectedFiles.ts
+++ src/layout/FileUpload/shared/handleRejectedFiles.ts
@@ -10,18 +10,16 @@ interface Args {
   maxFileSizeInMB: number;
 }
 
-export function handleRejectedFiles({ language, rejectedFiles, maxFileSizeInMB }: Args): string[] {
+export function handleRejectedFiles({ langTools, rejectedFiles, maxFileSizeInMB }: Args): string[] {
+  const { langAsString } = langTools;
   return rejectedFiles.length > 0
     ? rejectedFiles.map((fileRejection) => {
         if (fileRejection.file.size > maxFileSizeInMB * bytesInOneMB) {
-          return `${fileRejection.file.name} ${getLanguageFromKey(
-            'form_filler.file_uploader_validation_error_file_size',
-            language,
-          )}`;
+          return `${fileRejection.file.name} ${langAsString('form_filler.file_uploader_validation_error_file_size')}`;
         } else {
-          return `${getLanguageFromKey('form_filler.file_uploader_validation_error_general_1', language)} ${
+          return `${langAsString('form_filler.file_uploader_validation_error_general_1')} ${
             fileRejection.file.name
-          } ${getLanguageFromKey('form_filler.file_uploader_validation_error_general_2', language)}`;
+          } ${langAsString('form_filler.file_uploader_validation_error_general_2')}`;
         }
       })
     : [];
