438086cb405caee5ef58c72f9ce8ce6f150ba04c
= <bjosttveit@gmail.com>
2023-06-16 14:11:08 +0200

    custom empty validation for list

--- src/layout/List/index.tsx
+++ src/layout/List/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
+import { getParsedLanguageFromKey } from 'src/language/sharedLanguage';
 import { FormComponent } from 'src/layout/LayoutComponent';
 import { ListComponent } from 'src/layout/List/ListComponent';
 import { SummaryItemSimple } from 'src/layout/Summary/SummaryItemSimple';
+import { getFieldName } from 'src/utils/formComponentUtils';
+import { buildValidationObject } from 'src/utils/validation/validationHelpers';
 import type { ExprResolved } from 'src/features/expressions/types';
 import type { PropsFromGenericComponent } from 'src/layout';
 import type { SummaryRendererProps } from 'src/layout/LayoutComponent';
 import type { ILayoutCompList } from 'src/layout/List/types';
+import type { IRuntimeState } from 'src/types';
 import type { LayoutNodeFromType } from 'src/utils/layout/hierarchy.types';
 import type { LayoutNode } from 'src/utils/layout/LayoutNode';
+import type { IValidationObject } from 'src/utils/validation/types';
 
 export class List extends FormComponent<'List'> {
   render(props: PropsFromGenericComponent<'List'>): JSX.Element | null {
@@ -38,6 +43,35 @@ export class List extends FormComponent<'List'> {
 
   canRenderInTable(): boolean {
     return false;
+  }
+
+  runEmptyFieldValidations(node: LayoutNodeFromType<'List'>): IValidationObject[] {
+    if (node.isHidden() || !node.item.required) {
+      return [];
+    }
+
+    const state: IRuntimeState = window.reduxStore.getState();
+
+    const language = state.language.language ?? {};
+    const textResources = state.textResources.resources;
+    const formData = node.getFormData();
+    const validationObjects: IValidationObject[] = [];
+
+    const bindingKeys = Object.keys(formData);
+    let listHasErrors = false;
+    for (const bindingKey of bindingKeys) {
+      const data = formData[bindingKey];
+
+      if (!data?.length) {
+        listHasErrors = true;
+      }
+    }
+    if (listHasErrors) {
+      const fieldName = getFieldName(node.item.textResourceBindings, textResources, language, undefined);
+      const message = getParsedLanguageFromKey('form_filler.error_required', language, [fieldName], true);
+      validationObjects.push(buildValidationObject(node, 'errors', message));
+    }
+    return validationObjects;
   }
 }
 
