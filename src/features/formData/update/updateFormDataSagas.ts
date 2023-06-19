bd52de4cc0cbf5578fd0971ea86e60aa46b0582e
= <bjosttveit@gmail.com>
2023-06-19 14:48:48 +0200

    runValidations

--- src/features/formData/update/updateFormDataSagas.ts
+++ src/features/formData/update/updateFormDataSagas.ts
@@ -5,19 +5,14 @@ import type { SagaIterator } from 'redux-saga';
 import { FormDynamicsActions } from 'src/features/dynamics/formDynamicsSlice';
 import { FormDataActions } from 'src/features/formData/formDataSlice';
 import { ValidationActions } from 'src/features/validation/validationSlice';
-import { getCurrentDataTypeForApplication } from 'src/utils/appMetadata';
+import { implementsNodeValidation } from 'src/layout';
 import { removeAttachmentReference } from 'src/utils/databindings';
-import { getLayoutComponentById, getLayoutIdForComponent } from 'src/utils/layout';
-import {
-  getValidator,
-  mergeComponentValidations,
-  validateComponentFormData,
-  validateComponentSpecificValidations,
-} from 'src/utils/validation/validation';
+import { ResolvedNodesSelector } from 'src/utils/layout/hierarchy';
 import type { IAttachments } from 'src/features/attachments';
 import type { IFormData } from 'src/features/formData';
 import type { IDeleteAttachmentReference, IUpdateFormData } from 'src/features/formData/formDataTypes';
 import type { IRuntimeState } from 'src/types';
+import type { LayoutPages } from 'src/utils/layout/LayoutPages';
 
 export function* updateFormDataSaga({
   payload: { field, data, componentId, skipValidation, skipAutoSave, singleFieldValidation },
@@ -49,8 +44,11 @@ export function* updateFormDataSaga({
   }
 }
 
+// TODO: Use provided data?
 function* runValidations(field: string, data: any, componentId: string | undefined, state: IRuntimeState) {
-  if (!componentId) {
+  const resolvedNodes: LayoutPages = yield select(ResolvedNodesSelector);
+  const node = componentId && resolvedNodes.findById(componentId);
+  if (!node) {
     yield put(
       FormDataActions.updateRejected({
         error: new Error('Missing component ID!'),
@@ -58,62 +56,24 @@ function* runValidations(field: string, data: any, componentId: string | undefin
     );
     return;
   }
-  if (!state.language.language) {
-    return;
+
+  if (implementsNodeValidation(node.def)) {
+    const validationResult = node.def.validateComponent(node as any);
+    const invalidDataComponents = state.formValidations.invalidDataTypes || [];
+    const updatedInvalidDataComponents = invalidDataComponents.filter((item) => item !== field);
+    if (validationResult.invalidDataTypes) {
+      updatedInvalidDataComponents.push(field);
+    }
+
+    yield put(
+      ValidationActions.updateComponentValidations({
+        componentId: node.item.id,
+        layoutId: node.pageKey(),
+        validations: validationResult.validations,
+        invalidDataTypes: updatedInvalidDataComponents,
+      }),
+    );
   }
-
-  const layoutId = getLayoutIdForComponent(componentId, state.formLayout.layouts || {});
-
-  if (!layoutId) {
-    console.error('Failed to find layout ID for component', componentId);
-    return;
-  }
-
-  const currentDataTypeId = getCurrentDataTypeForApplication({
-    application: state.applicationMetadata.applicationMetadata,
-    instance: state.instanceData.instance,
-    layoutSets: state.formLayout.layoutsets,
-  });
-  const validator = getValidator(currentDataTypeId, state.formDataModel.schemas);
-  const component = getLayoutComponentById(componentId, state.formLayout.layouts);
-
-  const validationResult = validateComponentFormData(
-    layoutId,
-    data,
-    field,
-    component,
-    state.language.language,
-    state.textResources.resources,
-    validator,
-    state.formValidations.validations[componentId],
-    componentId !== component?.id ? componentId : null,
-  );
-
-  const componentValidations = validationResult?.validations[layoutId][componentId];
-
-  const profileLanguage = state.profile.selectedAppLanguage || state.profile.profile.profileSettingPreference.language;
-  const componentSpecificValidations = validateComponentSpecificValidations(
-    data,
-    component,
-    state.language.language,
-    profileLanguage,
-  );
-  const mergedValidations = mergeComponentValidations(componentValidations ?? {}, componentSpecificValidations);
-
-  const invalidDataComponents = state.formValidations.invalidDataTypes || [];
-  const updatedInvalidDataComponents = invalidDataComponents.filter((item) => item !== field);
-  if (validationResult?.invalidDataTypes) {
-    updatedInvalidDataComponents.push(field);
-  }
-
-  yield put(
-    ValidationActions.updateComponentValidations({
-      componentId,
-      layoutId,
-      validations: mergedValidations ?? {},
-      invalidDataTypes: updatedInvalidDataComponents,
-    }),
-  );
 }
 
 function shouldUpdateFormData(currentData: any, newData: any): boolean {
