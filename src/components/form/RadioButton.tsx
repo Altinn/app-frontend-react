9274245be86c8041ac9813e78dd3920013ca1f92
Mikael Solstad <mikaelrinosolstad@gmail.com>
2023-06-22 12:47:05 +0200

    Add unit tests for RadioButton

--- src/components/form/RadioButton.tsx
+++ src/components/form/RadioButton.tsx
@@ -7,18 +7,22 @@ import classes from 'src/components/form/RadioButton.module.css';
 
 const Card = ({ children }: { children: React.ReactNode }) => <div className={classes.card}>{children}</div>;
 
-export interface IAppRadioButtonProps extends RadioButtonProps {
+export interface IRadioButtonProps extends RadioButtonProps {
   showAsCard?: boolean;
 }
 
-export const RadioButton = ({ showAsCard = false, ...rest }: IAppRadioButtonProps) => {
+export const RadioButton = ({ showAsCard = false, ...rest }: IRadioButtonProps) => {
   const inputRef = useRef<HTMLInputElement>(null);
   if (showAsCard) {
     return (
       <Card>
+        {/** This element is only clickable for visual
+         effects. A screen reader would only want to click
+         the inner input element of the DesignSystemRadioButton. */}
         {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
         <div
           className={classes.cardLabel}
+          data-testid={`test-id-${rest.label}`}
           onClick={() => {
             if (inputRef.current) {
               inputRef.current.click();
