af54bd0e7a2add1f8e8fcc518f13fc7e49853b81
Ole Martin Handeland <git@olemartin.org>
2023-06-28 13:05:09 +0200

    Expanding tests to check and verify that all schemas are there, and are named correctly

--- src/utils/layout/schema.test.ts
+++ src/utils/layout/schema.test.ts
@@ -1,11 +1,14 @@
 import Ajv from 'ajv';
+import Ajv2020 from 'ajv/dist/2020';
 import dotenv from 'dotenv';
 import fs from 'node:fs';
 import numberFormatSchema from 'schemas/json/component/number-format.schema.v1.json';
 import expressionSchema from 'schemas/json/layout/expression.schema.v1.json';
 import layoutSchema from 'schemas/json/layout/layout.schema.v1.json';
+import layoutSchemaV2 from 'schemas/json/layout/layout.schema.v2.json';
 import type { ErrorObject } from 'ajv';
 
+import { ComponentConfigs } from 'src/layout/components';
 import { getAllLayoutSets } from 'src/utils/layout/getAllLayoutSets';
 
 describe('Layout schema', () => {
@@ -27,13 +30,29 @@ describe('Layout schema', () => {
           expect(() => JSON.parse(content)).not.toThrow();
 
           const schema = JSON.parse(content);
-          const ajv = new Ajv();
+          const ajv = schema.$schema && schema.$schema.includes('2020-12') ? new Ajv2020() : new Ajv();
           expect(ajv.validateSchema(schema)).toBeTruthy();
         });
       }
     };
 
     recurse('schemas');
+  });
+
+  describe('All known components should have their own schema file', () => {
+    const schemaDir = 'schemas/json/component';
+    const oneOf = new Set(layoutSchemaV2.$defs.component.oneOf.map((schema) => schema.$ref.split('/').pop()));
+    for (const component of Object.keys(ComponentConfigs)) {
+      it(`${component}.schema.v1.json should exist and be referenced`, () => {
+        const fileName = `${component}.schema.v1.json`;
+        const fullPath = `${schemaDir}/${fileName}`;
+        expect(fs.existsSync(fullPath)).toBeTruthy();
+        expect(oneOf.has(fileName)).toBeTruthy();
+        const content = fs.readFileSync(fullPath, 'utf-8');
+        const schema = JSON.parse(content);
+        expect(schema.$id).toBe(`https://altinncdn.no/schemas/json/component/${fileName}`);
+      });
+    }
   });
 
   describe('All known layout sets should validate against the layout schema', () => {
