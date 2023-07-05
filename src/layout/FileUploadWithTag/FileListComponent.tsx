30cbd04757be7b1b0b8969373f7a9dac06d03c66
lassopicasso <lars.walderhaug@digdir.no>
2023-07-05 11:33:15 +0200

    replaced material-ui with vanilla table

--- src/layout/FileUploadWithTag/FileListComponent.tsx
+++ src/layout/FileUploadWithTag/FileListComponent.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { Button, ButtonColor, ButtonSize, ButtonVariant } from '@digdir/design-system-react';
-import { Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
 import { CheckmarkCircleFillIcon, PencilIcon } from '@navikt/aksel-icons';
 
 import { AltinnLoader } from 'src/components/AltinnLoader';
@@ -53,151 +52,138 @@ export function FileList({
   const { textResourceBindings } = node.item;
 
   return (
-    <Grid
-      container={true}
-      item={true}
+    <div
       data-testid='tagFile'
       id='tagFile'
-      spacing={1}
     >
-      <TableContainer component={Grid}>
-        <Table className={!mobileView ? classes.table : classes.tableMobile}>
-          {atleastOneTagExists(attachments) && (
-            <TableHead className={classes.tableHeader}>
-              <TableRow className={mobileView ? classes.mobileTableRow : ''}>
-                <TableCell align='left'>{lang('form_filler.file_uploader_list_header_name')}</TableCell>
-                <TableCell align='left'>
-                  {textResourceBindings?.tagTitle && langAsString(textResourceBindings.tagTitle)}
-                </TableCell>
-                {!mobileView ? (
-                  <TableCell align='left'>{lang('form_filler.file_uploader_list_header_file_size')}</TableCell>
-                ) : null}
-                {!mobileView ? (
-                  <TableCell align='left'>{lang('form_filler.file_uploader_list_header_status')}</TableCell>
-                ) : null}
-                <TableCell />
-              </TableRow>
-            </TableHead>
-          )}
-          <TableBody className={classes.tableBody}>
-            {attachments.map((attachment: IAttachment, index: number) => {
-              // Check if filter is applied and includes specified index.
-              if (attachment.tags !== undefined && attachment.tags.length > 0 && editIndex !== index) {
-                const firstTag = attachment.tags[0];
-                const label = options?.find((option) => option.value === firstTag)?.label;
-
-                return (
-                  <TableRow
-                    key={`altinn-file-list-row-${attachment.id}`}
-                    className={mobileView ? classes.mobileTableRow : ''}
-                  >
-                    <TableCell key={`attachment-name-${index}`}>
-                      <div style={{ minWidth: '0px' }}>
-                        <AttachmentFileName
-                          attachment={attachments[index]}
-                          mobileView={mobileView}
-                        />
-                        {mobileView ? (
-                          <div
-                            style={{
-                              color: AltinnAppTheme.altinnPalette.primary.grey,
-                            }}
-                          >
-                            {attachment.uploaded ? (
-                              <div>
-                                {(attachment.size / bytesInOneMB).toFixed(2)} {lang('form_filler.file_uploader_mb')}
-                                <CheckmarkCircleFillIcon
-                                  aria-label={langAsString('form_filler.file_uploader_list_status_done')}
-                                  role='img'
-                                  style={{ marginLeft: '5px' }}
-                                />
-                              </div>
-                            ) : (
-                              <AltinnLoader
-                                id={`attachment-loader-upload-${attachments[index].id}`}
-                                style={{
-                                  marginBottom: '1rem',
-                                  marginRight: '0.8125rem',
-                                }}
-                                srContent={langAsString('general.loading')}
-                              />
-                            )}
-                          </div>
-                        ) : null}
-                      </div>
-                    </TableCell>
-                    <TableCell key={`attachment-tag-${index}`}>{label && langAsString(label)}</TableCell>
-                    {!mobileView ? (
-                      <TableCell key={`attachment-size-${index}`}>
-                        {`${(attachment.size / bytesInOneMB).toFixed(2)} ${langAsString(
-                          'form_filler.file_uploader_mb',
-                        )}`}
-                      </TableCell>
-                    ) : null}
-                    {!mobileView ? (
-                      <TableCell key={`attachment-status-${index}`}>
-                        {attachment.uploaded ? (
-                          <div className={classes.fileStatus}>
-                            {lang('form_filler.file_uploader_list_status_done')}
-                            <CheckmarkCircleFillIcon aria-hidden={true} />
-                          </div>
-                        ) : (
-                          <AltinnLoader
-                            id={`attachment-loader-upload-${attachment.id}`}
-                            style={{
-                              marginBottom: '1rem',
-                              marginRight: '0.8125rem',
-                            }}
-                            srContent={langAsString('general.loading')}
-                          />
-                        )}
-                      </TableCell>
-                    ) : null}
-                    <TableCell
-                      align='right'
-                      key={`edit-${index}`}
-                    >
-                      <Button
-                        className={classes.editButton}
-                        size={ButtonSize.Small}
-                        variant={ButtonVariant.Quiet}
-                        color={ButtonColor.Secondary}
-                        onClick={() => onEdit(index)}
-                        icon={<PencilIcon aria-hidden={true} />}
-                        iconPlacement='right'
-                      >
-                        {!mobileView && lang('general.edit_alt')}
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                );
-              }
+      <table className={!mobileView ? classes.table : classes.tableMobile}>
+        {atleastOneTagExists(attachments) && (
+          <thead className={classes.tableHeader}>
+            <tr className={mobileView ? classes.mobileTableRow : ''}>
+              <th align='left'>{lang('form_filler.file_uploader_list_header_name')}</th>
+              <th align='left'>{textResourceBindings?.tagTitle && langAsString(textResourceBindings.tagTitle)}</th>
+              {!mobileView ? <th align='left'>{lang('form_filler.file_uploader_list_header_file_size')}</th> : null}
+              {!mobileView ? <th align='left'>{lang('form_filler.file_uploader_list_header_status')}</th> : null}
+              <th />
+            </tr>
+          </thead>
+        )}
+        <tbody className={classes.tableBody}>
+          {attachments.map((attachment: IAttachment, index: number) => {
+            // Check if filter is applied and includes specified index.
+            if (attachment.tags !== undefined && attachment.tags.length > 0 && editIndex !== index) {
+              const firstTag = attachment.tags[0];
+              const label = options?.find((option) => option.value === firstTag)?.label;
 
               return (
-                <TableRow key={`altinn-unchosen-option-attachment-row-${index}`}>
-                  <TableCell
-                    className={mobileView ? classes.fullGrid : ''}
-                    colSpan={!mobileView ? 5 : undefined}
+                <tr
+                  key={`altinn-file-list-row-${attachment.id}`}
+                  className={mobileView ? classes.mobileTableRow : ''}
+                >
+                  <td key={`attachment-name-${index}`}>
+                    <div style={{ minWidth: '0px' }}>
+                      <AttachmentFileName
+                        attachment={attachments[index]}
+                        mobileView={mobileView}
+                      />
+                      {mobileView ? (
+                        <div
+                          style={{
+                            color: AltinnAppTheme.altinnPalette.primary.grey,
+                          }}
+                        >
+                          {attachment.uploaded ? (
+                            <div>
+                              {(attachment.size / bytesInOneMB).toFixed(2)} {lang('form_filler.file_uploader_mb')}
+                              <CheckmarkCircleFillIcon
+                                aria-label={langAsString('form_filler.file_uploader_list_status_done')}
+                                role='img'
+                                style={{ marginLeft: '5px' }}
+                              />
+                            </div>
+                          ) : (
+                            <AltinnLoader
+                              id={`attachment-loader-upload-${attachments[index].id}`}
+                              style={{
+                                marginBottom: '1rem',
+                                marginRight: '0.8125rem',
+                              }}
+                              srContent={langAsString('general.loading')}
+                            />
+                          )}
+                        </div>
+                      ) : null}
+                    </div>
+                  </td>
+                  <td key={`attachment-tag-${index}`}>{label && langAsString(label)}</td>
+                  {!mobileView ? (
+                    <td key={`attachment-size-${index}`}>
+                      {`${(attachment.size / bytesInOneMB).toFixed(2)} ${langAsString('form_filler.file_uploader_mb')}`}
+                    </td>
+                  ) : null}
+                  {!mobileView ? (
+                    <td key={`attachment-status-${index}`}>
+                      {attachment.uploaded ? (
+                        <div className={classes.fileStatus}>
+                          {lang('form_filler.file_uploader_list_status_done')}
+                          <CheckmarkCircleFillIcon aria-hidden={true} />
+                        </div>
+                      ) : (
+                        <AltinnLoader
+                          id={`attachment-loader-upload-${attachment.id}`}
+                          style={{
+                            marginBottom: '1rem',
+                            marginRight: '0.8125rem',
+                          }}
+                          srContent={langAsString('general.loading')}
+                        />
+                      )}
+                    </td>
+                  ) : null}
+                  <td
+                    align='right'
+                    key={`edit-${index}`}
                   >
-                    <EditWindowComponent
-                      node={node}
-                      attachment={attachments[index]}
-                      attachmentValidations={[
-                        ...new Map(attachmentValidations.map((validation) => [validation['id'], validation])).values(),
-                      ]}
-                      mobileView={mobileView}
-                      options={options}
-                      onSave={onSave}
-                      onDropdownDataChange={onDropdownDataChange}
-                      setEditIndex={setEditIndex}
-                    />
-                  </TableCell>
-                </TableRow>
+                    <Button
+                      className={classes.editButton}
+                      size={ButtonSize.Small}
+                      variant={ButtonVariant.Quiet}
+                      color={ButtonColor.Secondary}
+                      onClick={() => onEdit(index)}
+                      icon={<PencilIcon aria-hidden={true} />}
+                      iconPlacement='right'
+                    >
+                      {!mobileView && lang('general.edit_alt')}
+                    </Button>
+                  </td>
+                </tr>
               );
-            })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Grid>
+            }
+
+            return (
+              <tr key={`altinn-unchosen-option-attachment-row-${index}`}>
+                <td
+                  className={mobileView ? classes.fullGrid : ''}
+                  colSpan={!mobileView ? 5 : undefined}
+                >
+                  <EditWindowComponent
+                    node={node}
+                    attachment={attachments[index]}
+                    attachmentValidations={[
+                      ...new Map(attachmentValidations.map((validation) => [validation['id'], validation])).values(),
+                    ]}
+                    mobileView={mobileView}
+                    options={options}
+                    onSave={onSave}
+                    onDropdownDataChange={onDropdownDataChange}
+                    setEditIndex={setEditIndex}
+                  />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 }
