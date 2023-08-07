a15c6230f034cf45c7820ffd04b1cdfcb1be78d8
lassopicasso <lars.walderhaug@digdir.no>
2023-08-07 13:13:01 +0200

    fix crash when defaultSelectedOption is not valid

--- src/features/instantiate/containers/InstanceSelection.tsx
+++ src/features/instantiate/containers/InstanceSelection.tsx
@@ -43,11 +43,15 @@ function getDateDisplayString(timeStamp: string) {
 export function InstanceSelection({ instances, onNewInstance }: IInstanceSelectionProps) {
   const { data: applicationMetadata } = useApplicationMetadataQuery();
   const instanceSelectionOptions = applicationMetadata?.onEntry?.instanceSelection;
+  const selectedIndex = instanceSelectionOptions?.defaultSelectedOption;
   const { lang, langAsString, language } = useLanguage();
   const mobileView = useIsMobileOrTablet();
   const rowsPerPageOptions = instanceSelectionOptions?.rowsPerPageOptions ?? [10, 25, 50];
-  const defaultSelectedOption = instanceSelectionOptions?.defaultSelectedOption ?? 0;
 
+  const doesIndexExist = (selectedIndex: number) =>
+    rowsPerPageOptions.length - 1 >= selectedIndex && selectedIndex >= 0 ? selectedIndex : 0;
+
+  const defaultSelectedOption = selectedIndex ? doesIndexExist(selectedIndex) : 0;
   const [currentPage, setCurrentPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(rowsPerPageOptions[defaultSelectedOption]);
 
