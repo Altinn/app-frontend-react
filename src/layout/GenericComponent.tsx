a5d7a393fcbd2e5e5066af913e7607bc50e7d372
lassopicasso <lars.walderhaug@digdir.no>
2023-08-19 17:53:03 +0200

    check for maxLength error message

--- src/layout/GenericComponent.tsx
+++ src/layout/GenericComponent.tsx
@@ -105,7 +105,7 @@ export function GenericComponent<Type extends ComponentTypes = ComponentTypes>({
   const GetFocusSelector = makeGetFocus();
   const hasValidationMessages = node.hasValidationMessages('any');
   const hidden = node.isHidden();
-  const { lang } = useLanguage();
+  const { lang, langAsString } = useLanguage();
 
   const formData = node.getFormData();
   const currentView = useAppSelector((state) => state.formLayout.uiConfig.currentView);
@@ -117,13 +117,23 @@ export function GenericComponent<Type extends ComponentTypes = ComponentTypes>({
     shallowEqual,
   );
 
-  //Hide validation message if component maxLength exists and exceeded (handled by component's own message) and it's the only error
-  const componentMaxLength = node.item?.maxLength;
-  const hideValidationMessage =
-    componentMaxLength &&
-    formData?.simpleBinding &&
-    formData?.simpleBinding?.length > componentMaxLength &&
-    componentValidations?.simpleBinding?.errors?.length === 1;
+  const filterValidationErrors = () => {
+    const maxLength = node.item?.maxLength;
+
+    if (!maxLength) {
+      return componentValidations?.simpleBinding;
+    }
+
+    // If maxLength is set in both schema and component, don't display the schema message
+    const errorMessageMaxLength = langAsString('validation_errors.maxLength', [maxLength]) as string;
+    const componentErrors = componentValidations?.simpleBinding?.errors || [];
+    const updatedErrors = componentErrors.filter((error: string) => error !== errorMessageMaxLength);
+
+    return {
+      ...componentValidations.simpleBinding,
+      errors: updatedErrors,
+    };
+  };
 
   const formComponentContext = useMemo<IFormComponentContext>(
     () => ({
@@ -260,8 +270,7 @@ export function GenericComponent<Type extends ComponentTypes = ComponentTypes>({
     overrideDisplay,
   };
 
-  const showValidationMessages =
-    hasValidationMessages && !hideValidationMessage && layoutComponent.renderDefaultValidations();
+  const showValidationMessages = hasValidationMessages && layoutComponent.renderDefaultValidations();
 
   if (node.item.renderAsSummary) {
     const RenderSummary = 'renderSummary' in node.def ? node.def.renderSummary.bind(node.def) : null;
@@ -285,7 +294,7 @@ export function GenericComponent<Type extends ComponentTypes = ComponentTypes>({
       </FormComponentContext.Provider>
     );
   }
-  console.log(componentValidations?.simpleBinding?.errors?.length === 1);
+
   return (
     <FormComponentContext.Provider value={formComponentContext}>
       <Grid
@@ -320,7 +329,7 @@ export function GenericComponent<Type extends ComponentTypes = ComponentTypes>({
           {...gridBreakpoints(item.grid?.innerGrid)}
         >
           <RenderComponent {...componentProps} />
-          {showValidationMessages && renderValidationMessagesForComponent(componentValidations?.simpleBinding, id)}
+          {showValidationMessages && renderValidationMessagesForComponent(filterValidationErrors(), id)}
         </Grid>
       </Grid>
     </FormComponentContext.Provider>
