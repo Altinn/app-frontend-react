a279412d074585c80ac8501c7524bd37b76ed3ce
Mikael Solstad <mikaelrinosolstad@gmail.com>
2023-08-22 09:29:19 +0200

    refactor(AccordionGroup): refactor to be a ContainerComponent instead of PresentationComponent

--- src/layout/AccordionGroup/index.tsx
+++ src/layout/AccordionGroup/index.tsx
@@ -3,14 +3,15 @@ import React from 'react';
 import { AccordionGroup as AccordionGroupComponent } from 'src/layout/AccordionGroup/AccordionGroup';
 import { AccordionGroupHierarchyGenerator } from 'src/layout/AccordionGroup/hierarchy';
 import { SummaryAccordionGroupComponent } from 'src/layout/AccordionGroup/SummaryAccordionGroupComponent';
-import { PresentationComponent } from 'src/layout/LayoutComponent';
+import { ContainerComponent } from 'src/layout/LayoutComponent';
 import type { PropsFromGenericComponent } from 'src/layout';
 import type { IAccordionGroup, ILayoutAccordionGroup } from 'src/layout/AccordionGroup/types';
 import type { SummaryRendererProps } from 'src/layout/LayoutComponent';
+import type { LayoutNodeFromType } from 'src/utils/layout/hierarchy.types';
 import type { ComponentHierarchyGenerator } from 'src/utils/layout/HierarchyGenerator';
 import type { LayoutNode } from 'src/utils/layout/LayoutNode';
 
-export class AccordionGroup extends PresentationComponent<'AccordionGroup'> {
+export class AccordionGroup extends ContainerComponent<'AccordionGroup'> {
   private _hierarchyGenerator = new AccordionGroupHierarchyGenerator();
 
   render(props: PropsFromGenericComponent<'AccordionGroup'>): React.JSX.Element | null {
@@ -24,8 +25,17 @@ export class AccordionGroup extends PresentationComponent<'AccordionGroup'> {
   renderSummary(props: SummaryRendererProps<'AccordionGroup'>): JSX.Element | null {
     return <SummaryAccordionGroupComponent {...props} />;
   }
+
   renderSummaryBoilerplate(): boolean {
     return false;
+  }
+
+  shouldRenderInAutomaticPDF(node: LayoutNodeFromType<AccordionGroup>): boolean {
+    return !node.item.renderAsSummary;
+  }
+
+  getDisplayData(): string {
+    return '';
   }
 }
 
