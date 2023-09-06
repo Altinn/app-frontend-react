6203a9c7293edfae0ab0be8baf9645072b97fd32
Magnus Revheim Martinsen <mrmartinsen.96@gmail.com>
2023-09-06 14:27:55 +0200

    use redux for formData and repeatingGroups

--- src/hooks/useGetOptions.ts
+++ src/hooks/useGetOptions.ts
@@ -3,16 +3,14 @@ import { useEffect } from 'react';
 import { useApplicationMetadataQuery } from 'src/hooks/queries/useApplicationMetadataQuery';
 import { useApplicationSettingsQuery } from 'src/hooks/queries/useApplicationSettingsQuery';
 import { useCurrentInstanceQuery } from 'src/hooks/queries/useCurrentInstanceQuery';
-import { useFormDataForOptionsQuery } from 'src/hooks/queries/useFormdataForOptionsQuery';
 import { useGetOptionsQuery } from 'src/hooks/queries/useGetOptionsQuery';
 import { useLayoutSetsQuery } from 'src/hooks/queries/useLayoutSetsQuery';
 import { useLayoutsQuery } from 'src/hooks/queries/useLayoutsQuery';
-import { useRepeatingGroupsQuery } from 'src/hooks/queries/useRepeatingGroupsQuery';
 import { useTextResourcesQuery } from 'src/hooks/queries/useTextResourcesQuery';
+import { useAppSelector } from 'src/hooks/useAppSelector';
 import { useLanguage } from 'src/hooks/useLanguage';
 import { useStateDeepEqual } from 'src/hooks/useStateDeepEqual';
-import { getCurrentTaskDataElementId, getLayoutSetIdForApplication } from 'src/utils/appMetadata';
-import { convertModelToDataBinding } from 'src/utils/databindings';
+import { getLayoutSetIdForApplication } from 'src/utils/appMetadata';
 import { buildInstanceContext } from 'src/utils/instanceContext';
 import { getRelevantFormDataForOptionSource, setupSourceOptions } from 'src/utils/options';
 import type { IMapping, IOption, IOptionSource } from 'src/layout/common.generated';
@@ -42,26 +40,11 @@ export const useGetOptions = ({ optionsId, mapping, queryParameters, secure, sou
   const { data: applicationSettings } = useApplicationSettingsQuery();
   const { data: layoutSets } = useLayoutSetsQuery();
 
-  const currentTaskDataElementId = getCurrentTaskDataElementId(
-    applicationMetadata || null,
-    instance || null,
-    layoutSets || null,
-  );
-  const { data: fetchedFormData } = useFormDataForOptionsQuery(
-    instanceId || '',
-    currentTaskDataElementId || '',
-    !!currentTaskDataElementId && !!instanceId,
-  );
-  const formData = fetchedFormData && convertModelToDataBinding(fetchedFormData);
+  const formData = useAppSelector((state) => state.formData.formData);
+  const repeatingGroups = useAppSelector((state) => state.formLayout?.uiConfig.repeatingGroups);
 
   const layoutSetId = getLayoutSetIdForApplication(applicationMetadata || null, instance, layoutSets);
   const { data: layouts } = useLayoutsQuery(layoutSetId || '', !!layoutSetId);
-  const { data: repeatingGroups } = useRepeatingGroupsQuery(
-    instanceId || '',
-    currentTaskDataElementId || '',
-    layouts || null,
-    !!instanceId && !!currentTaskDataElementId && !!layouts,
-  );
 
   const { label, description, helpText } = source || {};
   const { data: textResource } = useTextResourcesQuery(selectedLanguage, !!selectedLanguage);
@@ -81,34 +64,35 @@ export const useGetOptions = ({ optionsId, mapping, queryParameters, secure, sou
       setOptions(fetchedOptions);
     }
 
-    if (!source || !repeatingGroups) {
-      return;
+    if (!!source && !!repeatingGroups) {
+      console.log('fomrmData', formData);
+
+      // should be solved with useLanguage hook in setupSourceOptions
+      const relevantFormData = (formData && getRelevantFormDataForOptionSource(formData, source)) || {};
+      const findResourceById = (id?: string) => textResource?.resources?.find((resource) => resource.id === id);
+      const relevantTextResources = {
+        label: findResourceById(label),
+        description: findResourceById(description),
+        helpText: findResourceById(helpText),
+      };
+
+      const instanceContext = buildInstanceContext(instance);
+      const dataSources: IDataSources = {
+        dataModel: relevantFormData,
+        applicationSettings: applicationSettings || {},
+        instanceContext,
+      };
+
+      setOptions(
+        setupSourceOptions({
+          source,
+          relevantTextResources,
+          relevantFormData,
+          repeatingGroups,
+          dataSources,
+        }),
+      );
     }
-
-    const findResourceById = (id?: string) => textResource?.resources?.find((resource) => resource.id === id);
-    const relevantTextResources = {
-      label: findResourceById(label),
-      description: findResourceById(description),
-      helpText: findResourceById(helpText),
-    };
-
-    const relevantFormData = (formData && getRelevantFormDataForOptionSource(formData, source)) || {};
-    const instanceContext = buildInstanceContext(instance);
-    const dataSources: IDataSources = {
-      dataModel: relevantFormData,
-      applicationSettings: applicationSettings || {},
-      instanceContext,
-    };
-
-    setOptions(
-      setupSourceOptions({
-        source,
-        relevantTextResources,
-        relevantFormData,
-        repeatingGroups,
-        dataSources,
-      }),
-    );
   }, [
     applicationSettings,
     instance,
@@ -121,6 +105,7 @@ export const useGetOptions = ({ optionsId, mapping, queryParameters, secure, sou
     description,
     helpText,
     textResource?.resources,
+    layouts,
   ]);
   return options;
 };
