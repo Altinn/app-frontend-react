f0c341cd0c19eddf942a13678e7e2cb63508b7a9
Ole Martin Handeland <git@olemartin.org>
2023-09-28 17:17:27 +0200

    Trying to write a new reducer for the attachments context

--- src/features/attachments/map/mapAttachments.ts
+++ src/features/attachments/map/mapAttachments.ts
@@ -2,7 +2,7 @@ import { useMemo } from 'react';
 
 import { useAppSelector } from 'src/hooks/useAppSelector';
 import { useExprContext } from 'src/utils/layout/ExprContext';
-import type { IAttachment, IAttachments } from 'src/features/attachments';
+import type { IAttachments } from 'src/features/attachments';
 import type { IData } from 'src/types/shared';
 import type { LayoutPages } from 'src/utils/layout/LayoutPages';
 
@@ -24,19 +24,7 @@ export function mapAttachments(dataElements: IData[], nodes: LayoutPages): IAtta
     });
   }
 
-  // Sort all attachments by name
-  for (const attachmentComponentId in attachments) {
-    attachments[attachmentComponentId]?.sort(sortAttachmentsByName);
-  }
-
   return attachments;
-}
-
-function sortAttachmentsByName(a: IAttachment, b: IAttachment) {
-  if (a.data.filename && b.data.filename) {
-    return a.data.filename.localeCompare(b.data.filename);
-  }
-  return 0;
 }
 
 export function useMappedAttachments() {
