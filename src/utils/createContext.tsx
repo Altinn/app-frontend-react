e1f76fb4959123990f77bbf6dfe49a6a3cf4ce9b
Ole Martin Handeland <git@olemartin.org>
2023-10-17 11:04:52 +0200

    Fixing the useHasProvider which was broken (it reported we had an instance provider even in a stateless app)

--- src/utils/createContext.tsx
+++ src/utils/createContext.tsx
@@ -39,12 +39,15 @@ export function createStrictContext<T>(props?: CreateStrictContextProps): Contex
  * This is just a wrapper around the React.createContext function, and provides a slightly nicer API.
  */
 export function createLaxContext<T>(initialState?: T): LaxContextProvider<T | undefined> {
-  const Context = React.createContext<{ innerValue: T | undefined }>({ innerValue: initialState });
+  const Context = React.createContext<{ innerValue: T | undefined; provided: boolean }>({
+    innerValue: initialState,
+    provided: false,
+  });
   const useCtx = () => React.useContext(Context).innerValue;
-  const useHasProvider = () => React.useContext(Context) !== undefined;
+  const useHasProvider = () => Boolean(React.useContext(Context).provided);
 
   const Provider = ({ value, children }: Parameters<React.Provider<T | undefined>>[0]) => (
-    <Context.Provider value={{ innerValue: value }}>{children}</Context.Provider>
+    <Context.Provider value={{ innerValue: value, provided: true }}>{children}</Context.Provider>
   );
 
   return {
