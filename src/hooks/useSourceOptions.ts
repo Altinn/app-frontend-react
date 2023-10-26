9c34ef89bf060cc3783164f095609ed282982d40
Mikael Solstad <mikaelrinosolstad@gmail.com>
2023-10-26 08:30:18 +0200

    feat: Add support for dynamic expressions in source.label, source.description, and source.helpText

--- src/hooks/useSourceOptions.ts
+++ src/hooks/useSourceOptions.ts
@@ -2,13 +2,13 @@ import { useMemo } from 'react';
 
 import { pick } from 'dot-object';
 
+import { evalExpr, isExpression } from 'src/features/expressions';
 import { useAppSelector } from 'src/hooks/useAppSelector';
-import { useLanguage } from 'src/hooks/useLanguage';
 import { convertDataBindingToModel, getKeyWithoutIndexIndicators } from 'src/utils/databindings';
 import { transposeDataBinding } from 'src/utils/databindings/DataBinding';
-import type { IFormData } from 'src/features/formData';
-import type { IUseLanguage } from 'src/hooks/useLanguage';
+import { selectDataSourcesFromState } from 'src/utils/layout/hierarchy';
 import type { IOption, IOptionSource } from 'src/layout/common.generated';
+import type { HierarchyDataSources } from 'src/layout/layout';
 import type { LayoutNode } from 'src/utils/layout/LayoutNode';
 
 interface IUseSourceOptionsArgs {
@@ -17,22 +17,21 @@ interface IUseSourceOptionsArgs {
 }
 
 export const useSourceOptions = ({ source, node }: IUseSourceOptionsArgs): IOption[] | undefined => {
-  const formData = useAppSelector((state) => state.formData.formData);
-  const langTools = useLanguage(node);
+  const dataSources = useAppSelector(selectDataSourcesFromState);
 
-  return useMemo(() => getSourceOptions({ source, node, formData, langTools }), [source, node, formData, langTools]);
+  return useMemo(() => getSourceOptions({ source, node, dataSources }), [source, node, dataSources]);
 };
 
 interface IGetSourceOptionsArgs extends IUseSourceOptionsArgs {
-  formData: IFormData;
-  langTools: IUseLanguage;
+  dataSources: HierarchyDataSources;
 }
 
-export function getSourceOptions({ source, node, formData, langTools }: IGetSourceOptionsArgs): IOption[] | undefined {
+export function getSourceOptions({ source, node, dataSources }: IGetSourceOptionsArgs): IOption[] | undefined {
   if (!source) {
     return undefined;
   }
 
+  const { formData, langTools } = dataSources;
   const { group, value, label, helpText, description } = source;
   const cleanValue = getKeyWithoutIndexIndicators(value);
   const cleanGroup = getKeyWithoutIndexIndicators(group);
@@ -46,11 +45,36 @@ export function getSourceOptions({ source, node, formData, langTools }: IGetSour
       for (const idx in groupData) {
         const path = `${groupPath}[${idx}]`;
         const valuePath = transposeDataBinding({ subject: cleanValue, currentLocation: path });
+
+        /**
+         * Running evalExpression is all that is needed to support dynamic expressions in
+         * source options. However, since there are multiple rows of content which might
+         * contain text variables, evalExpr needs to be able to resolve these values at
+         * the correct path in the data model i.e. use langAsStringUsingPathInDataModel.
+         *
+         * To coerce the text-function in dynamic expressions to use the correct function
+         * (langAsStringUsingPathInDataModel), this modified dataSources modifies the
+         * langAsString function to actually be langAsStringUsingPathInDataModel partially
+         * applied with the correct path in the data model.
+         */
+        const modifiedDataSources = {
+          ...dataSources,
+          langTools: {
+            ...langTools,
+            langAsString: (key: string) => langTools.langAsStringUsingPathInDataModel(key, path),
+          },
+        };
         output.push({
           value: pick(valuePath, formDataAsObject),
-          label: langTools.langAsStringUsingPathInDataModel(label, path),
-          description: langTools.langAsStringUsingPathInDataModel(description, path),
-          helpText: langTools.langAsStringUsingPathInDataModel(helpText, path),
+          label: isExpression(label)
+            ? evalExpr(label, node, modifiedDataSources)
+            : langTools.langAsStringUsingPathInDataModel(label, path),
+          description: isExpression(description)
+            ? evalExpr(description, node, modifiedDataSources)
+            : langTools.langAsStringUsingPathInDataModel(description, path),
+          helpText: isExpression(helpText)
+            ? evalExpr(helpText, node, modifiedDataSources)
+            : langTools.langAsStringUsingPathInDataModel(helpText, path),
         });
       }
     }
