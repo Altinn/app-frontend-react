e2184e343690e4251d23fc5bc245c7ed262f4fd6
Ole Martin Handeland <git@olemartin.org>
2023-11-16 09:41:15 +0100

    Attempting a rewrite of attachment selecting in AttachmentListComponent, adding a few test-cases to customReceipt.ts

--- src/layout/AttachmentList/AttachmentListComponent.tsx
+++ src/layout/AttachmentList/AttachmentListComponent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { Grid, Typography } from '@material-ui/core';
 
@@ -7,23 +7,56 @@ import { useLaxInstanceData } from 'src/features/instance/InstanceContext';
 import { useLaxProcessData } from 'src/features/instance/ProcessContext';
 import { useAppSelector } from 'src/hooks/useAppSelector';
 import { useLanguage } from 'src/hooks/useLanguage';
-import { selectAttachments, selectDataTypesByIds } from 'src/selectors/dataTypes';
-import { DataTypeReference } from 'src/utils/attachmentsUtils';
+import { DataTypeReference, filterDisplayPdfAttachments, getDisplayAttachments } from 'src/utils/attachmentsUtils';
 import type { PropsFromGenericComponent } from 'src/layout';
+import type { IData, IDataType } from 'src/types/shared';
 
 export type IAttachmentListProps = PropsFromGenericComponent<'AttachmentList'>;
 
+const emptyDataArray: IData[] = [];
+const emptyDataTypeArray: IDataType[] = [];
+
 export function AttachmentListComponent({ node }: IAttachmentListProps) {
   const { lang } = useLanguage();
-  const instance = useLaxInstanceData();
-  const process = useLaxProcessData();
-  const dataForTask = useAppSelector(selectDataTypesByIds(node.item.dataTypeIds, instance, process));
+  const instanceData = useLaxInstanceData()?.data ?? emptyDataArray;
+  const currentTaskId = useLaxProcessData()?.currentTask?.elementId;
+  const dataTypes =
+    useAppSelector((state) => state.applicationMetadata.applicationMetadata?.dataTypes) ?? emptyDataTypeArray;
 
-  const includePdf =
-    node.item.dataTypeIds?.some((id) =>
-      [DataTypeReference.RefDataAsPdf, DataTypeReference.IncludeAll].includes(id as DataTypeReference),
-    ) ?? false;
-  const attachments = useAppSelector(selectAttachments(dataForTask, includePdf, process));
+  const attachments = useMemo(() => {
+    const allowedTypes = new Set(node.item.dataTypeIds ?? []);
+    const includePdf =
+      allowedTypes.has(DataTypeReference.RefDataAsPdf) || allowedTypes.has(DataTypeReference.IncludeAll);
+    const pdfAttachments = includePdf ? filterDisplayPdfAttachments(instanceData) : [];
+
+    // These are the data types that are marked as available in the current task
+    const dataTypesInTask = new Set(dataTypes.filter((type) => type.taskId === currentTaskId).map((type) => type.id));
+
+    // This is a list of data types that are clearly data models. We don't show those when listing attachments.
+    const dataModelTypes = new Set(dataTypes.filter((dataType) => dataType.appLogic?.classRef).map((type) => type.id));
+
+    // This filter function takes all the instance data and filters down to only the attachments
+    // we're interested in showing here.
+    const attachments = instanceData.filter((el) => {
+      if (el.dataType === DataTypeReference.RefDataAsPdf || dataModelTypes.has(el.dataType)) {
+        return false;
+      }
+
+      if (allowedTypes.has(DataTypeReference.IncludeAll) || allowedTypes.has(el.dataType)) {
+        return true;
+      }
+
+      if (allowedTypes.size === 0) {
+        // If no data types are specified, we show all data types in the current task
+        return dataTypesInTask.has(el.dataType);
+      }
+
+      return false;
+    });
+
+    const otherAttachments = getDisplayAttachments(attachments);
+    return [...pdfAttachments, ...otherAttachments];
+  }, [currentTaskId, dataTypes, instanceData, node.item.dataTypeIds]);
 
   return (
     <Grid
