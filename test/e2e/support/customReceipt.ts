58667524c398819df0dcf350ba59d64b05f80e97
lassopicasso <lars.walderhaug@digdir.no>
2023-11-17 08:09:29 +0100

    rename header-id

--- test/e2e/support/customReceipt.ts
+++ test/e2e/support/customReceipt.ts
@@ -20,7 +20,7 @@ export const customReceipt: ILayout = [
     type: 'Paragraph',
     textResourceBindings: { title: 'Takk for din innsending, dette er en veldig fin custom kvittering.' },
   },
-  { id: 'r-header-pdfs', type: 'Header', size: 'M', textResourceBindings: { title: 'receipt.title_submitted' } },
+  { id: 'r-pdfs-header', type: 'Header', size: 'M', textResourceBindings: { title: 'receipt.title_submitted' } },
   { id: 'r-pdfs', type: 'AttachmentList', dataTypeIds: ['ref-data-as-pdf'] },
   ...generateAttachmentLists({
     id: 'r-attachments-one',
