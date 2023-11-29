7b01d9b6b778981f3f8a854f930d001e91507a06
Mikael Solstad <mikaelrinosolstad@gmail.com>
2023-11-29 07:54:38 +0100

    fix: issue with back to summary page being shown after navigating to next step

--- src/layout/Button/ButtonComponent.tsx
+++ src/layout/Button/ButtonComponent.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { usePageNavigationContext } from 'src/features/form/layout/PageNavigationContext';
 import { useLaxProcessData } from 'src/features/instance/ProcessContext';
 import { useProcessNavigation } from 'src/features/instance/ProcessNavigationContext';
 import { useLanguage } from 'src/features/language/useLanguage';
@@ -22,6 +23,7 @@ export const ButtonComponent = ({ node, ...componentProps }: IButtonReceivedProp
   const currentTaskType = useLaxProcessData()?.currentTask?.altinnTaskType;
   const { actions, write } = useLaxProcessData()?.currentTask || {};
   const { next, canSubmit, busyWithId, attachmentsPending } = useProcessNavigation() || {};
+  const { setReturnToView } = usePageNavigationContext() || {};
 
   const disabled =
     !canSubmit || (currentTaskType === 'data' && !write) || (currentTaskType === 'confirmation' && !actions?.confirm);
@@ -43,6 +45,7 @@ export const ButtonComponent = ({ node, ...componentProps }: IButtonReceivedProp
 
   const submitTask = () => {
     if (!disabled && next) {
+      setReturnToView(undefined);
       if (currentTaskType === 'data') {
         next({ nodeId: node.item.id });
       } else if (currentTaskType === 'confirmation') {
