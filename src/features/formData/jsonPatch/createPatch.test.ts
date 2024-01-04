8c7102d1287ef34e02834c85b455cd3b57df66f8
Ole Martin Handeland <git@olemartin.org>
2024-01-04 17:45:39 +0100

    Implementing a JsonPatch generator

---
+++ src/features/formData/jsonPatch/createPatch.test.ts
@@ -0,0 +1,321 @@
+import { applyPatch } from 'fast-json-patch';
+
+import { createPatch } from 'src/features/formData/jsonPatch/createPatch';
+import type { JsonPatch } from 'src/features/formData/jsonPatch/types';
+
+interface TestPatchProps<T> {
+  prev: T;
+  next: T;
+  expected: JsonPatch;
+}
+
+function testPatch<T extends object>({ prev, next, expected }: TestPatchProps<T>) {
+  const patch = createPatch({ prev, next });
+  expect(patch).toEqual(expected);
+
+  const patched = applyPatch(prev, patch).newDocument;
+  expect(patched).toEqual(next);
+}
+
+describe('createPatch', () => {
+  it('should return an empty array when given two empty objects', () => {
+    testPatch({ prev: {}, next: {}, expected: [] });
+  });
+
+  it('should return an empty array when given two identical objects', () => {
+    testPatch({ prev: { a: 1 }, next: { a: 1 }, expected: [] });
+  });
+
+  it('should return simple replace', () => {
+    testPatch({
+      prev: { a: 1 },
+      next: { a: 2 },
+      expected: [
+        { op: 'test', path: '/a', value: 1 },
+        { op: 'replace', path: '/a', value: 2 },
+      ],
+    });
+  });
+
+  it('should return simple remove', () => {
+    testPatch({
+      prev: { a: 1 },
+      next: {},
+      expected: [
+        { op: 'test', path: '/a', value: 1 },
+        { op: 'remove', path: '/a' },
+      ],
+    });
+  });
+
+  it('should return simple add', () => {
+    testPatch({ prev: {}, next: { a: 1 }, expected: [{ op: 'add', path: '/a', value: 1 }] });
+  });
+
+  it('should create an add op for a nested object', () => {
+    testPatch({ prev: {}, next: { a: { b: 1 } }, expected: [{ op: 'add', path: '/a', value: { b: 1 } }] });
+  });
+
+  it('should create an add op for a nested array', () => {
+    testPatch({ prev: {}, next: { a: [1, 2, 3] }, expected: [{ op: 'add', path: '/a', value: [1, 2, 3] }] });
+  });
+
+  it('should create an add op for a nested array with objects', () => {
+    testPatch({
+      prev: {},
+      next: { a: [{ b: 1 }, { b: 2 }] },
+      expected: [{ op: 'add', path: '/a', value: [{ b: 1 }, { b: 2 }] }],
+    });
+  });
+
+  it('should create a shallower add op for a nested object', () => {
+    testPatch({
+      prev: { a: { b: 1 } },
+      next: { a: { b: 1, c: 2 } },
+      expected: [{ op: 'add', path: '/a/c', value: 2 }],
+    });
+  });
+
+  it('should create a shallower add op for a nested array', () => {
+    testPatch({
+      prev: { a: [1, 2, 3] },
+      next: { a: [1, 2, 3, 4] },
+      expected: [
+        { op: 'test', path: '/a', value: [1, 2, 3] },
+        { op: 'add', path: '/a/-', value: 4 },
+      ],
+    });
+  });
+
+  it('should create a shallower add op for a nested array with objects', () => {
+    const common = { d: 5, e: 6 };
+    testPatch({
+      prev: {
+        a: [
+          { b: 1, ...common },
+          { b: 2, ...common },
+        ],
+      },
+      next: { a: [{ b: 1, ...common }, { b: 2, ...common }, { b: 3 }] },
+      expected: [
+        {
+          op: 'test',
+          path: '/a',
+          value: [
+            { b: 1, ...common },
+            { b: 2, ...common },
+          ],
+        },
+        { op: 'add', path: '/a/-', value: { b: 3 } },
+      ],
+    });
+  });
+
+  it('should create a remove op for a nested object', () => {
+    testPatch({
+      prev: { a: { b: 1 } },
+      next: {},
+      expected: [
+        { op: 'test', path: '/a', value: { b: 1 } },
+        { op: 'remove', path: '/a' },
+      ],
+    });
+  });
+
+  it('should create a remove op for a nested array', () => {
+    testPatch({
+      prev: { a: [1, 2, 3] },
+      next: {},
+      expected: [
+        { op: 'test', path: '/a', value: [1, 2, 3] },
+        { op: 'remove', path: '/a' },
+      ],
+    });
+  });
+
+  it('should create a remove op when removing an index from a nested array', () => {
+    testPatch({
+      prev: { a: [1, 2, 3] },
+      next: { a: [1, 3] },
+      expected: [
+        { op: 'test', path: '/a', value: [1, 2, 3] },
+        { op: 'remove', path: '/a/1' },
+      ],
+    });
+  });
+
+  it('should create a remove op when removing an object from a nested array', () => {
+    testPatch({
+      prev: { a: [{ b: 1 }, { b: 2 }] },
+      next: { a: [{ b: 1 }] },
+      expected: [
+        { op: 'test', path: '/a', value: [{ b: 1 }, { b: 2 }] },
+        { op: 'remove', path: '/a/1' },
+      ],
+    });
+  });
+
+  it('should not produce a patch when deep arrays are equal', () => {
+    testPatch({
+      prev: {
+        a: [
+          [1, 2],
+          [3, 4],
+        ],
+      },
+      next: {
+        a: [
+          [1, 2],
+          [3, 4],
+        ],
+      },
+      expected: [],
+    });
+  });
+
+  it('should create multiple remove operations when removing multiple values from arrays', () => {
+    testPatch({
+      prev: { a: [1, 2, 3] },
+      next: { a: [1] },
+      expected: [
+        { op: 'test', path: '/a', value: [1, 2, 3] },
+        { op: 'remove', path: '/a/2' },
+        { op: 'remove', path: '/a/1' },
+      ],
+    });
+  });
+
+  it('should create a valid patch when removing values and adding values to the array at the same time', () => {
+    testPatch({
+      prev: { a: [1, 2, 3] },
+      next: { a: [2, 3, 4] },
+      expected: [
+        { op: 'test', path: '/a', value: [1, 2, 3] },
+        { op: 'remove', path: '/a/0' },
+        { op: 'add', path: '/a/-', value: 4 },
+      ],
+    });
+  });
+
+  it('should create a valid patch when removing objects and adding objects to the array while the length stays the same', () => {
+    testPatch({
+      prev: {
+        a: [
+          { b: 1, row: 'first' },
+          { b: 2, row: 'second' },
+          { b: 3, row: 'third' },
+        ],
+      },
+      next: {
+        a: [{ b: 2, row: 'second' }, { b: 3, row: 'third' }, { b: 4 }],
+      },
+      expected: [
+        {
+          op: 'test',
+          path: '/a',
+          value: [
+            { b: 1, row: 'first' },
+            { b: 2, row: 'second' },
+            { b: 3, row: 'third' },
+          ],
+        },
+        { op: 'remove', path: '/a/0' },
+        { op: 'add', path: '/a/-', value: { b: 4 } },
+      ],
+    });
+  });
+
+  it('should create multiple remove operations when removing multiple values the middle of an array', () => {
+    testPatch({
+      prev: { a: [0, 1, 2, 3, 4, 5, 6] },
+      next: { a: [0, 1, 5, 6] },
+      expected: [
+        { op: 'test', path: '/a', value: [0, 1, 2, 3, 4, 5, 6] },
+        { op: 'remove', path: '/a/4' },
+        { op: 'remove', path: '/a/3' },
+        { op: 'remove', path: '/a/2' },
+      ],
+    });
+  });
+
+  it('should prefer remove and add operations for a simple string in a large array', () => {
+    testPatch({
+      prev: { a: ['foo', 'bar', 'baz', 'qux'] },
+      next: { a: ['foo', 'bar2', 'baz', 'qux'] },
+      expected: [
+        { op: 'test', path: '/a', value: ['foo', 'bar', 'baz', 'qux'] },
+        { op: 'remove', path: '/a/1' },
+        { op: 'add', path: '/a/1', value: 'bar2' },
+      ],
+    });
+  });
+
+  it('should prefer a simple replace op for a number in a nested array', () => {
+    testPatch({
+      prev: { a: [{ b: [{ a: 5, b: 3, c: 8 }] }] },
+      next: { a: [{ b: [{ a: 5, b: 3, c: 9 }] }] },
+      expected: [
+        { op: 'test', path: '/a/0/b/0/c', value: 8 },
+        { op: 'replace', path: '/a/0/b/0/c', value: 9 },
+      ],
+    });
+  });
+
+  it('should create an add operation with a dash key when appending to an array', () => {
+    testPatch({
+      prev: { a: [1, 2, 3] },
+      next: { a: [1, 2, 3, 4] },
+      expected: [
+        { op: 'test', path: '/a', value: [1, 2, 3] },
+        { op: 'add', path: '/a/-', value: 4 },
+      ],
+    });
+  });
+
+  it('should create a minimal patch with replace operations when multiple values are changed inside nested arrays', () => {
+    const equalObject = { foo: 'bar', baz: 'qux' };
+    testPatch({
+      prev: { a: [{ b: [equalObject, { a: 5, b: 3, c: 8 }, equalObject, { d: 4, ...equalObject }] }] },
+      next: { a: [{ b: [equalObject, { a: 5, b: 3, c: 9 }, equalObject, { d: 5, ...equalObject }] }] },
+      expected: [
+        { op: 'test', path: '/a/0/b/1/c', value: 8 },
+        { op: 'replace', path: '/a/0/b/1/c', value: 9 },
+        { op: 'test', path: '/a/0/b/3/d', value: 4 },
+        { op: 'replace', path: '/a/0/b/3/d', value: 5 },
+      ],
+    });
+  });
+
+  it('should create a simple replace op for a string in a nested object', () => {
+    testPatch({
+      prev: { a: { b: 'foo' } },
+      next: { a: { b: 'bar' } },
+      expected: [
+        { op: 'test', path: '/a/b', value: 'foo' },
+        { op: 'replace', path: '/a/b', value: 'bar' },
+      ],
+    });
+  });
+
+  it('should create a simple replace when new value is null', () => {
+    testPatch({
+      prev: { a: { b: 'foo' } },
+      next: { a: { b: null } },
+      expected: [
+        { op: 'test', path: '/a/b', value: 'foo' },
+        { op: 'replace', path: '/a/b', value: null },
+      ],
+    });
+  });
+
+  it('should create a simple replace when old value is null', () => {
+    testPatch({
+      prev: { a: { b: null } },
+      next: { a: { b: 'foo' } },
+      expected: [
+        { op: 'test', path: '/a/b', value: null },
+        { op: 'replace', path: '/a/b', value: 'foo' },
+      ],
+    });
+  });
+});
---
+++ src/features/formData/jsonPatch/createPatch.ts
@@ -0,0 +1,180 @@
+import { getPatch } from 'fast-array-diff';
+import deepEqual from 'fast-deep-equal';
+
+import type { JsonPatch } from 'src/features/formData/jsonPatch/types';
+
+interface Props<T> {
+  prev: T;
+  next: T;
+}
+
+interface CompareProps<T> extends Props<T> {
+  patch: JsonPatch;
+  path: string[];
+  stats: Stats;
+}
+
+interface Stats {
+  comparisons: number;
+}
+
+/**
+ * This will create a JSON patch that can be used to update the previous object to the next object.
+ * It will look for the most shallow changes possible, and will always add a 'test' operation before
+ * changes that can be tested for. This should in theory also make the patch reversible, and will ensure
+ * that the patch can be applied to the previous object without overwriting any changes that may have
+ * been made to the previous object since the next object was created.
+ */
+export function createPatch({ prev, next }: Props<object>): JsonPatch {
+  const patch: JsonPatch = [];
+  if (!isObject(prev)) {
+    throw new Error('prev must be an object');
+  }
+  if (!isObject(next)) {
+    throw new Error('next must be an object');
+  }
+
+  const stats = newStats();
+  compareObjects({ prev, next, patch, path: [], stats });
+  return patch;
+}
+
+function newStats(): Stats {
+  return {
+    comparisons: 0,
+  };
+}
+
+function isObject(value: any): value is object {
+  return typeof value === 'object' && value !== null && !Array.isArray(value);
+}
+
+function isScalarOrNull(value: any): value is string | number | boolean | null {
+  return typeof value === 'string' || typeof value === 'number' || typeof value === 'boolean' || value === null;
+}
+
+function compareAny(props: CompareProps<any>) {
+  const { prev, next } = props;
+  if (isObject(prev) && isObject(next)) {
+    compareObjects(props);
+    return;
+  }
+  if (Array.isArray(prev) && Array.isArray(next)) {
+    compareArrays(props);
+    return;
+  }
+  compareValues(props);
+}
+
+function compareObjects({ prev, next, path, ...rest }: CompareProps<object>) {
+  const keys = new Set([...Object.keys(prev), ...Object.keys(next)]);
+  for (const key of keys) {
+    const prevValue = prev[key];
+    const nextValue = next[key];
+    compareAny({ prev: prevValue, next: nextValue, path: [...path, key], ...rest });
+  }
+}
+
+/**
+ * This comparison function is used to determine if two values in an array are similar enough to be considered
+ * the same. This is used to determine if an item has been removed or added - or if it has been (slightly) changed and
+ * the values within it should be compared individually instead.
+ */
+function isSimilarEnough(stats: Stats): (left: any, right: any) => boolean {
+  return (left, right) => {
+    stats.comparisons++;
+    if (isObject(left) && isObject(right)) {
+      const patch: JsonPatch = [];
+      const innerStats = newStats();
+      compareObjects({ prev: left, next: right, patch, path: [], stats: innerStats });
+      const actualChanges = patch.filter((p) => p.op !== 'test').length;
+      const potentialChanges = innerStats.comparisons;
+      if (potentialChanges === 0 || actualChanges === 0) {
+        return true;
+      }
+      const changedPercentage = actualChanges / potentialChanges;
+      return changedPercentage < 0.6;
+    }
+
+    if (Array.isArray(left) && Array.isArray(right)) {
+      return deepEqual(left, right);
+    }
+
+    return left === right;
+  };
+}
+
+/**
+ * When comparing arrays, some items may have been removed, some added, and some changed. In order to figure ou
+ * which operation to choose, we need to do something a bit more complex than the code in this file. This uses
+ * an LCS algorithm to figure out the longest common subsequence between the two arrays, and then uses that to
+ * produce the JsonPatch to create. Do not be fooled by the format returned from getPatch, it is not a JsonPatch
+ * even if it looks like it at a glance.
+ */
+function compareArrays({ prev, next, patch, path, stats }: CompareProps<any[]>) {
+  const diff = getPatch(prev, next, isSimilarEnough(stats));
+  const localPatch: JsonPatch = [];
+  const arrayAfterChanges = [...prev];
+  for (const part of diff) {
+    const { type, newPos: nextPos, oldPos: prevPos, items } = part;
+    if (type === 'add') {
+      let nextIndex = nextPos;
+      for (const item of items) {
+        const isAppend = nextIndex === arrayAfterChanges.length;
+        localPatch.push({ op: 'add', path: pointer([...path, isAppend ? '-' : String(nextIndex)]), value: item });
+        if (isAppend) {
+          arrayAfterChanges.push(item);
+        } else {
+          arrayAfterChanges.splice(nextIndex, 0, item);
+        }
+        nextIndex++;
+      }
+    } else if (type === 'remove') {
+      // We'll count down instead of up so that we can remove the items from the end first, and then we won't have to
+      // worry about the indices changing.
+      let addToIndex = items.length - 1;
+      for (const _item of items) {
+        const oldIdx = prevPos + addToIndex--;
+        localPatch.push({ op: 'remove', path: pointer([...path, String(oldIdx)]) });
+        arrayAfterChanges.splice(oldIdx, 1);
+      }
+    }
+  }
+
+  if (localPatch.length) {
+    // Always add a test first to make sure the original array is still the same as the one we're changing
+    patch.push({ op: 'test', path: pointer(path), value: prev });
+    patch.push(...localPatch);
+  }
+
+  // We still have to compare items within the array, as the code above just checks if items within
+  // are similar enough, it doesn't check that they're entirely equal.
+  const childPatches: JsonPatch = [];
+  for (const [index, prevItem] of arrayAfterChanges.entries()) {
+    const nextItem = next[index];
+    compareAny({ prev: prevItem, next: nextItem, patch: childPatches, path: [...path, String(index)], stats });
+  }
+  patch.push(...childPatches);
+}
+
+function compareValues({ prev, next, patch, path, stats }: CompareProps<any>) {
+  stats.comparisons++;
+  if (prev === next) {
+    return;
+  }
+  if (next === undefined) {
+    patch.push({ op: 'test', path: pointer(path), value: prev });
+    patch.push({ op: 'remove', path: pointer(path) });
+  } else if (prev === undefined) {
+    patch.push({ op: 'add', path: pointer(path), value: next });
+  } else if (isScalarOrNull(prev) && isScalarOrNull(next)) {
+    patch.push({ op: 'test', path: pointer(path), value: prev });
+    patch.push({ op: 'replace', path: pointer(path), value: next });
+  } else {
+    throw new Error(`Unexpected values: ${JSON.stringify(prev)} and ${JSON.stringify(next)}`);
+  }
+}
+
+function pointer(path: string[]) {
+  return `/${path.join('/')}`;
+}
