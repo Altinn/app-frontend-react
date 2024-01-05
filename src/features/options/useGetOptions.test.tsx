dbb645e95c45595948e593d0860d0f3f9d08d121
Ole Martin Handeland <git@olemartin.org>
2024-01-05 12:31:46 +0100

    Extending tests to make sure the setData call works as expected, as well as the 'current' and 'currentStringy' return values

--- src/features/options/useGetOptions.test.tsx
+++ src/features/options/useGetOptions.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
 import type { AxiosResponse } from 'axios';
 
 import { useGetOptions } from 'src/features/options/useGetOptions';
@@ -24,12 +25,26 @@ interface RenderProps {
 }
 
 function TestOptions({ node }: { node: LayoutNode<'Dropdown' | 'MultipleSelect'> }) {
-  const { options } = useGetOptions({
+  const { options, setData, current, currentStringy } = useGetOptions({
     ...node.item,
     node,
     valueType: node.item.type === 'Dropdown' ? 'single' : 'multi',
   });
-  return <div data-testid='options'>{JSON.stringify(options)}</div>;
+
+  const setterFor = (index: number) => () =>
+    (setData as any)(node.item.type === 'Dropdown' ? options[index] : [options[index]]);
+
+  return (
+    <>
+      <div data-testid='options'>{JSON.stringify(options)}</div>
+      <div data-testid='current'>{JSON.stringify(current)}</div>
+      <div data-testid='currentStringy'>{JSON.stringify(currentStringy)}</div>
+      <button onClick={setterFor(0)}>Choose first option</button>
+      <button onClick={setterFor(1)}>Choose second option</button>
+      <button onClick={setterFor(2)}>Choose third option</button>
+      <button onClick={setterFor(3)}>Choose fourth option</button>
+    </>
+  );
 }
 
 async function render(props: RenderProps) {
@@ -109,14 +124,15 @@ describe('useGetOptions', () => {
   ];
 
   it.each(permutations)('options should be cast to strings for $type + $via', async (props) => {
-    await render({
+    const options = [
+      { label: 'first', value: 'hello' },
+      { label: 'second', value: false },
+      { label: 'third', value: 2 },
+      { label: 'fourth', value: 3.14 },
+    ];
+    const { formDataMethods } = await render({
       ...props,
-      options: [
-        { label: 'first', value: 'hello' },
-        { label: 'second', value: false },
-        { label: 'third', value: 2 },
-        { label: 'fourth', value: 3.14 },
-      ],
+      options,
     });
 
     const textContent = screen.getByTestId('options').textContent;
@@ -128,5 +144,37 @@ describe('useGetOptions', () => {
       { label: 'third', value: '2' },
       { label: 'fourth', value: '3.14' },
     ]);
+
+    // Try setting the value to all the options, and observing that the saved value is the stringy version
+    for (const option of options) {
+      await userEvent.click(screen.getByRole('button', { name: `Choose ${option.label} option` }));
+      expect(formDataMethods.setLeafValue).toHaveBeenCalledWith({
+        path: 'result',
+        newValue: option.value.toString(),
+      });
+      (formDataMethods.setLeafValue as jest.Mock).mockClear();
+
+      const currentOption = JSON.parse(screen.getByTestId('current').textContent as string);
+      if (props.type === 'single') {
+        expect(currentOption).toEqual({
+          label: option.label,
+          value: option.value.toString(),
+        });
+      } else {
+        expect(currentOption).toEqual([
+          {
+            label: option.label,
+            value: option.value.toString(),
+          },
+        ]);
+      }
+
+      const currentStringy = JSON.parse(screen.getByTestId('currentStringy').textContent as string);
+      if (props.type === 'single') {
+        expect(currentStringy).toEqual(option.value.toString());
+      } else {
+        expect(currentStringy).toEqual([option.value.toString()]);
+      }
+    }
   });
 });
