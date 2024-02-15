b5636ad0925577dd194b46997e03bb3bbddf7248
Ole Martin Handeland <git@olemartin.org>
2024-02-15 11:08:08 +0100

    Making props an object

--- src/utils/versionCompare.ts
+++ src/utils/versionCompare.ts
@@ -1,3 +1,9 @@
+interface VersionCompareProps {
+  actualVersion: string;
+  minimumVersion: string;
+  allowZeroInLast?: boolean;
+}
+
 /**
  * Checks if the given version is at least the given minimum version. Expects the version numbers to be
  * dot-separated numbers, e.g. "1.0.15". String parts, such as 'preview', 'alpha', 'rc' are not supported.
@@ -5,9 +11,9 @@
  * The allowZeroInLast parameter is used to allow the last part of the version to be zero. This is useful
  * when running the backend with project references, as the build number is set to zero in that case.
  */
-export function isAtLeastVersion(version: string, minVersion: string, allowZeroInLast = false): boolean {
-  const parts = version.split('.');
-  const expectedParts = minVersion.split('.');
+export function isAtLeastVersion({ actualVersion, minimumVersion, allowZeroInLast }: VersionCompareProps): boolean {
+  const parts = actualVersion.split('.');
+  const expectedParts = minimumVersion.split('.');
   if (parts.length !== expectedParts.length) {
     return false;
   }
