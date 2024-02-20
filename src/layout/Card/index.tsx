fe82bc22da732031bb230b0da02835487a5fcfe4
walldenfilippa <filippa.wallden@avanade.com>
2024-02-20 16:09:08 +0100

    card reference

--- src/layout/Card/index.tsx
+++ src/layout/Card/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 
 import { Card as CardComponent } from 'src/layout/Card/Card';
 import { CardDef } from 'src/layout/Card/config.def.generated';
 import type { PropsFromGenericComponent } from 'src/layout';
 
 export class Card extends CardDef {
-  render(props: PropsFromGenericComponent<'Card'>): React.JSX.Element | null {
-    return <CardComponent {...props} />;
-  }
+  render = forwardRef<HTMLElement, PropsFromGenericComponent<'Card'>>(
+    function LayoutComponentCardRender(props, _): React.JSX.Element | null {
+      return <CardComponent {...props} />;
+    },
+  );
 }
