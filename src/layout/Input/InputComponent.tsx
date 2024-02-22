cbef9708d5bd3926f7f0ac68e9186a76f40e7c5d
Ole Martin Handeland <git@olemartin.org>
2024-02-22 11:33:21 +0100

    Removing useRerender()-hack that worked around issues when saving large numbers in number-formatted fields. This should now have been implemented in the formData system, with the 'invalidCurrentData' state instead.

--- src/layout/Input/InputComponent.tsx
+++ src/layout/Input/InputComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { NumericFormat, PatternFormat } from 'react-number-format';
 
 import { SearchField } from '@altinn/altinn-design-system';
@@ -7,7 +7,6 @@ import { Textfield } from '@digdir/design-system-react';
 import { useDataModelBindings } from 'src/features/formData/useDataModelBindings';
 import { useLanguage } from 'src/features/language/useLanguage';
 import { useMapToReactNumberConfig } from 'src/hooks/useMapToReactNumberConfig';
-import { useRerender } from 'src/hooks/useReload';
 import classes from 'src/layout/Input/InputComponent.module.css';
 import { isNumericFormat, isPatternFormat } from 'src/layout/Input/number-format-helpers';
 import { useCharacterLimit } from 'src/utils/inputUtils';
@@ -40,14 +39,6 @@ export const InputComponent: React.FunctionComponent<IInputProps> = ({ node, isV
 
   const reactNumberFormatConfig = useMapToReactNumberConfig(formatting as IInputFormatting | undefined, formValue);
   const ariaLabel = overrideDisplay?.renderedInTable === true ? langAsString(textResourceBindings?.title) : undefined;
-  const [inputKey, rerenderInput] = useRerender('input');
-
-  const onBlur = useCallback(() => {
-    if (reactNumberFormatConfig.number) {
-      rerenderInput();
-    }
-    debounce();
-  }, [debounce, reactNumberFormatConfig.number, rerenderInput]);
 
   const characterLimit = useCharacterLimit(maxLength);
 
@@ -62,20 +53,20 @@ export const InputComponent: React.FunctionComponent<IInputProps> = ({ node, isV
     readOnly,
     isValid,
     required,
+    onBlur: debounce,
   };
 
   if (variant === 'search') {
     return (
       <SearchField
         id={id}
-        key={inputKey}
         value={formValue}
         onChange={(e: React.ChangeEvent<HTMLInputElement>) => setValue('simpleBinding', e.target.value)}
-        onBlur={onBlur}
         disabled={readOnly}
         aria-label={ariaLabel}
         aria-describedby={textResourceBindings?.description ? `description-${id}` : undefined}
         data-testid={`${id}-${variant}`}
+        onBlur={debounce}
       />
     );
   }
@@ -83,7 +74,6 @@ export const InputComponent: React.FunctionComponent<IInputProps> = ({ node, isV
   if (!reactNumberFormatConfig?.number) {
     return (
       <Textfield
-        key={inputKey}
         value={formValue}
         onChange={(event) => {
           setValue('simpleBinding', event.target.value);
@@ -97,7 +87,6 @@ export const InputComponent: React.FunctionComponent<IInputProps> = ({ node, isV
   if (isPatternFormat(reactNumberFormatConfig.number)) {
     return (
       <PatternFormat
-        key={inputKey}
         value={formValue}
         onValueChange={(values, sourceInfo) => {
           setValue('simpleBinding', values.value);
@@ -113,12 +102,9 @@ export const InputComponent: React.FunctionComponent<IInputProps> = ({ node, isV
   if (isNumericFormat(reactNumberFormatConfig.number)) {
     return (
       <NumericFormat
-        key={inputKey}
         value={formValue}
         onValueChange={(values, sourceInfo) => {
-          if (values.value !== formValue) {
-            setValue('simpleBinding', values.value);
-          }
+          setValue('simpleBinding', values.value);
         }}
         onPaste={(event) => {
           /* This is a workaround for a react-number-format bug that
