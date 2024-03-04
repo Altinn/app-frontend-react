fe9a0dbbcd67b501c53a53de5638d2ef70dba142
Ole Martin Handeland <git@olemartin.org>
2024-03-04 18:29:23 +0100

    In the rush to rewrite to zustand, I entirely forgot to actually implement the AttachmentsProvider properly. This way it should not cause re-renders, as none of the state-manipulating hooks wraps the app rendering pipeline.

--- src/features/attachments/AttachmentsContext.tsx
+++ src/features/attachments/AttachmentsContext.tsx
@@ -1,11 +1,12 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import type { PropsWithChildren } from 'react';
 
 import deepEqual from 'fast-deep-equal';
 import { createStore } from 'zustand';
 
-import { ContextNotProvided, createContext } from 'src/core/contexts/context';
+import { ContextNotProvided } from 'src/core/contexts/context';
 import { createZustandContext } from 'src/core/contexts/zustandContext';
+import { Loader } from 'src/core/loading/Loader';
 import { usePostUpload } from 'src/features/attachments/utils/postUpload';
 import { usePreUpload } from 'src/features/attachments/utils/preUpload';
 import { mergeAndSort } from 'src/features/attachments/utils/sorting';
@@ -20,7 +21,7 @@ import type {
 import type { IData } from 'src/types/shared';
 import type { LayoutNode } from 'src/utils/layout/LayoutNode';
 
-interface IAttachmentsMethodsCtx {
+interface IAttachmentsMethods {
   upload(action: RawAttachmentAction<AttachmentActionUpload>): Promise<string | undefined>;
   update(action: RawAttachmentAction<AttachmentActionUpdate>): Promise<void>;
   remove(action: RawAttachmentAction<AttachmentActionRemove>): Promise<boolean>;
@@ -30,11 +31,14 @@ interface IAttachmentsMethodsCtx {
 interface IAttachmentsStoreCtx {
   attachments: IAttachments;
   setAttachments: (newAttachments: IAttachments) => void;
+  methods: Partial<IAttachmentsMethods>;
+  setMethods: (newMethods: IAttachmentsMethods) => void;
 }
 
 function initialCreateStore() {
   return createStore<IAttachmentsStoreCtx>((set) => ({
     attachments: {},
+    methods: {},
     setAttachments: (attachments) =>
       set((state) => {
         if (deepEqual(state.attachments, attachments)) {
@@ -42,22 +46,14 @@ function initialCreateStore() {
         }
         return { attachments };
       }),
+    setMethods: (methods) => set({ methods }),
   }));
 }
 
-const {
-  Provider: StoreProvider,
-  useSelector,
-  useLaxMemoSelector,
-} = createZustandContext({
-  name: 'AttachmentsStore',
+const { Provider, useSelector, useLaxMemoSelector } = createZustandContext({
+  name: 'Attachments',
   required: true,
   initialCreateStore,
-});
-
-const { Provider: MethodsProvider, useCtx: useMethodsCtx } = createContext<IAttachmentsMethodsCtx>({
-  name: 'AttachmentsMethods',
-  required: true,
 });
 
 /**
@@ -68,33 +64,22 @@ const { Provider: MethodsProvider, useCtx: useMethodsCtx } = createContext<IAtta
  *   NodesProvider. The node hierarchy needs to know about the attachments, but the cyclical dependency between
  *   AttachmentsProvider and NodesProvider makes it impossible to do this in a single provider.
  */
-export const AttachmentsProvider = ({ children }: PropsWithChildren) => {
-  const setAttachments = useSelector((store) => store.setAttachments);
-  const { state: preUpload, upload, awaitUpload } = usePreUpload();
-  const { state: postUpload, update, remove } = usePostUpload();
+export const AttachmentsProvider = ({ children }: PropsWithChildren) => (
+  <>
+    <ProvideAttachmentsAndMethods />
+    <LoadingUntilMethodsProvided>{children}</LoadingUntilMethodsProvided>
+  </>
+);
 
-  const attachments = useMemo(() => mergeAndSort(preUpload, postUpload), [preUpload, postUpload]);
+export const AttachmentsStoreProvider = ({ children }: PropsWithChildren) => (
+  <Provider>
+    {window.Cypress && <UpdateAttachmentsForCypress />}
+    {children}
+  </Provider>
+);
 
-  useEffect(() => {
-    setAttachments(attachments);
-  }, [attachments, setAttachments]);
-
-  return (
-    <MethodsProvider
-      value={{
-        upload,
-        update,
-        remove,
-        awaitUpload,
-      }}
-    >
-      {children}
-    </MethodsProvider>
-  );
-};
-
-export const AttachmentsStoreProvider = ({ children }: PropsWithChildren) => {
-  const [attachments, setAttachments] = useState<IAttachments>({});
+function UpdateAttachmentsForCypress() {
+  const attachments = useAttachments();
 
   useEffect(() => {
     if (window.Cypress) {
@@ -102,23 +87,53 @@ export const AttachmentsStoreProvider = ({ children }: PropsWithChildren) => {
     }
   }, [attachments]);
 
-  return (
-    <StoreProvider
-      value={{
-        attachments,
-        setAttachments,
-      }}
-    >
-      {children}
-    </StoreProvider>
-  );
-};
+  return null;
+}
 
-export const useAttachments = () => useSelector((store) => store.attachments);
-export const useAttachmentsUploader = () => useMethodsCtx().upload;
-export const useAttachmentsUpdater = () => useMethodsCtx().update;
-export const useAttachmentsRemover = () => useMethodsCtx().remove;
-export const useAttachmentsAwaiter = () => useMethodsCtx().awaitUpload;
+function ProvideAttachmentsAndMethods() {
+  const setAttachments = useSelector((store) => store.setAttachments);
+  const setMethods = useSelector((store) => store.setMethods);
+  const { state: preUpload, upload, awaitUpload } = usePreUpload();
+  const { state: postUpload, update, remove } = usePostUpload();
+
+  const attachments = useMemo(() => mergeAndSort(preUpload, postUpload), [preUpload, postUpload]);
+
+  const methods = useMemo(
+    () => ({
+      upload,
+      update,
+      remove,
+      awaitUpload,
+    }),
+    [upload, update, remove, awaitUpload],
+  );
+
+  useEffect(() => {
+    setAttachments(attachments);
+  }, [attachments, setAttachments]);
+
+  useEffect(() => {
+    setMethods(methods);
+  }, [methods, setMethods]);
+
+  return null;
+}
+
+function LoadingUntilMethodsProvided({ children }: PropsWithChildren) {
+  const methods = useSelector((store) => store.methods);
+
+  if (Object.keys(methods).length === 0) {
+    return <Loader reason='attachments' />;
+  }
+
+  return children;
+}
+
+export const useAttachments = () => useSelector((state) => state.attachments);
+export const useAttachmentsUploader = () => useSelector((state) => state.methods.upload!);
+export const useAttachmentsUpdater = () => useSelector((state) => state.methods.update!);
+export const useAttachmentsRemover = () => useSelector((state) => state.methods.remove!);
+export const useAttachmentsAwaiter = () => useSelector((state) => state.methods.awaitUpload!);
 
 const emptyArray = [];
 export const useAttachmentsFor = (node: LayoutNode<'FileUploadWithTag' | 'FileUpload'>) =>
