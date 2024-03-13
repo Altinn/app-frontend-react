e522deeceac86d00859683dc328b044a53ed0d71
Adam Haeger <adamgullerud@gmail.com>
2024-03-13 08:44:26 +0100

    chore:wip

--- src/features/payment/Payment.tsx
+++ src/features/payment/Payment.tsx
@@ -3,13 +3,11 @@ import React from 'react';
 import { Button } from '@digdir/design-system-react';
 import { useQuery } from '@tanstack/react-query';
 
-import { useLaxInstanceData } from 'src/features/instance/InstanceContext';
 import { ConfirmButton } from 'src/features/processEnd/confirm/components/ConfirmButton';
 import { useInstanceIdParams } from 'src/hooks/useInstanceIdParams';
 import { fetchPaymentInfo } from 'src/queries/queries';
 export const Payment: React.FunctionComponent = () => {
   const { partyId, instanceGuid } = useInstanceIdParams();
-  const instanceId = useLaxInstanceData()?.id;
   const paymentInfoQuery = useQuery({
     queryKey: ['fetchPaymentInfo', partyId, instanceGuid],
     queryFn: () => {
@@ -22,14 +20,11 @@ export const Payment: React.FunctionComponent = () => {
 
   return (
     <div>
-      <h1>welcome to payment</h1>
-      {`instanceGuid: ${instanceGuid}`}
-      <br />
-      {`instanceId: ${instanceId}`}
-      <ul>
-        <li>fetch payment info from api</li>
-        <li>View the info</li>
-      </ul>
+      {paymentInfoQuery.data?.orderDetails?.orderLines?.length && (
+        <ul>
+          {paymentInfoQuery.data?.orderDetails.orderLines.map((orderLine, idx) => <li key={idx}>{orderLine.name}</li>)}
+        </ul>
+      )}
 
       {paymentInfoQuery.isFetched && paymentInfoQuery.data?.status === 'Paid' && <h1>You have paid!</h1>}
       {paymentInfoQuery.isFetched && paymentInfoQuery.data?.status !== 'Paid' && (
