c5533559ad688752caf74671d0de7e1198dc644c
Magnus Revheim Martinsen <mrmartinsen.96@gmail.com>
2024-03-25 15:36:57 +0100

    add mapping

--- src/layout/PaymentDetails/PaymentDetailsComponent.tsx
+++ src/layout/PaymentDetails/PaymentDetailsComponent.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { useQuery } from '@tanstack/react-query';
 
+import { FD } from 'src/features/formData/FormDataWrite';
 import { useInstanceIdParams } from 'src/hooks/useInstanceIdParams';
 import { PaymentDetailsTable } from 'src/layout/PaymentDetails/PaymentDetailsTable';
 import { fetchOrderDetails } from 'src/queries/queries';
@@ -12,9 +13,20 @@ export type IPaymentDetailsProps = PropsFromGenericComponent<'PaymentDetails'>;
 export function PaymentDetailsComponent({ node }: IPaymentDetailsProps) {
   const { partyId, instanceGuid } = useInstanceIdParams();
   const { title, description } = node.item.textResourceBindings || {};
+  /*   const updateOrderDetailsWhenChanged = node.item?.updateOrderDetailsWhenChanged;
 
-  const useOrderDetailsQuery = useQuery({
-    queryKey: ['fetchOrderDetails', partyId, instanceGuid],
+  const testNode = useResolvedNode<IPaymentDetailsProps>(node.item.id);
+  const updateWhenChanged = testNode?.item.updateOrderDetailsWhenChanged;
+
+  console.log('updateOrderDetailsWhenChanged', updateWhenChanged); */
+
+  const mappedValues = FD.useMapping(node.item.mapping);
+
+  const test = JSON.stringify(mappedValues);
+  console.log('mappedValues', test);
+
+  const { data: useOrderDetailsQuery } = useQuery({
+    queryKey: ['fetchOrderDetails', partyId, instanceGuid, test],
     queryFn: () => {
       if (partyId) {
         return fetchOrderDetails(partyId, instanceGuid);
@@ -25,7 +37,7 @@ export function PaymentDetailsComponent({ node }: IPaymentDetailsProps) {
 
   return (
     <PaymentDetailsTable
-      orderDetails={useOrderDetailsQuery.data}
+      orderDetails={useOrderDetailsQuery}
       title={title}
       description={description}
     />
