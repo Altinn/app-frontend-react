bb8456ab1f62dc8b97d51398af099f1fc8e865c8
Ole Martin Handeland <git@olemartin.org>
2024-05-13 10:50:04 +0200

    Adding missing config for Payment components

--- src/layout/PaymentDetails/config.ts
+++ src/layout/PaymentDetails/config.ts
@@ -1,15 +1,20 @@
 import { CG } from 'src/codegen/CG';
+import { LabelRendering } from 'src/codegen/Config';
 import { CompCategory } from 'src/layout/common';
 
 export const Config = new CG.component({
   category: CompCategory.Presentation,
-  rendersWithLabel: false,
+  rendersWithLabel: LabelRendering.Off,
   capabilities: {
     renderInTable: false,
     renderInButtonGroup: false,
     renderInAccordion: false,
     renderInAccordionGroup: false,
   },
+  functionality: {
+    customExpressions: false,
+  },
+  directRendering: false,
 })
   .addTextResource(
     new CG.trb({
