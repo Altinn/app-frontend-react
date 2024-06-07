f32887ab95b3ee12d90fc7c8d5efd7f87ea38e48
Magnus Revheim Martinsen <mrmartinsen.96@gmail.com>
2024-06-07 08:56:23 +0200

    add validation message to input summary

--- src/layout/Input/InputComponentSummary.tsx
+++ src/layout/Input/InputComponentSummary.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-import { Label, Paragraph } from '@digdir/designsystemet-react';
+import { ErrorMessage, Label, Paragraph } from '@digdir/designsystemet-react';
+import cn from 'classnames';
 
 import { Lang } from 'src/features/language/Lang';
+import { useUnifiedValidationsForNode } from 'src/features/validation/selectors/unifiedValidationsForNode';
+import { validationsOfSeverity } from 'src/features/validation/utils';
 import classes from 'src/layout/Input/InputComponentSummary.module.css';
 import { EditButton } from 'src/layout/Summary2/EditButton';
 import type { CompInputInternal } from 'src/layout/Input/config.generated';
@@ -15,10 +18,11 @@ type InputComponentSummaryProps = {
 };
 
 export const InputComponentSummary = ({ componentNode, summaryOverrides, displayData }: InputComponentSummaryProps) => {
+  const validations = useUnifiedValidationsForNode(componentNode);
+  const errors = validationsOfSeverity(validations, 'error');
   if (summaryOverrides?.hidden) {
     return <h1>Im so hidden!!!</h1>;
   }
-  const value = <Lang id={'general.empty_summary'}></Lang>;
 
   const { textResourceBindings } = componentNode.item;
   return (
@@ -36,7 +40,7 @@ export const InputComponentSummary = ({ componentNode, summaryOverrides, display
       {displayData && (
         <Paragraph
           asChild
-          className={classes.formValue}
+          className={cn(classes.formValue, { [classes.error]: errors.length > 0 })}
         >
           <span>{displayData}</span>
         </Paragraph>
@@ -44,11 +48,21 @@ export const InputComponentSummary = ({ componentNode, summaryOverrides, display
       {!displayData && (
         <Paragraph
           asChild
-          className={classes.emptyValue}
+          className={cn(classes.emptyValue, { [classes.error]: errors.length > 0 })}
         >
-          <span>{value}</span>
+          <span>{<Lang id={'general.empty_summary'}></Lang>}</span>
         </Paragraph>
       )}
+      {errors.length > 0 &&
+        errors.map(({ message }) => (
+          <ErrorMessage key={message.key}>
+            <Lang
+              id={message.key}
+              params={message.params}
+              node={componentNode}
+            ></Lang>
+          </ErrorMessage>
+        ))}
     </div>
   );
 };
