4b9ab7b1c2d1d353991d787553f4035cf6af88af
Bjørnar Østtveit <bjosttveit@gmail.com>
2024-06-10 08:59:07 +0200

    upgrade and remove workaround

--- src/features/options/useGetOptions.ts
+++ src/features/options/useGetOptions.ts
@@ -215,20 +215,11 @@ export function useGetOptions(props: Props): OptionsResult {
   usePreselectedOptionIndex(effectProps);
   useRemoveStaleValues(effectProps);
 
-  // This is a workaround for https://github.com/digdir/designsystemet/issues/2081
-  // Pls remove this ASAP
-  const wrappedSelectedValues = useMemo(() => selectedValues.map((v) => `_value_${v}`), [selectedValues]);
-  const wrappedSetData = useCallback((values: string[]) => setData(values.map((v) => v.substring(7))), [setData]);
-  const wrappedOptions = useMemo(
-    () => alwaysOptions.map((o) => ({ ...o, value: `_value_${o.value}` })),
-    [alwaysOptions],
-  );
-
   return {
     rawData: value,
-    selectedValues: wrappedSelectedValues,
-    setData: wrappedSetData,
-    options: wrappedOptions,
+    selectedValues,
+    setData,
+    options: alwaysOptions,
     isFetching: isFetching || !calculatedOptions,
     isError,
   };
