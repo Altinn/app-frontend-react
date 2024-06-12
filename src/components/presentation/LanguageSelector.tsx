6b17fe4e9c3c9e89d1d9ee2e96214a51512acce3
Bjørnar Østtveit <bjosttveit@gmail.com>
2024-06-12 12:38:30 +0200

    upgrade language selector

--- src/components/presentation/LanguageSelector.tsx
+++ src/components/presentation/LanguageSelector.tsx
@@ -1,8 +1,10 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 
-import { LegacySelect } from '@digdir/design-system-react';
+import { Combobox } from '@digdir/designsystemet-react';
 
 import { AltinnSpinner } from 'src/components/AltinnSpinner';
+import classes from 'src/components/presentation/LanguageSelector.module.css';
+import { Lang } from 'src/features/language/Lang';
 import { useCurrentLanguage, useSetCurrentLanguage } from 'src/features/language/LanguageProvider';
 import { useGetAppLanguageQuery } from 'src/features/language/textResources/useGetAppLanguagesQuery';
 import { useLanguage } from 'src/features/language/useLanguage';
@@ -14,18 +16,12 @@ export const LanguageSelector = ({ hideLabel }: { hideLabel?: boolean }) => {
 
   const { data: appLanguages, isError: appLanguageError } = useGetAppLanguageQuery();
 
-  const handleAppLanguageChange = (languageCode: string) => {
-    setWithLanguageSelector(languageCode);
+  const handleAppLanguageChange = (values: string[]) => {
+    const lang = values.at(0);
+    if (lang) {
+      setWithLanguageSelector(lang);
+    }
   };
-
-  const optionsMap = useMemo(
-    () =>
-      appLanguages?.map((lang) => ({
-        label: langAsString(`language.full_name.${lang.language}`),
-        value: lang.language,
-      })),
-    [appLanguages, langAsString],
-  );
 
   if (appLanguageError) {
     console.error('Failed to load app languages.');
@@ -34,14 +30,24 @@ export const LanguageSelector = ({ hideLabel }: { hideLabel?: boolean }) => {
 
   if (appLanguages) {
     return (
-      <div style={{ minWidth: 160 }}>
-        <LegacySelect
-          label={!hideLabel ? langAsString('language.selector.label') : undefined}
-          options={optionsMap || []}
-          onChange={(value) => handleAppLanguageChange(value)}
-          value={selectedLanguage}
-        />
-      </div>
+      <Combobox
+        size='sm'
+        hideLabel={hideLabel}
+        label={langAsString('language.selector.label')}
+        onValueChange={handleAppLanguageChange}
+        value={[selectedLanguage]}
+        className={classes.container}
+      >
+        {appLanguages?.map((lang) => (
+          <Combobox.Option
+            key={lang.language}
+            value={lang.language}
+            displayValue={langAsString(`language.full_name.${lang.language}`)}
+          >
+            <Lang id={`language.full_name.${lang.language}`} />
+          </Combobox.Option>
+        ))}
+      </Combobox>
     );
   }
 
