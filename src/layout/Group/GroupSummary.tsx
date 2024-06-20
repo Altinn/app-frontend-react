36a51f7b0a4f6b90ab13cfb1919c70ab146de2e5
Benjamin Dehli <superelg@gmail.com>
2024-06-20 15:25:20 +0200

    Move componentSummary logic for groups to GroupSummary component

--- src/layout/Group/GroupSummary.tsx
+++ src/layout/Group/GroupSummary.tsx
@@ -2,26 +2,75 @@ import React from 'react';
 
 import { Heading, Paragraph } from '@digdir/designsystemet-react';
 import cn from 'classnames';
+import type { HeadingProps } from '@digdir/designsystemet-react';
 
 import classes from 'src/layout/Group/GroupSummary.module.css';
+import { ComponentSummary } from 'src/layout/Summary2/SummaryComponent2/ComponentSummary';
+import type { CompGroupInternal } from 'src/layout/Group/config.generated';
 import type { LayoutNode } from 'src/utils/layout/LayoutNode';
 
 type GroupComponentSummaryProps = {
   componentNode: LayoutNode<'Group'>;
-  childComponents: JSX.Element[] | null;
   hierarchyLevel?: number;
+  summaryOverrides?: CompGroupInternal['summaryProps'];
 };
 
-export const GroupSummary = ({ componentNode, childComponents, hierarchyLevel = 0 }: GroupComponentSummaryProps) => {
+type HeadingLevel = HeadingProps['level'];
+
+function getHeadingLevel(hierarchyLevel: number): HeadingLevel {
+  const minimumHeadingLevel = 3;
+  const maximumHeadingLevel = 6;
+  const computedHeadingLevel = minimumHeadingLevel + hierarchyLevel;
+  if (computedHeadingLevel <= maximumHeadingLevel) {
+    return computedHeadingLevel as HeadingLevel;
+  }
+  if (computedHeadingLevel > maximumHeadingLevel) {
+    return maximumHeadingLevel;
+  }
+}
+
+function renderGroupComponentSummary(componentNode: LayoutNode<'Group'>, hierarchyLevel = 0) {
+  return (
+    <GroupSummary
+      componentNode={componentNode}
+      hierarchyLevel={hierarchyLevel + 1}
+      key={componentNode.item.id}
+    />
+  );
+}
+
+function renderChildComponents(componentNode: LayoutNode, hierarchyLevel = 0, summaryOverrides) {
+  if ('childComponents' in componentNode.item) {
+    return (
+      !!componentNode?.item?.childComponents?.length &&
+      componentNode.item.childComponents.map((child) => {
+        if (child?.item?.type === 'Group') {
+          return renderGroupComponentSummary(child as LayoutNode<'Group'>, hierarchyLevel);
+        } else {
+          return (
+            <ComponentSummary
+              key={child?.item?.id}
+              componentNode={child}
+              summaryOverrides={summaryOverrides}
+            />
+          );
+        }
+      })
+    );
+  }
+}
+
+export const GroupSummary = ({ componentNode, hierarchyLevel = 0, summaryOverrides }: GroupComponentSummaryProps) => {
   const title = componentNode.item.textResourceBindings?.title;
   const description = componentNode.item.textResourceBindings?.description;
-  const headingLevel = 3 + hierarchyLevel <= 6 ? 3 + hierarchyLevel : 6;
-  const isNestedGroup = hierarchyLevel > 0;
+  const headingLevel = getHeadingLevel(hierarchyLevel);
+  const isGroup = componentNode.item.type === 'Group';
+  const isNestedGroup = isGroup && hierarchyLevel > 0;
   return (
     <section className={isNestedGroup ? cn(classes.groupContainer, classes.nested) : cn(classes.groupContainer)}>
       <Heading
         size='small'
-        level={headingLevel as 3 | 4 | 5 | 6}
+        level={headingLevel}
       >
         {title}
       </Heading>
@@ -31,7 +80,7 @@ export const GroupSummary = ({ componentNode, childComponents, hierarchyLevel = 
       >
         {description}
       </Paragraph>
-      {childComponents}
+      {renderChildComponents(componentNode, hierarchyLevel, summaryOverrides)}
     </section>
   );
 };
--- src/layout/Summary2/SummaryComponent2/ComponentSummary.tsx
+++ src/layout/Summary2/SummaryComponent2/ComponentSummary.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 import cn from 'classnames';
 
-import { GroupSummary } from 'src/layout/Group/GroupSummary';
 import classes from 'src/layout/Summary2/SummaryComponent2/SummaryComponent2.module.css';
 import { gridBreakpoints, pageBreakStyles } from 'src/utils/formComponentUtils';
 import { useNode } from 'src/utils/layout/NodesContext';
@@ -21,23 +20,12 @@ interface ResolveComponentProps {
   summaryOverrides: any;
 }
 
-export function ComponentSummary({ componentNode, summaryOverrides, hierarchyLevel = 0 }: ComponentSummaryProps) {
+export function ComponentSummary({ componentNode, summaryOverrides }: ComponentSummaryProps) {
   if (componentNode.isHidden()) {
     return null;
   }
 
   const overrides = summaryOverrides?.find((override) => override.componentId === componentNode.item.id);
-
-  const childComponents =
-    componentNode.item.type === 'Group' &&
-    componentNode.item.childComponents.map((child) => (
-      <ComponentSummary
-        componentNode={child}
-        hierarchyLevel={hierarchyLevel + 1}
-        key={child.item.id}
-        summaryOverrides={summaryOverrides}
-      />
-    ));
 
   const renderedComponent = componentNode.def.renderSummary2
     ? componentNode.def.renderSummary2(componentNode as LayoutNode<any>, overrides)
@@ -47,8 +35,6 @@ export function ComponentSummary({ componentNode, summaryOverrides, hierarchyLev
     return null;
   }
 
-  const isGroup = componentNode.item.type === 'Group';
-
   return (
     <Grid
       item={true}
@@ -56,13 +42,6 @@ export function ComponentSummary({ componentNode, summaryOverrides, hierarchyLev
       {...gridBreakpoints(componentNode.item.grid)}
     >
       {renderedComponent}
-      {isGroup && (
-        <GroupSummary
-          componentNode={componentNode as LayoutNode<'Group'>}
-          hierarchyLevel={hierarchyLevel}
-          childComponents={childComponents || null}
-        />
-      )}
     </Grid>
   );
 }
