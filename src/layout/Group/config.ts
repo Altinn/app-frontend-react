6a29aba0282ae10495028f946ef9a08438442f1b
adamhaeger <adamgullerud@gmail.com>
2024-06-21 11:26:23 +0200

    removed config prop that is now common

--- src/layout/Group/config.ts
+++ src/layout/Group/config.ts
@@ -1,15 +1,7 @@
 import { CG, Variant } from 'src/codegen/CG';
 import { CompCategory } from 'src/layout/common';
 
-export const GROUP_SUMMARY_PROPS = new CG.obj(
-  new CG.prop(
-    'hidden',
-    new CG.bool()
-      .optional()
-      .setTitle('Hidden')
-      .setDescription('Boolean value indicating if the component should be hidden in the summary'),
-  ),
-)
+export const GROUP_SUMMARY_PROPS = new CG.obj()
   .extends(CG.common('ISummaryOverridesCommon'))
   .optional()
   .setTitle('Summary properties')
