c878f41b53f7fcd8e39334aafbfcbbb1a6ff96ad
palmanshaus <pal.manshaus@knowit.no>
2024-06-25 13:29:18 +0200

    Get non-viewable map from leaflet

--- src/layout/GeometryMap/GeometryMapComponent.tsx
+++ src/layout/GeometryMap/GeometryMapComponent.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AttributionControl, MapContainer, TileLayer } from 'react-leaflet';
 
-import { Map } from '@altinn/altinn-design-system';
-// import { Map } from 'leaflet';
-import { makeStyles, Typography } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 import type { Location } from '@altinn/altinn-design-system';
+import type { Map as LeafletMap } from 'leaflet';
 
 import { useDataModelBindings } from 'src/features/formData/useDataModelBindings';
 import { Lang } from 'src/features/language/Lang';
-import { markerIcon } from 'src/layout/GeometryMap/GeometryMapIcons';
+import classes from 'src/layout/GeometryMap/GeometryMapComponent.module.css';
 import type { PropsFromGenericComponent } from 'src/layout';
+import type { GeometryMapLocation } from 'src/layout/GeometryMap/config.generated';
 
 export type IGeometryMapComponentProps = PropsFromGenericComponent<'GeometryMap'>;
 
-export const useStyles = makeStyles(() => ({
-  footer: {
-    paddingTop: '12px',
-  },
-}));
-
 export function GeometryMapComponent({ isValid, node }: IGeometryMapComponentProps) {
   const { readOnly, layers, centerLocation, zoom, dataModelBindings } = node.item;
-  const classes = useStyles();
   const { formData, setValue } = useDataModelBindings(dataModelBindings);
   const value = 'simpleBinding' in formData ? formData.simpleBinding : undefined;
   const location = parseLocation(value);
@@ -30,18 +24,48 @@ export function GeometryMapComponent({ isValid, node }: IGeometryMapComponentPro
     setValue('simpleBinding', `${latitude.toFixed(fractionDigits)},${longitude.toFixed(fractionDigits)}`);
   };
 
+  const DefaultCenterLocation: Location = {
+    latitude: 64.888996,
+    longitude: 12.8186054,
+  };
+
+  const center = location
+    ? locationToTuple(location)
+    : centerLocation
+      ? locationToTuple(centerLocation)
+      : locationToTuple(DefaultCenterLocation);
+  const [map, setMap] = useState<LeafletMap | null>(null);
+
   return (
     <div className={`geometry-map-component${isValid ? '' : ' validation-error'}`}>
-      <Map
+      <MapContainer
+        className={classes.map}
+        center={center}
+        ref={setMap}
+        zoom={location ? 16 : zoom}
+        dragging={!readOnly}
+        attributionControl={false}
+      >
+        {layers?.map((layer, i) => (
+          <TileLayer
+            key={i}
+            url={layer.url}
+            attribution={layer.attribution}
+            subdomains={layer.subdomains ? layer.subdomains : []}
+            opacity={readOnly ? 0.5 : 1.0}
+          />
+        ))}
+        <AttributionControl prefix={false} />
+      </MapContainer>
+      {/* <Map
         layers={layers}
         centerLocation={location || centerLocation}
-        zoom={location ? 16 : zoom}
         markerLocation={location}
         readOnly={readOnly}
         onClick={handleGeometryMapClicked}
         markerIcon={markerIcon}
-      />
-      <Typography className={classes.footer}>
+      /> */}
+      <Typography className={classes.contaier}>
         {location ? (
           <Lang
             id={'geometry_map_component.selectedLocation'}
@@ -76,4 +100,8 @@ export function parseLocation(locationString: string | undefined): Location | un
     latitude: lat,
     longitude: lon,
   } as Location;
+}
+
+function locationToTuple(location: Location | GeometryMapLocation): [number, number] {
+  return [location.latitude, location.longitude];
 }
