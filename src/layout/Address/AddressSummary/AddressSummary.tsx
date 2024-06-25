06b13523aef82e96f11cd68604b0fc515e43a97f
Adam Haeger <adamgullerud@gmail.com>
2024-06-25 16:06:19 +0200

    Fix: fixed error locations on address

--- src/layout/Address/AddressSummary/AddressSummary.tsx
+++ src/layout/Address/AddressSummary/AddressSummary.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import { useDataModelBindings } from 'src/features/formData/useDataModelBindings';
 import { Lang } from 'src/features/language/Lang';
-import { useUnifiedValidationsForNode } from 'src/features/validation/selectors/unifiedValidationsForNode';
-import { validationsOfSeverity } from 'src/features/validation/utils';
-import styles from 'src/layout/Address/AddressSummary/AddressSummary.module.css';
+import { ComponentValidations } from 'src/features/validation/ComponentValidations';
+import { useBindingValidationsForNode } from 'src/features/validation/selectors/bindingValidationsForNode';
+import classes from 'src/layout/Address/AddressSummary.module.css';
 import { SingleValueSummary } from 'src/layout/Summary2/CommonSummaryComponents/SingleValueSummary';
 import type { LayoutNode } from 'src/utils/layout/LayoutNode';
 
@@ -17,59 +17,81 @@ export function AddressSummary({ componentNode }: AddressSummaryProps) {
   const { title, careOfTitle, zipCodeTitle, postPlaceTitle, houseNumberTitle } = textResourceBindings ?? {};
   const { formData } = useDataModelBindings(componentNode.item.dataModelBindings);
   const { address, postPlace, zipCode, careOf, houseNumber } = formData;
-  const validations = useUnifiedValidationsForNode(componentNode);
-  const errors = validationsOfSeverity(validations, 'error');
+
+  const bindingValidations = useBindingValidationsForNode(componentNode);
 
   return (
-    <>
-      <div className={styles.container}>
+    <div className={classes.addressSummaryComponent}>
+      <div>
         <SingleValueSummary
           title={<Lang id={title || 'address_component.address'} />}
           displayData={address}
-          errors={errors}
           componentNode={componentNode}
         />
-
-        <SingleValueSummary
-          title={<Lang id={careOfTitle || 'address_component.care_of'} />}
-          displayData={careOf}
-          errors={errors}
-          componentNode={componentNode}
-          hideEditButton={true}
+        <ComponentValidations
+          validations={bindingValidations?.address}
+          node={componentNode}
         />
-      </div>
-
-      <div className={styles.row}>
-        <div className={styles.flexGrow}>
-          <SingleValueSummary
-            title={<Lang id={zipCodeTitle || 'address_component.zip_code'} />}
-            displayData={zipCode}
-            errors={errors}
-            componentNode={componentNode}
-            hideEditButton={true}
-          />
-        </div>
-
-        <div className={styles.flexGrow}>
-          <SingleValueSummary
-            title={<Lang id={postPlaceTitle || 'address_component.post_place'} />}
-            displayData={postPlace}
-            errors={errors}
-            componentNode={componentNode}
-            hideEditButton={true}
-          />
-        </div>
       </div>
 
       {!componentNode.item.simplified && (
-        <SingleValueSummary
-          title={<Lang id={houseNumberTitle || 'address_component.house_number'} />}
-          displayData={houseNumber}
-          errors={errors}
-          componentNode={componentNode}
-          hideEditButton={true}
-        />
+        <div>
+          <SingleValueSummary
+            title={<Lang id={careOfTitle || 'address_component.care_of'} />}
+            displayData={careOf}
+            componentNode={componentNode}
+            hideEditButton={true}
+          />
+          <ComponentValidations
+            validations={bindingValidations?.careOf}
+            node={componentNode}
+          />
+        </div>
       )}
-    </>
+
+      <div className={classes.addressSummaryComponentZipCode}>
+        <div className={classes.addressComponentZipCode}>
+          <SingleValueSummary
+            title={<Lang id={zipCodeTitle || 'address_component.zip_code'} />}
+            displayData={zipCode}
+            componentNode={componentNode}
+            hideEditButton={true}
+          />
+          <ComponentValidations
+            validations={bindingValidations?.zipCode}
+            node={componentNode}
+          />
+        </div>
+
+        <div className={classes.addressSummaryComponentPostplace}>
+          <SingleValueSummary
+            title={<Lang id={postPlaceTitle || 'address_component.post_place'} />}
+            displayData={postPlace}
+            componentNode={componentNode}
+            hideEditButton={true}
+          />
+        </div>
+
+        <ComponentValidations
+          validations={bindingValidations?.postPlace}
+          node={componentNode}
+        />
+      </div>
+
+      {!componentNode.item.simplified && (
+        <div>
+          <SingleValueSummary
+            title={<Lang id={houseNumberTitle || 'address_component.house_number'} />}
+            displayData={houseNumber}
+            componentNode={componentNode}
+            hideEditButton={true}
+          />
+          <ComponentValidations
+            validations={bindingValidations?.houseNumber}
+            node={componentNode}
+          />
+        </div>
+      )}
+    </div>
   );
 }
