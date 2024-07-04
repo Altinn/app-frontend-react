8d15f508d0bf96d4316fc52c240afb1a5b9d7276
Ole Martin Handeland <git@olemartin.org>
2024-07-04 10:06:48 +0200

    The layout preprocessor does not exist in this way anymore, and I don't think the backend ever used these tests. No need to try to replicate them.

--- src/features/expressions/validation.test.ts
+++ src/features/expressions/validation.test.ts
@@ -33,46 +33,6 @@ describe('Expression validation', () => {
     });
   });
 
-  describe('Shared tests for layout preprocessor', () => {
-    const tests = getSharedTests('layout-preprocessor');
-    it.each(tests.content)('$name', (t) => {
-      const warningsExpected = t.expectsWarnings || [];
-      const logSpy = jest.spyOn(console, 'log');
-      if (warningsExpected.length > 0) {
-        logSpy.mockImplementation(() => undefined);
-      }
-
-      const result: (typeof tests)['content'][number]['layouts'] = {};
-      for (const page of Object.keys(t.layouts)) {
-        const layout = t.layouts[page].data.layout;
-        result[page] = {
-          $schema: t.layouts[page].$schema,
-          data: { layout },
-        };
-      }
-
-      expect(result).toEqual(t.expects);
-
-      // Runs all the expressions inside the layout. This is done so that we have shared tests that make sure to
-      // check that evaluating expressions in a component/node context works (i.e. that "triggers": ["validation"]
-      // is not interpreted as an expression). This will throw errors if anything goes wrong, which should make the
-      // test fail.
-      evalAllExpressions(result);
-
-      const warningsFound: string[] = [];
-      for (const call of logSpy.mock.calls) {
-        for (const warning of warningsExpected) {
-          if ((call[0] as string).includes(`%c${warning}%c`)) {
-            warningsFound.push(warning);
-          }
-        }
-      }
-      expect(warningsFound.sort()).toEqual(warningsExpected.sort());
-
-      logSpy.mockRestore();
-    });
-  });
-
   describe('Some values/objects should not validate', () => {
     it.each([
       '',
