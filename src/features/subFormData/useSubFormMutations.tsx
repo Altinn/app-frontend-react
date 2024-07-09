b0e3e5cb8a44cffa55db73f98adf9a3044bbdfab
HauklandJ <hauklandjohannes@gmail.com>
2024-07-09 16:17:00 +0200

    use app mutations instead of queries directly

--- src/features/subFormData/useSubFormMutations.tsx
+++ src/features/subFormData/useSubFormMutations.tsx
@@ -1,30 +1,45 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'react-toastify';
 
+import { useMutation } from '@tanstack/react-query';
+
+import { useAppMutations } from 'src/core/contexts/AppQueriesProvider';
 import { useStrictInstance } from 'src/features/instance/InstanceContext';
-import { doSubFormEntryAdd, doSubFormEntryDelete } from 'src/queries/queries';
+import { useLanguage } from 'src/features/language/useLanguage';
 
 export const useAddEntryMutation = (dataType: string) => {
   const instanceContext = useStrictInstance();
-  const queryClient = useQueryClient();
+  const { langAsString } = useLanguage();
+  const { doSubFormEntryAdd } = useAppMutations();
+
   return useMutation({
     mutationKey: ['addSubForm'],
     mutationFn: async (data: any) => await doSubFormEntryAdd(instanceContext.instanceId, dataType, data),
     onSuccess: async () => {
-      queryClient.invalidateQueries({ queryKey: ['subFormEntries'] });
+      // TODO: instanceContext.changeData?
       await instanceContext.reFetch();
+    },
+    onError: () => {
+      // TODO: where are the language keys defined?
+      toast(langAsString('form_filler.error_add_sub_form'), { type: 'error' });
     },
   });
 };
 
 export const useDeleteEntryMutation = (id: string) => {
   const instanceContext = useStrictInstance();
-  const queryClient = useQueryClient();
+  const { langAsString } = useLanguage();
+  const { doSubFormEntryDelete } = useAppMutations();
+
   return useMutation({
     mutationKey: ['deleteSubForm', id],
     mutationFn: async () => await doSubFormEntryDelete(instanceContext.instanceId, id),
     onSuccess: async () => {
-      queryClient.invalidateQueries({ queryKey: ['subFormEntries'] });
+      // TODO: instanceContext.changeData?
       await instanceContext.reFetch();
+    },
+    onError: () => {
+      // TODO: where are the language keys defined?
+      toast(langAsString('form_filler.error_delete_sub_form'), { type: 'error' });
     },
   });
 };
