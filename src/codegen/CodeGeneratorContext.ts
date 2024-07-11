0082ce7739adc2732964d2bb8fbaf06cbd71b09a
cammiida <camillamdalan@gmail.com>
2024-07-11 20:27:08 +0200

    fixes errors

--- src/codegen/CodeGeneratorContext.ts
+++ src/codegen/CodeGeneratorContext.ts
@@ -1,8 +1,10 @@
 import deepEqual from 'fast-deep-equal';
-import type { JSONSchema7, JSONSchema7Definition } from 'json-schema';
+import type { JSONSchema7 } from 'json-schema';
 
 import type { MaybeSymbolizedCodeGenerator } from 'src/codegen/CodeGenerator';
 import type { SchemaFile } from 'src/codegen/SchemaFile';
+
+type JsonSchema7WithDefinitions = Required<Pick<JSONSchema7, 'definitions'>> & JSONSchema7;
 
 /**
  * This code relies on the fact that the code generator will generate one file at a time, and reset the context
@@ -56,87 +58,71 @@ export class CodeGeneratorContext {
     return { result: parts.join('\n\n') };
   }
 
-  private static async getJsonSchema({
-    file,
-    jsonSchemaFileContext,
-  }: {
-    file: SchemaFile;
-    jsonSchemaFileContext: CodeGeneratorFileContext<'jsonSchema'>;
-  }): Promise<JSONSchema7> {
-    const symbols: SymbolTable<'jsonSchema'> = {};
-    while (Object.keys(jsonSchemaFileContext.symbols).length) {
-      const newSymbols = jsonSchemaFileContext.getSymbols(symbols);
-      Object.assign(symbols, newSymbols);
-    }
-    const sortedSymbols = Object.keys(symbols).sort((a, b) => a.localeCompare(b));
-
-    // Sort symbols and add them in sorted order to the file
-    const jsonSchema = await file.getSchema();
-    for (const symbol of sortedSymbols) {
-      jsonSchema.definitions = jsonSchema.definitions || {};
-      jsonSchema.definitions[symbol] = symbols[symbol];
-    }
-
-    return jsonSchema;
-  }
-
-  private static findRefs({
-    defs,
-    jsonSchemaWithoutDefs,
-  }: {
-    defs: {
-      [x: string]: JSONSchema7Definition;
-    };
-    jsonSchemaWithoutDefs: Omit<JSONSchema7, 'definitions'>;
-  }) {
+  private static findRefs(jsonSchema: JsonSchema7WithDefinitions): Set<string> {
+    const { definitions, ...jsonSchemaWithoutDefs } = jsonSchema;
     const refRegex = /"\$ref":\s*"([^"]+)"/g;
     const foundRefs = new Set<string>();
 
-    for (const value of [...Object.values(defs), jsonSchemaWithoutDefs]) {
-      const asJson = JSON.stringify(value);
-      const refMatch = asJson.match(refRegex)?.at(0);
-
-      if (refMatch) {
-        const result = refMatch.replace('"$ref":', '').replace(/"/g, '').trim().replace('#/definitions/', '');
-        foundRefs.add(result);
-      } else if (asJson.includes('$ref')) {
-        throw new Error(`Could not find ref in ${asJson}`);
+    for (const definition of [...Object.values(definitions), jsonSchemaWithoutDefs]) {
+      const jsonDefinition = JSON.stringify(definition);
+      const refMatches = jsonDefinition.match(refRegex);
+      if (refMatches) {
+        for (const ref of refMatches) {
+          const result = ref.replace('"$ref":', '').replace(/"/g, '').trim().replace('#/definitions/', '');
+          foundRefs.add(result);
+        }
+      } else if (jsonDefinition.includes('$ref')) {
+        throw new Error(`Could not find ref in ${jsonDefinition}`);
       }
     }
 
     return foundRefs;
   }
 
-  private static cleanJsonSchemaDefinitions(jsonSchema: JSONSchema7) {
-    const { definitions, ...jsonSchemaWithoutDefs } = jsonSchema;
-    if (!definitions) {
-      return jsonSchema;
-    }
-
-    const tempDefinitions = { ...definitions };
+  private static cleanDefinitions(jsonSchema: JsonSchema7WithDefinitions): NonNullable<JSONSchema7['definitions']> {
     let removedLastRound: number | undefined = undefined;
+    const { definitions: originalDefinitions, ...rest } = jsonSchema;
+    let cleanedDefinitions = { ...originalDefinitions };
 
     while (removedLastRound === undefined || removedLastRound > 0) {
-      const allRefs = new Set<string>(Object.keys(tempDefinitions));
-      const foundRefs = this.findRefs({ defs: tempDefinitions, jsonSchemaWithoutDefs });
+      const foundRefs = this.findRefs({ ...rest, definitions: cleanedDefinitions });
+      const allRefs = new Set<string>(Object.keys(cleanedDefinitions));
       const notFoundRefs = [...allRefs].filter((ref) => !foundRefs.has(ref));
 
+      const finalSchemaDefs = structuredClone(cleanedDefinitions);
       removedLastRound = 0;
-      for (const notFoundRefKey of notFoundRefs) {
-        delete tempDefinitions[notFoundRefKey];
+      for (const key of notFoundRefs) {
+        delete finalSchemaDefs[key];
         removedLastRound++;
       }
-    }
 
-    return { ...jsonSchema, definitions: tempDefinitions };
+      cleanedDefinitions = finalSchemaDefs;
+    }
+    return cleanedDefinitions;
   }
 
   public static async generateJsonSchema(baseFilePath: string, file: SchemaFile): Promise<{ result: JSONSchema7 }> {
     const targetFile = baseFilePath + file.getFileName();
     const jsonSchemaFileContext = new CodeGeneratorFileContext(targetFile, 'jsonSchema');
     CodeGeneratorContext.fileContext = jsonSchemaFileContext;
-    const jsonSchema = await this.getJsonSchema({ file, jsonSchemaFileContext });
-    const cleanedJsonSchema = file.shouldCleanDefinitions() ? this.cleanJsonSchemaDefinitions(jsonSchema) : jsonSchema;
+    const jsonSchema = await file.getSchema();
+
+    const symbols: SymbolTable<'jsonSchema'> = {};
+    while (Object.keys(jsonSchemaFileContext.symbols).length) {
+      const newSymbols = jsonSchemaFileContext.getSymbols(symbols);
+      Object.assign(symbols, newSymbols);
+    }
+
+    // Sort symbols and add them in sorted order to the file
+    const sortedSymbols = Object.keys(symbols).sort((a, b) => a.localeCompare(b));
+    for (const symbol of sortedSymbols) {
+      jsonSchema.definitions = jsonSchema.definitions ?? {};
+      jsonSchema.definitions[symbol] = symbols[symbol];
+    }
+
+    if (file.shouldCleanDefinitions() && jsonSchema.definitions) {
+      jsonSchema.definitions = this.cleanDefinitions({ ...jsonSchema, definitions: jsonSchema.definitions });
+    }
 
     CodeGeneratorContext.fileContext = undefined;
 
@@ -144,7 +130,7 @@ export class CodeGeneratorContext {
       result: {
         $schema: 'http://json-schema.org/draft-07/schema#',
         $id: `https://altinncdn.no/${targetFile}`,
-        ...cleanedJsonSchema,
+        ...jsonSchema,
       },
     };
   }
