00325f687139ea91753e5d95fb1a424361405f48
Ole Martin Handeland <git@olemartin.org>
2024-07-23 11:57:27 +0200

    Making this test less flaky

--- test/e2e/integration/frontend-test/tabbing.ts
+++ test/e2e/integration/frontend-test/tabbing.ts
@@ -14,6 +14,7 @@ describe('Tabbing', () => {
       .should('have.value', 'a')
       .should('have.attr', 'id')
       .and('eq', appFrontend.changeOfName.newFullName.substring(1));
+    cy.waitUntilSaved();
     cy.tab().tab().tab({
       shift: true,
     });
