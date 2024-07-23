1a1243531c2af60506f83f625efbaf05dcf50d00
Ole Martin Handeland <git@olemartin.org>
2024-07-23 10:57:50 +0200

    Making sure this validation test waits until the button is clickable and the validation is present

--- test/e2e/integration/frontend-test/validation.ts
+++ test/e2e/integration/frontend-test/validation.ts
@@ -163,9 +163,13 @@ describe('Validation', () => {
   });
 
   it('Validation on uploaded attachment type with tag', () => {
+    cy.intercept('POST', '**/instances/**/data?dataType=*').as('upload');
     cy.goto('changename');
     cy.get(appFrontend.changeOfName.uploadWithTag.uploadZone).selectFile('test/e2e/fixtures/test.pdf', { force: true });
-    cy.get(appFrontend.changeOfName.uploadWithTag.saveTag).click({ multiple: true });
+    cy.wait('@upload');
+    cy.waitUntilNodesReady();
+    cy.get(appFrontend.changeOfName.uploadWithTag.saveTag).should('not.be.disabled');
+    cy.get(appFrontend.changeOfName.uploadWithTag.saveTag).click();
     cy.get(appFrontend.changeOfName.uploadWithTag.error).should(
       'not.contain.text',
       appFrontend.changeOfName.uploadWithTag.unwantedChar,
