a620bfda64968acaf4fac09c17f3d46b1869b918
Ole Martin Handeland <git@olemartin.org>
2024-08-06 13:47:09 +0200

    Prettier++

--- src/features/baseurlinjection.ts
+++ src/features/baseurlinjection.ts
@@ -1,4 +1,3 @@
-
 // Apps are running in a subpath of `/{org}/{app}` both locally and in deployed environments
 // some features make use of relative paths that should resolve relative to this subpath.
 // * `image.src` on the `ImageComponent` might have `some-image.jpg` which we expect to then exist in the `wwwroot/`
@@ -9,9 +8,12 @@ const heads = document.getElementsByTagName('head');
 if (heads && heads.length > 0) {
   const head = heads[0];
   const base = document.createElement('base');
-  base.href = window.location.protocol + '//' +
-    window.location.hostname +
-    (window.location.port ? ':' + window.location.port : '');
-  base.href += `${window.org}/${window.app}/`;
+  const { protocol, hostname, port: _port } = window.location;
+  const { org, app } = window;
+  const isDefaultHttpsPort = protocol === 'https:' && _port === '443';
+  const isDefaultHttpPort = protocol === 'http:' && _port === '80';
+  const isDefaultPort = isDefaultHttpsPort || isDefaultHttpPort;
+  const port = _port && !isDefaultPort ? `:${_port}` : '';
+  base.href = `${protocol}//${hostname}${port}/${org}/${app}/`;
   head.appendChild(base);
 }
