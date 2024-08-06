a80862efe27e7e6f5eddae1913d6849c59b10f02
Martin Othamar <martin.othamar@digdir.no>
2024-08-06 12:35:10 +0200

    Try to format

--- src/features/baseurlinjection.ts
+++ src/features/baseurlinjection.ts
@@ -6,8 +6,7 @@
 // The `base` element in the HTML head will make relative references resolve from `base.href`.
 // Bugreport: https://github.com/Altinn/app-frontend-react/issues/2257
 const heads = document.getElementsByTagName('head');
-if (heads && heads.length > 0)
-{
+if (heads && heads.length > 0) {
   const head = heads[0];
   const base = document.createElement('base');
   base.href = window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port : '');
