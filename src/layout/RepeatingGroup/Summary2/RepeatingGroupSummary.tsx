4ef522bd27ae36b2d33e34aedc70b749d4a09b47
Magnus Revheim Martinsen <mrmartinsen.96@gmail.com>
2024-08-07 12:28:53 +0200

    add nested repeating group frame

--- src/layout/RepeatingGroup/Summary2/RepeatingGroupSummary.tsx
+++ src/layout/RepeatingGroup/Summary2/RepeatingGroupSummary.tsx
@@ -8,8 +8,8 @@ import { useUnifiedValidationsForNode } from 'src/features/validation/selectors/
 import { validationsOfSeverity } from 'src/features/validation/utils';
 import { useRepeatingGroup } from 'src/layout/RepeatingGroup/RepeatingGroupProviders/RepeatingGroupContext';
 import classes from 'src/layout/RepeatingGroup/Summary2/RepeatingGroupSummary.module.css';
+import { BaseLayoutNode, type LayoutNode } from 'src/utils/layout/LayoutNode';
 import type { InputSummaryOverrideProps } from 'src/layout/Summary2/config.generated';
-import type { LayoutNode } from 'src/utils/layout/LayoutNode';
 
 type RepeatingGroupComponentSummaryProps = {
   componentNode: LayoutNode<'RepeatingGroup'>;
@@ -21,30 +21,32 @@ export const RepeatingGroupSummary = ({ componentNode }: RepeatingGroupComponent
   const errors = validationsOfSeverity(validations, 'error');
   const title = componentNode.item.textResourceBindings?.title;
   const rowsToDisplaySet = new Set(rowsToDisplay.map((row) => row.uuid));
-
   const rows = componentNode.item.rows.filter((row) => rowsToDisplaySet.has(row.uuid));
+  const isNested = componentNode.parent instanceof BaseLayoutNode;
 
   return (
-    <>
+    <div className={cn(classes.repeatingGroupSummaryWrapper)}>
       <Label weight={'medium'}>{<Lang id={title} />}</Label>
-      {rows.map((row, index) => (
-        <div
-          key={row.uuid}
-          className={cn(classes.repeatingGroupSummaryWrapper)}
-          // className={cn({ [classes.repeatingGroupRow]: index < rows.length - 1 })}
-        >
-          {row.items &&
-            row.items.map((layoutItem) => (
-              <div
-                key={layoutItem.item.id}
-                className={cn(classes.repeatingGroupSummaryWrapper)}
-              >
-                {layoutItem.def.renderSummary2 && layoutItem.def.renderSummary2(layoutItem as LayoutNode<any>)}
-              </div>
-            ))}
-          {index < rows.length - 1 && <hr />}
-        </div>
-      ))}
-    </>
+      <div className={cn({ [classes.nestedRepeatingGroupWrapper]: isNested })}>
+        {rows.map((row, index) => (
+          <div
+            key={row.uuid}
+            className={cn(classes.repeatingGroupSummaryWrapper, {
+              [classes.repeatingGroupRow]: index < rows.length - 1,
+            })}
+          >
+            {row.items &&
+              row.items.map((layoutItem) => (
+                <div
+                  key={layoutItem.item.id}
+                  className={cn(classes.repeatingGroupSummaryWrapper)}
+                >
+                  {layoutItem.def.renderSummary2 && layoutItem.def.renderSummary2(layoutItem as LayoutNode<any>)}
+                </div>
+              ))}
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
