e02aa8bf8b49dd251f217c74929ac3996b39af96
Daniel Skovli <daniel.skovli@gmail.com>
2024-08-13 15:03:23 +0200

    Adds summary row borders as per figma designs

--- src/layout/FileUpload/FileUploadTable/FileTableRow.tsx
+++ src/layout/FileUpload/FileUploadTable/FileTableRow.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import classNames from 'classnames';
+
 import { AltinnLoader } from 'src/components/AltinnLoader';
 import { isAttachmentUploaded } from 'src/features/attachments';
 import { Lang } from 'src/features/language/Lang';
@@ -38,14 +40,10 @@ export function FileTableRow({ node, attachment, mobileView, tagLabel, isSummary
     ? langAsString('form_filler.file_uploader_list_status_done')
     : langAsString('general.loading');
 
-  let rowStyle: string;
-  if (isSummary) {
-    rowStyle = classes.noRowSpacing;
-  } else if (pdfModeActive) {
-    rowStyle = classes.grayUnderline;
-  } else {
-    rowStyle = classes.blueUnderlineDotted;
-  }
+  const rowStyle =
+    isSummary || pdfModeActive
+      ? classNames(classes.noRowSpacing, classes.grayUnderlineDotted)
+      : classes.blueUnderlineDotted;
 
   return (
     <tr
