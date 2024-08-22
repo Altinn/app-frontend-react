546382d49fa5bf5dfaf9f95de17ae34e4c923a55
Ole Martin Handeland <git@olemartin.org>
2024-08-22 12:52:41 +0200

    Updating test to also test with only 'person' party type being allowed

--- test/e2e/integration/frontend-test/self-identified-user.ts
+++ test/e2e/integration/frontend-test/self-identified-user.ts
@@ -1,15 +1,38 @@
+import { CyHttpMessages } from 'cypress/types/net-stubbing';
+
 import { AppFrontend } from 'test/e2e/pageobjects/app-frontend';
+import IncomingHttpResponse = CyHttpMessages.IncomingHttpResponse;
+import type { IncomingApplicationMetadata } from 'src/features/applicationMetadata/types';
 
 const appFrontend = new AppFrontend();
 
 describe('Self identified user', () => {
-  it('should be able to log in and create an instance (without fetching party list or current party)', () => {
-    cy.intercept('**/active', []).as('noActiveInstances');
-    cy.startAppInstance(appFrontend.apps.frontendTest, { user: 'selfIdentified', authenticationLevel: '0' });
+  it('should be able to log in and create an instance', () => {
+    testSelfIdentifiedUser();
+  });
 
-    cy.get(appFrontend.closeButton).should('be.visible');
-    cy.findByRole('heading', { name: /Appen for test av app frontend/i }).should('exist');
+  it('should be able to log in and create an instance when only persons are allowed', () => {
+    cy.intercept('GET', '**/api/v1/applicationmetadata', (req) => {
+      req.on('response', (res: IncomingHttpResponse<IncomingApplicationMetadata>) => {
+        res.body.partyTypesAllowed = {
+          person: true,
+          subUnit: false,
+          bankruptcyEstate: false,
+          organisation: false,
+        };
+      });
+    });
 
-    cy.assertUser('selfIdentified');
+    testSelfIdentifiedUser();
   });
 });
+
+function testSelfIdentifiedUser() {
+  cy.intercept('**/active', []).as('noActiveInstances');
+  cy.startAppInstance(appFrontend.apps.frontendTest, { user: 'selfIdentified', authenticationLevel: '0' });
+
+  cy.get(appFrontend.closeButton).should('be.visible');
+  cy.findByRole('heading', { name: /Appen for test av app frontend/i }).should('exist');
+
+  cy.assertUser('selfIdentified');
+}
