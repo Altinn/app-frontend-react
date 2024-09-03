21e48ba4a09f9646c8e58f70058cb0e3c115ce2e
HauklandJ <hauklandjohannes@gmail.com>
2024-09-03 13:13:47 +0200

    fix todo

--- src/features/form/layoutSets/TypeGuards.ts
+++ src/features/form/layoutSets/TypeGuards.ts
@@ -3,7 +3,7 @@ import type { ILayoutSet, ILayoutSetDefault, ILayoutSetSubForm } from 'src/layou
 export function layoutSetIsSubForm(layoutSet: ILayoutSet): layoutSet is ILayoutSetSubForm {
   return 'type' in layoutSet && layoutSet.type === 'subform';
 }
-// TODO: normal needs a better name
+
 export function layoutSetIsDefault(layoutSet: ILayoutSet): layoutSet is ILayoutSetDefault {
   return 'tasks' in layoutSet && Array.isArray(layoutSet.tasks) && layoutSet.tasks.length != 0;
 }
