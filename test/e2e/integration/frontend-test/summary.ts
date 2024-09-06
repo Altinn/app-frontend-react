2802210220e76fbb900608b5e1aa8d39934c51e7
Ole Martin Handeland <git@olemartin.org>
2024-09-06 15:17:13 +0200

    Revert "Massive cheating: Disabling all other tests than the one that's consistently failing on github+tt02 (but not locally). Will revert this later, obviously."
    This reverts commit 396570145055d45aa54bea3fe52a50dc82c58f51.

--- test/e2e/integration/frontend-test/summary.ts
+++ test/e2e/integration/frontend-test/summary.ts
@@ -377,7 +377,7 @@ describe('Summary', () => {
       .and('not.contain.text', 'Skjul kommentar felt');
   });
 
-  it.only('Navigation between summary and pages', () => {
+  it('Navigation between summary and pages', () => {
     cy.gotoAndComplete('changename');
 
     const pageValidationConfigs: (PageValidation | undefined)[] = [
