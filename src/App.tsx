92a2f70e72dbd667c8ab1ebd751f705c04e8f837
Daniel Skovli <daniel.skovli@gmail.com>
2024-09-26 13:00:52 +0200

    Removes (presumed) unnecessary TaskStoreProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,50 +2,47 @@ import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
 import { ProcessWrapperWrapper } from 'src/components/wrappers/ProcessWrapper';
-import { TaskStoreProvider } from 'src/core/contexts/taskStoreContext';
 import { Entrypoint } from 'src/features/entrypoint/Entrypoint';
 import { InstanceProvider } from 'src/features/instance/InstanceContext';
 import { PartySelection } from 'src/features/instantiate/containers/PartySelection';
 import { InstanceSelectionWrapper } from 'src/features/instantiate/selection/InstanceSelection';
 
 export const App = () => (
-  <TaskStoreProvider>
-    <Routes>
-      <Route
-        path={'*'}
-        element={<Entrypoint />}
-      />
-      <Route
-        path='/instance-selection/*'
-        element={<InstanceSelectionWrapper />}
-      />
+  <Routes>
+    <Route
+      path={'*'}
+      element={<Entrypoint />}
+    />
+    <Route
+      path='/instance-selection/*'
+      element={<InstanceSelectionWrapper />}
+    />
 
-      <Route
-        path='/party-selection/*'
-        element={<PartySelection />}
-      />
+    <Route
+      path='/party-selection/*'
+      element={<PartySelection />}
+    />
 
-      <Route
-        path='/instance/:partyId/:instanceGuid/*'
-        element={
-          <InstanceProvider>
-            <ProcessWrapperWrapper />
-          </InstanceProvider>
-        }
-      />
+    <Route
+      path='/instance/:partyId/:instanceGuid/*'
+      element={
+        <InstanceProvider>
+          <ProcessWrapperWrapper />
+        </InstanceProvider>
+      }
+    />
 
-      {/**
-       * Redirects from legacy URLs to new URLs
-       */}
-      <Route
-        path='/partyselection/*'
-        element={
-          <Navigate
-            to='/party-selection/'
-            replace={true}
-          />
-        }
-      />
-    </Routes>
-  </TaskStoreProvider>
+    {/**
+     * Redirects from legacy URLs to new URLs
+     */}
+    <Route
+      path='/partyselection/*'
+      element={
+        <Navigate
+          to='/party-selection/'
+          replace={true}
+        />
+      }
+    />
+  </Routes>
 );
