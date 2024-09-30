971658a7db7376b27067166217d7935740c31dfa
Ole Martin Handeland <git@olemartin.org>
2024-09-30 10:28:20 +0200

    Fixing overwriting of attachment validations

--- src/features/validation/StoreValidationsInNode.tsx
+++ src/features/validation/StoreValidationsInNode.tsx
@@ -8,6 +8,7 @@ import { NodesStateQueue } from 'src/utils/layout/generator/CommitQueue';
 import { GeneratorInternal } from 'src/utils/layout/generator/GeneratorContext';
 import { GeneratorCondition, StageFormValidation } from 'src/utils/layout/generator/GeneratorStages';
 import { NodesInternal } from 'src/utils/layout/NodesContext';
+import type { AnyValidation, AttachmentValidation } from 'src/features/validation/index';
 import type { CompCategory } from 'src/layout/common';
 import type { TypesFromCategory } from 'src/layout/layout';
 import type { LayoutNode } from 'src/utils/layout/LayoutNode';
@@ -23,18 +24,19 @@ export function StoreValidationsInNode() {
   );
 }
 
+type Node = LayoutNode<TypesFromCategory<CompCategory.Form | CompCategory.Container>>;
+
 function StoreValidationsInNodeWorker() {
   const item = GeneratorInternal.useIntermediateItem()!;
-  const node = GeneratorInternal.useParent() as LayoutNode<
-    TypesFromCategory<CompCategory.Form | CompCategory.Container>
-  >;
+  const node = GeneratorInternal.useParent() as Node;
 
   const shouldValidate = useMemo(
     () => item !== undefined && !('renderAsSummary' in item && item.renderAsSummary),
     [item],
   );
 
-  const validations = useNodeValidation(node, shouldValidate);
+  const freshValidations = useNodeValidation(node, shouldValidate);
+  const validations = useUpdatedValidations(freshValidations, node);
 
   const hasBeenSet = NodesInternal.useNodeData(node, (data) => deepEqual(data.validations, validations));
   NodesStateQueue.useSetNodeProp({ node, prop: 'validations', value: validations }, !hasBeenSet && shouldValidate);
@@ -45,4 +47,28 @@ function StoreValidationsInNodeWorker() {
   isInitialRender.current = false;
 
   return null;
+}
+
+function useUpdatedValidations(validations: AnyValidation[], node: Node) {
+  return NodesInternal.useNodeData(node, (data) => {
+    if (!data.validations) {
+      return validations;
+    }
+
+    const copy = [...validations];
+    for (const [idx, validation] of copy.entries()) {
+      if (!('attachmentId' in validation)) {
+        continue;
+      }
+      // Preserve the visibility of existing attachment validations
+      const existing = data.validations.find(
+        (v) => 'attachmentId' in v && v.attachmentId === validation.attachmentId,
+      ) as AttachmentValidation;
+      if (existing) {
+        copy[idx] = { ...validation, visibility: existing.visibility };
+      }
+    }
+
+    return copy;
+  });
 }
