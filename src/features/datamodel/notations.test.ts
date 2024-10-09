5a03cc4b9af467a318355c85d2e7a88fcf5bad88
Bjørnar Østtveit <bjosttveit@gmail.com>
2024-10-09 14:01:55 +0200

    added test-case for #1918

--- src/features/datamodel/notations.test.ts
+++ src/features/datamodel/notations.test.ts
@@ -9,6 +9,13 @@ describe('notations', () => {
       { input: '/path/list/7/property', output: 'path.list[7].property' },
       { input: '/path/list/7/group/nested-list/3/property', output: 'path.list[7].group.nested-list[3].property' },
       { input: '/path/to-dashed/property', output: 'path.to-dashed.property' },
+
+      // https://github.com/Altinn/app-frontend-react/issues/1918
+      { input: '/Oppgave/rapporteringsenhet/e-post', output: 'Oppgave.rapporteringsenhet.e-post' },
+      {
+        input: '/Oppgave/rapporteringsenhet/kontaktperson/epost_1',
+        output: 'Oppgave.rapporteringsenhet.kontaktperson.epost_1',
+      },
     ];
 
     test.each(testCases)('pointerToDotNotation($input) returns $output', ({ input, output }) => {
