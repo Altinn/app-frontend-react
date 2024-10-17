10e1596b41e5a107b4544c35a51d2d2c730ae607
Camilla Marie Dalan <camillamdalan@gmail.com>
2024-10-17 16:27:07 +0200

    refactors Entrypoint routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
+import { Form, FormFirstPage } from 'src/components/form/Form';
+import { PresentationComponent } from 'src/components/presentation/Presentation';
 import { ProcessWrapperWrapper } from 'src/components/wrappers/ProcessWrapper';
 import { TaskStoreProvider } from 'src/core/contexts/taskStoreContext';
 import { Entrypoint } from 'src/features/entrypoint/Entrypoint';
 import { InstanceProvider } from 'src/features/instance/InstanceContext';
 import { PartySelection } from 'src/features/instantiate/containers/PartySelection';
 import { InstanceSelectionWrapper } from 'src/features/instantiate/selection/InstanceSelection';
+import { PresentationType } from 'src/types';
 
 export const App = () => (
   <TaskStoreProvider>
     <Routes>
-      <Route
-        path={'*'}
-        element={<Entrypoint />}
-      />
+      <Route element={<Entrypoint />}>
+        <Route
+          path=':pageKey'
+          element={
+            <PresentationComponent type={PresentationType.Stateless}>
+              <Form />
+            </PresentationComponent>
+          }
+        />
+        <Route
+          index
+          element={<FormFirstPage />}
+        />
+      </Route>
       <Route
         path='/instance-selection/*'
         element={<InstanceSelectionWrapper />}
