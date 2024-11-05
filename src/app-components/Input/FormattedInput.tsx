8d16c2ea82778b7f0257c3c28d812cc4f028e84c
Camilla Marie Dalan <camillamdalan@gmail.com>
2024-11-05 09:31:39 +0100

    fixes correct inputprops import in formattedinput

--- src/app-components/Input/FormattedInput.tsx
+++ src/app-components/Input/FormattedInput.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 import { PatternFormat } from 'react-number-format';
 import type { PatternFormatProps } from 'react-number-format';
 
-import type { InputProps } from '@material-ui/core';
-
 import { Input } from 'src/app-components/Input/Input';
+import type { InputProps } from 'src/app-components/Input/Input';
 
 export function FormattedInput(props: Omit<PatternFormatProps, 'customInput' | 'size'> & InputProps) {
   return (
