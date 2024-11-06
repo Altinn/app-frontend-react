6c1f416a152c7140751a9fafb88a4ef8d261d67b
Camilla Marie Dalan <camillamdalan@gmail.com>
2024-11-06 12:27:52 +0100

    fixes tests

--- src/app-components/button/Button.tsx
+++ src/app-components/button/Button.tsx
@@ -4,33 +4,45 @@ import type { PropsWithChildren } from 'react';
 import { Button as DesignSystemButton, Spinner } from '@digdir/designsystemet-react';
 
 export interface ButtonProps {
-  id: string;
+  id?: string;
   disabled?: boolean;
   variant?: ButtonVariant;
   color?: ButtonColor;
   isLoading?: boolean;
   size?: 'sm' | 'md' | 'lg';
+  className?: string;
   onClick: () => void;
 }
 
 export type ButtonVariant = Parameters<typeof DesignSystemButton>[0]['variant'];
 export type ButtonColor = Parameters<typeof DesignSystemButton>[0]['color'];
 
-export const Button = ({ children, isLoading = false, size = 'sm', ...props }: PropsWithChildren<ButtonProps>) => (
+export const Button = ({
+  id,
+  disabled,
+  variant,
+  color,
+  size = 'sm',
+  onClick,
+  className,
+  isLoading = false,
+  children,
+}: PropsWithChildren<ButtonProps>) => (
   <DesignSystemButton
-    id={props.id}
-    disabled={props.disabled || isLoading}
-    variant={props.variant}
-    color={props.color}
+    id={id}
+    disabled={disabled || isLoading}
+    variant={variant}
+    color={color}
     size={size}
-    onClick={props.onClick}
+    onClick={onClick}
+    className={className}
   >
     {isLoading && (
       <Spinner
         aria-hidden='true'
-        color={props.color}
+        color={color}
         size={size === 'lg' ? 'sm' : 'xs'}
-        title='button-spinner-loading'
+        title='Laster innhold'
       />
     )}
     {children}
