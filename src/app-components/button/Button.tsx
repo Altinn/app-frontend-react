d37b191153a2786ed12c555d73d1d84c6f9ac4f0
Camilla Marie Dalan <camillamdalan@gmail.com>
2024-11-11 15:07:21 +0100

    fixes popover not working with new button

--- src/app-components/button/Button.tsx
+++ src/app-components/button/Button.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import type { ButtonHTMLAttributes, PropsWithChildren } from 'react';
 
 import { Button as DesignSystemButton, Spinner } from '@digdir/designsystemet-react';
@@ -33,61 +33,65 @@ export type ButtonProps = {
 export type ButtonVariant = Parameters<typeof DesignSystemButton>[0]['variant'];
 export type ButtonColor = Parameters<typeof DesignSystemButton>[0]['color'];
 
-export const Button = ({
-  id,
-  title,
-  disabled,
-  variant = 'primary',
-  color, // TODO: set default to 'accent' when design system is updated
-  size = 'sm',
-  className,
-  icon,
-  isLoading = false,
-  'aria-label': ariaLabel,
-  'aria-controls': ariaControls,
-  'aria-haspopup': ariaHasPopup,
-  'aria-expanded': ariaExpanded,
-  'aria-labelledby': ariaLabelledBy,
-  'aria-describedby': ariaDescribedBy,
+export const Button = forwardRef<HTMLButtonElement, PropsWithChildren<ButtonProps>>(function Button(
+  {
+    id,
+    title,
+    disabled,
+    variant = 'primary',
+    color = 'first',
+    size = 'sm',
+    className,
+    icon,
+    isLoading = false,
+    'aria-label': ariaLabel,
+    'aria-controls': ariaControls,
+    'aria-haspopup': ariaHasPopup,
+    'aria-expanded': ariaExpanded,
+    'aria-labelledby': ariaLabelledBy,
+    'aria-describedby': ariaDescribedBy,
+    style,
+    tabIndex,
+    fullWidth,
+    onClick,
+    onMouseDown,
+    children,
+  },
   ref,
-  style,
-  tabIndex,
-  fullWidth,
-  onClick,
-  onMouseDown,
-  children,
-}: PropsWithChildren<ButtonProps>) => (
-  <DesignSystemButton
-    id={id}
-    title={title}
-    disabled={disabled || isLoading}
-    variant={variant}
-    color={color}
-    size={size}
-    icon={icon}
-    className={className}
-    aria-label={ariaLabel}
-    style={style}
-    tabIndex={tabIndex}
-    onClick={onClick}
-    onMouseDown={onMouseDown}
-    aria-busy={isLoading}
-    aria-controls={ariaControls}
-    aria-haspopup={ariaHasPopup}
-    aria-expanded={ariaExpanded}
-    aria-labelledby={ariaLabelledBy}
-    aria-describedby={ariaDescribedBy}
-    ref={ref}
-    fullWidth={fullWidth}
-  >
-    {isLoading && (
-      <Spinner
-        aria-hidden='true'
-        color={color}
-        size={size === 'lg' ? 'sm' : 'xs'}
-        title='Laster innhold'
-      />
-    )}
-    {children}
-  </DesignSystemButton>
-);
+) {
+  return (
+    <DesignSystemButton
+      id={id}
+      title={title}
+      disabled={disabled || isLoading}
+      variant={variant}
+      color={color}
+      size={size}
+      icon={icon}
+      className={className}
+      aria-label={ariaLabel}
+      style={style}
+      tabIndex={tabIndex}
+      onClick={onClick}
+      onMouseDown={onMouseDown}
+      aria-busy={isLoading}
+      aria-controls={ariaControls}
+      aria-haspopup={ariaHasPopup}
+      aria-expanded={ariaExpanded}
+      aria-labelledby={ariaLabelledBy}
+      aria-describedby={ariaDescribedBy}
+      ref={ref}
+      fullWidth={fullWidth}
+    >
+      {isLoading && (
+        <Spinner
+          aria-hidden='true'
+          color={color}
+          size={size === 'lg' ? 'sm' : 'xs'}
+          title='Laster innhold'
+        />
+      )}
+      {children}
+    </DesignSystemButton>
+  );
+});
