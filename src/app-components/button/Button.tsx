9648d01e079857e18f3387b0c15d68e234a60072
Camilla Marie Dalan <camillamdalan@gmail.com>
2024-11-12 14:36:07 +0100

    cleans up button props

--- src/app-components/button/Button.tsx
+++ src/app-components/button/Button.tsx
@@ -1,22 +1,25 @@
 import React, { forwardRef } from 'react';
-import type { ButtonHTMLAttributes, PropsWithChildren } from 'react';
+import type { PropsWithChildren } from 'react';
 
 import { Button as DesignSystemButton, Spinner } from '@digdir/designsystemet-react';
+import type { ButtonProps as DesignSystemButtonProps } from '@digdir/designsystemet-react';
+
+export type ButtonVariant = 'primary' | 'secondary' | 'tertiary' | undefined;
+export type ButtonColor = 'first' | 'second' | 'success' | 'danger' | undefined;
 
 export type ButtonProps = {
-  id?: string;
-  title?: string;
-  disabled?: boolean;
   variant?: ButtonVariant;
   color?: ButtonColor;
   isLoading?: boolean;
   size?: 'sm' | 'md' | 'lg';
   className?: string;
-  icon?: boolean;
-  ref?: React.RefObject<HTMLButtonElement>;
-  fullWidth?: boolean;
 } & Pick<
-  ButtonHTMLAttributes<HTMLButtonElement>,
+  DesignSystemButtonProps,
+  | 'id'
+  | 'title'
+  | 'disabled'
+  | 'icon'
+  | 'fullWidth'
   | 'onClick'
   | 'style'
   | 'tabIndex'
@@ -31,59 +34,19 @@ export type ButtonProps = {
   | 'onKeyUp'
 >;
 
-export type ButtonVariant = Parameters<typeof DesignSystemButton>[0]['variant'];
-export type ButtonColor = Parameters<typeof DesignSystemButton>[0]['color'];
-
 export const Button = forwardRef<HTMLButtonElement, PropsWithChildren<ButtonProps>>(function Button(
-  {
-    id,
-    title,
-    disabled,
-    variant = 'primary',
-    color = 'first',
-    size = 'sm',
-    className,
-    icon,
-    isLoading = false,
-    'aria-label': ariaLabel,
-    'aria-busy': ariaBusy,
-    'aria-controls': ariaControls,
-    'aria-haspopup': ariaHasPopup,
-    'aria-expanded': ariaExpanded,
-    'aria-labelledby': ariaLabelledBy,
-    'aria-describedby': ariaDescribedBy,
-    style,
-    tabIndex,
-    fullWidth,
-    onClick,
-    onMouseDown,
-    children,
-  },
+  { id, disabled, isLoading = false, variant = 'primary', color = 'first', size = 'sm', children, ...props },
   ref,
 ) {
   return (
     <DesignSystemButton
       id={id}
-      title={title}
       disabled={disabled || isLoading}
       variant={variant}
       color={color}
       size={size}
-      icon={icon}
-      className={className}
-      aria-label={ariaLabel}
-      style={style}
-      tabIndex={tabIndex}
-      onClick={onClick}
-      onMouseDown={onMouseDown}
-      aria-busy={ariaBusy}
-      aria-controls={ariaControls}
-      aria-haspopup={ariaHasPopup}
-      aria-expanded={ariaExpanded}
-      aria-labelledby={ariaLabelledBy}
-      aria-describedby={ariaDescribedBy}
       ref={ref}
-      fullWidth={fullWidth}
+      {...props}
     >
       {isLoading && (
         <Spinner
