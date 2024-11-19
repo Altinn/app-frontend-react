405e23185b0de13d064eec7bcfe67cfc26de8fbd
Camilla Marie Dalan <camillamdalan@gmail.com>
2024-11-19 13:52:42 +0100

    removes padlock and readonly prop

--- src/app-components/Label/Label.tsx
+++ src/app-components/Label/Label.tsx
@@ -2,27 +2,16 @@ import React from 'react';
 import type { PropsWithChildren, ReactElement } from 'react';
 
 import { Label as DesignsystemetLabel } from '@digdir/designsystemet-react';
-import { PadlockLockedFillIcon } from '@navikt/aksel-icons';
 import type { LabelProps as DesignsystemetLabelProps } from '@digdir/designsystemet-react';
 
 type LabelProps = {
   label: string;
   required?: boolean;
-  readonly?: boolean;
   help?: ReactElement;
   className?: string;
 } & Pick<DesignsystemetLabelProps, 'htmlFor' | 'style'>;
 
-export function Label({
-  label,
-  required,
-  readonly,
-  htmlFor,
-  style,
-  help,
-  className,
-  children,
-}: PropsWithChildren<LabelProps>) {
+export function Label({ label, required, htmlFor, style, help, className, children }: PropsWithChildren<LabelProps>) {
   if (!label) {
     return children;
   }
@@ -36,12 +25,6 @@ export function Label({
       style={{ display: 'flex', flexDirection: 'column', gap: '0.5rem', ...style }}
     >
       <div style={{ display: 'flex', gap: '0.5rem' }}>
-        {readonly && (
-          <PadlockLockedFillIcon
-            aria-hidden
-            className='fds-textfield__readonly__icon'
-          />
-        )}
         {label}
         {required && ' *'}
         {help}
