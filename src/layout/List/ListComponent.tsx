c30f82c81e9d886b884625ec4eae6e450c4b7df5
Filippa Wallden <filippa.wallden@avanade.com>
2024-11-20 12:03:53 +0100

    Added componentType property + checkbox

--- src/layout/List/ListComponent.tsx
+++ src/layout/List/ListComponent.tsx
@@ -6,6 +6,7 @@ import { Heading, Radio, Table } from '@digdir/designsystemet-react';
 import cn from 'classnames';
 import type { DescriptionText } from '@altinn/altinn-design-system/dist/types/src/components/Pagination/Pagination';
 
+import { CustomCheckbox } from 'src/components/form/Checkbox';
 import { Description } from 'src/components/form/Description';
 import { RadioButton } from 'src/components/form/RadioButton';
 import { RequiredIndicator } from 'src/components/form/RequiredIndicator';
@@ -77,6 +78,7 @@ export const ListComponent = ({ node }: IListProps) => {
 
   const title = item.textResourceBindings?.title;
   const description = item.textResourceBindings?.description;
+  const component = item.componentType;
 
   if (isMobile) {
     return (
@@ -180,16 +182,29 @@ export const ListComponent = ({ node }: IListProps) => {
                   [classes.selectedRowCell]: isRowSelected(row),
                 })}
               >
-                <RadioButton
-                  className={classes.radio}
-                  aria-label={JSON.stringify(row)}
-                  onChange={() => {
-                    handleRowSelect({ selectedValue: row });
-                  }}
-                  value={JSON.stringify(row)}
-                  checked={isRowSelected(row)}
-                  name={node.id}
-                />
+                {component === 'CheckBoxes' ? (
+                  <CustomCheckbox
+                    className={classes.radio}
+                    aria-label={JSON.stringify(row)}
+                    onChange={() => {
+                      handleRowSelect({ selectedValue: row });
+                    }}
+                    value={JSON.stringify(row)}
+                    checked={isRowSelected(row)}
+                    name={node.id}
+                  />
+                ) : (
+                  <RadioButton
+                    className={classes.radio}
+                    aria-label={JSON.stringify(row)}
+                    onChange={() => {
+                      handleRowSelect({ selectedValue: row });
+                    }}
+                    value={JSON.stringify(row)}
+                    checked={isRowSelected(row)}
+                    name={node.id}
+                  />
+                )}
               </Table.Cell>
               {Object.keys(tableHeaders).map((key) => (
                 <Table.Cell
--- src/layout/List/config.ts
+++ src/layout/List/config.ts
@@ -132,6 +132,15 @@ export const Config = new CG.component({
   )
   .addProperty(
     new CG.prop(
+      'componentType',
+      new CG.enum('RadioButtons', 'CheckBoxes')
+        .optional({ default: 'RadioButtons' })
+        .setTitle('Use of checkbox or radio button')
+        .setDescription('Specify if the list should use checkboxes or radio buttons. '),
+    ),
+  )
+  .addProperty(
+    new CG.prop(
       'tableHeadersMobile',
       new CG.arr(new CG.str())
         .optional()
