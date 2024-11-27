3f43109b3945b5505ed8b770bddf3428b98df761
HauklandJ <hauklandjohannes@gmail.com>
2024-11-27 09:38:30 +0100

    additional validation tests

--- src/layout/OrganisationLookup/validation.test.ts
+++ src/layout/OrganisationLookup/validation.test.ts
@@ -7,4 +7,10 @@ describe('CheckValidOrgNr', () => {
   it('should return false when the orgNr is invalid', () => {
     expect(checkValidOrgnNr('143871668')).toBe(false);
   });
+  it('should return false when the orgNr is too short', () => {
+    expect(checkValidOrgnNr('12345678')).toBe(false);
+  });
+  it('should return false when the orgNr is too long', () => {
+    expect(checkValidOrgnNr('1234567890')).toBe(false);
+  });
 });
