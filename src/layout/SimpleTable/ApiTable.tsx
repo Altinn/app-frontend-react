d793f144280b7685e1ce65de2379451f6e30f0ca
Camilla Marie Dalan <camillamdalan@gmail.com>
2024-11-29 14:42:09 +0100

    updates table import casing

--- src/layout/SimpleTable/ApiTable.tsx
+++ src/layout/SimpleTable/ApiTable.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 import { Link } from '@digdir/designsystemet-react';
 import { pick } from 'dot-object';
 
-import { AppTable } from 'src/app-components/table/Table';
+import { AppTable } from 'src/app-components/Table/Table';
 import { Caption } from 'src/components/form/caption/Caption';
 import { useExternalApis } from 'src/features/externalApi/useExternalApi';
 import { Lang } from 'src/features/language/Lang';
 import { useLanguage } from 'src/features/language/useLanguage';
 import { useIsMobile } from 'src/hooks/useDeviceWidths';
 import { useNodeItem } from 'src/utils/layout/useNodeItem';
-import type { FormDataObject } from 'src/app-components/table/Table';
+import type { FormDataObject } from 'src/app-components/Table/Table';
 import type { PropsFromGenericComponent } from 'src/layout';
 import type { DataConfig } from 'src/layout/SimpleTable/config.generated';
 
