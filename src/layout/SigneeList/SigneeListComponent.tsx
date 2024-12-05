216b5e6b2eb28c0044bb5f2e8739832bb7dbda8e
Camilla Marie Dalan <camillamdalan@gmail.com>
2024-12-05 11:12:21 +0100

    WIP

---
+++ src/layout/SigneeList/SigneeListComponent.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import type { PropsFromGenericComponent } from 'src/layout';
+
+type SigneeListComponentProps = PropsFromGenericComponent<'SigneeList'>;
+
+export const SigneeListComponent: React.FC<SigneeListComponentProps> = () => <div>SigneeListComponent</div>;
