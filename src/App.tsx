26f33b5cb7faef76e89a06fed6866d605b6f25fa
Adam Haeger <adamgullerud@gmail.com>
2024-12-11 09:14:52 +0100

    added feature flags

--- src/App.tsx
+++ src/App.tsx
@@ -28,50 +28,44 @@ export const App = () => (
         element={<FormFirstPage />}
       />
     </Route>
+    <Route
+      path='/instance-selection/*'
+      element={<InstanceSelectionWrapper />}
+    />
 
-    <Route path='/instance-selection'>
+    <Route
+      path='/party-selection/*'
+      element={<PartySelection />}
+    />
+
+    <Route
+      path='/instance/:partyId/:instanceGuid/*'
+      element={
+        <InstanceProvider>
+          <Outlet />
+        </InstanceProvider>
+      }
+    >
       <Route
-        path='*'
-        element={<InstanceSelectionWrapper />}
+        path=':taskId/*'
+        element={<ProcessWrapper />}
       />
-    </Route>
-
-    <Route path='/party-selection'>
       <Route
-        path='*'
-        element={<PartySelection />}
+        path={TaskKeys.ProcessEnd}
+        element={<DefaultReceipt />}
       />
-    </Route>
-
-    <Route path='/instance/:partyId/:instanceGuid'>
-      <Route
-        path='*'
-        element={
-          <InstanceProvider>
-            <Outlet />
-          </InstanceProvider>
-        }
-      >
+      <Route path={TaskKeys.CustomReceipt}>
         <Route
-          path=':taskId/*'
-          element={<ProcessWrapper />}
-        />
-        <Route
-          path={TaskKeys.ProcessEnd}
-          element={<DefaultReceipt />}
-        />
-        <Route path={TaskKeys.CustomReceipt}>
-          <Route
-            path='*'
-            element={<CustomReceipt />}
-          />
-        </Route>
-        <Route
-          index
-          element={<NavigateToStartUrl />}
+          path='*'
+          element={<CustomReceipt />}
         />
       </Route>
+      <Route
+        index
+        element={<NavigateToStartUrl />}
+      />
     </Route>
+
     {/**
      * Redirects from legacy URLs to new URLs
      */}
@@ -86,3 +80,98 @@ export const App = () => (
     />
   </Routes>
 );
+
+// import React from 'react';
+// import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
+//
+// import { Form, FormFirstPage } from 'src/components/form/Form';
+// import { PresentationComponent } from 'src/components/presentation/Presentation';
+// import { NavigateToStartUrl, ProcessWrapper } from 'src/components/wrappers/ProcessWrapper';
+// import { Entrypoint } from 'src/features/entrypoint/Entrypoint';
+// import { InstanceProvider } from 'src/features/instance/InstanceContext';
+// import { PartySelection } from 'src/features/instantiate/containers/PartySelection';
+// import { InstanceSelectionWrapper } from 'src/features/instantiate/selection/InstanceSelection';
+// import { CustomReceipt, DefaultReceipt } from 'src/features/receipt/ReceiptContainer';
+// import { TaskKeys } from 'src/hooks/useNavigatePage';
+// import { PresentationType } from 'src/types';
+//
+// export const App = () => (
+//   <Routes>
+//     <Route element={<Entrypoint />}>
+//       <Route
+//         path=':pageKey'
+//         element={
+//           <PresentationComponent type={PresentationType.Stateless}>
+//             <Form />
+//           </PresentationComponent>
+//         }
+//       />
+//       <Route
+//         index
+//         element={<FormFirstPage />}
+//       />
+//     </Route>
+//
+//     <Route
+//       path='/instance-selection'
+//       element={<Entrypoint />}
+//     >
+//       <Route
+//         path='*'
+//         element={<InstanceSelectionWrapper />}
+//       />
+//     </Route>
+//
+//     <Route
+//       path='/party-selection'
+//       element={<Entrypoint />}
+//     >
+//       <Route
+//         path='*'
+//         element={<PartySelection />}
+//       />
+//     </Route>
+//
+//     <Route path='/instance/:partyId/:instanceGuid'>
+//       <Route
+//         path='*'
+//         element={
+//           <InstanceProvider>
+//             <Outlet />
+//           </InstanceProvider>
+//         }
+//       >
+//         <Route
+//           path=':taskId/*'
+//           element={<ProcessWrapper />}
+//         />
+//         <Route
+//           path={TaskKeys.ProcessEnd}
+//           element={<DefaultReceipt />}
+//         />
+//         <Route path={TaskKeys.CustomReceipt}>
+//           <Route
+//             path='*'
+//             element={<CustomReceipt />}
+//           />
+//         </Route>
+//         <Route
+//           index
+//           element={<NavigateToStartUrl />}
+//         />
+//       </Route>
+//     </Route>
+//     {/**
+//      * Redirects from legacy URLs to new URLs
+//      */}
+//     <Route
+//       path='/partyselection/*'
+//       element={
+//         <Navigate
+//           to='/party-selection/'
+//           replace={true}
+//         />
+//       }
+//     />
+//   </Routes>
+// );
--- src/index.tsx
+++ src/index.tsx
@@ -60,6 +60,10 @@ const router = createHashRouter(
   {
     future: {
       v7_relativeSplatPath: true,
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_skipActionErrorRevalidation: true,
     },
   },
 );
