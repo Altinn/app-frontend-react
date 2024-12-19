7954676aac87aa8f12ab1c0f3d5e03364dcd6258
Filippa Wallden <filippa.wallden@avanade.com>
2024-12-19 09:01:59 +0100

    summary2

--- src/layout/List/ListSummary.tsx
+++ src/layout/List/ListSummary.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import { Table } from '@digdir/designsystemet-react';
+
+import { Caption } from 'src/components/form/caption/Caption';
+import { useDataModelBindings } from 'src/features/formData/useDataModelBindings';
 import { Lang } from 'src/features/language/Lang';
 import { useUnifiedValidationsForNode } from 'src/features/validation/selectors/unifiedValidationsForNode';
 import { validationsOfSeverity } from 'src/features/validation/utils';
+import { EditButton } from 'src/layout/Summary2/CommonSummaryComponents/EditButton';
 import { SingleValueSummary } from 'src/layout/Summary2/CommonSummaryComponents/SingleValueSummary';
 import { useNodeItem } from 'src/utils/layout/useNodeItem';
 import type { LayoutNode } from 'src/utils/layout/LayoutNode';
@@ -18,6 +23,64 @@ export const ListSummary = ({ componentNode, isCompact, emptyFieldText }: ListCo
   const validations = useUnifiedValidationsForNode(componentNode);
   const errors = validationsOfSeverity(validations, 'error');
   const title = useNodeItem(componentNode, (i) => i.textResourceBindings?.title);
+
+  const { tableHeaders, dataModelBindings } = useNodeItem(componentNode);
+  const { formData } = useDataModelBindings(dataModelBindings, 1, 'raw');
+
+  const displayRows: unknown[] = [];
+  // @ts-expect-error Please replace with typechecking
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  formData?.saveToList?.forEach((row: any) => {
+    const { altinnRowId, ...rest } = row;
+    displayRows.push(rest);
+  });
+
+  if (displayRows.length > 0) {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 'var(--fds-spacing-6)',
+        }}
+      >
+        <Table>
+          <Caption title={<Lang id={title} />} />
+          <Table.Head>
+            <EditButton
+              style={{ marginLeft: 'auto', minWidth: 'unset' }}
+              componentNode={componentNode}
+              summaryComponentId={''}
+            />
+            <Table.Row>
+              {Object.entries(tableHeaders).map(([key, value]) => (
+                <Table.HeaderCell key={key}>
+                  <Lang id={value} />
+                </Table.HeaderCell>
+              ))}
+            </Table.Row>
+          </Table.Head>
+          <Table.Body>
+            {displayRows.map((row, rowIndex) => {
+              const rowItem = row as { array: unknown[] };
+              return (
+                <Table.Row key={rowIndex}>
+                  {Object.entries(tableHeaders).map(([key, value]) => (
+                    <Table.Cell
+                      key={key}
+                      align='left'
+                    >
+                      {rowItem[key]}
+                    </Table.Cell>
+                  ))}
+                </Table.Row>
+              );
+            })}
+          </Table.Body>
+        </Table>
+      </div>
+    );
+  }
 
   return (
     <SingleValueSummary
