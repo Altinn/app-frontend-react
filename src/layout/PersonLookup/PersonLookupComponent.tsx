08eec3deda9406063b232a3a096364bf2a4a779f
HauklandJ <hauklandjohannes@gmail.com>
2025-01-06 15:32:54 +0100

    validate tempName inline

--- src/layout/PersonLookup/PersonLookupComponent.tsx
+++ src/layout/PersonLookup/PersonLookupComponent.tsx
@@ -19,7 +19,7 @@ import { useBindingValidationsForNode } from 'src/features/validation/selectors/
 import { hasValidationErrors } from 'src/features/validation/utils';
 import { ComponentStructureWrapper } from 'src/layout/ComponentStructureWrapper';
 import classes from 'src/layout/PersonLookup/PersonLookupComponent.module.css';
-import { validateName, validatePersonLookupResponse, validateSsn } from 'src/layout/PersonLookup/validation';
+import { validatePersonLookupResponse, validateSsn } from 'src/layout/PersonLookup/validation';
 import { useLabel } from 'src/utils/layout/useLabel';
 import { useNodeItem } from 'src/utils/layout/useNodeItem';
 import { httpPost } from 'src/utils/network/networking';
@@ -85,7 +85,7 @@ export function PersonLookupComponent({ node, overrideDisplay }: PropsFromGeneri
   const [tempSsn, setTempSsn] = useState('');
   const [tempName, setTempName] = useState('');
   const [ssnErrors, setSsnErrors] = useState<string[]>();
-  const [nameErrors, setNameErrors] = useState<string[]>();
+  const [nameError, setNameError] = useState<string>();
 
   const bindingValidations = useBindingValidationsForNode(node);
   const { langAsString } = useLanguage();
@@ -97,16 +97,11 @@ export function PersonLookupComponent({ node, overrideDisplay }: PropsFromGeneri
   const { data, refetch: performLookup, isFetching } = useQuery(personLookupQueries.lookup(tempSsn, tempName));
 
   function handleValidateName(name: string) {
-    if (!validateName({ name })) {
-      const nameErrors = validateName.errors
-        ?.filter((error) => error.instancePath === '/name')
-        .map((error) => error.message)
-        .filter((it) => it != null);
-
-      setNameErrors(nameErrors);
+    if (name.length < 2) {
+      setNameError('person_lookup.validation_error_name_too_short');
       return false;
     }
-    setNameErrors(undefined);
+    setNameError(undefined);
     return true;
   }
 
@@ -225,7 +220,7 @@ export function PersonLookupComponent({ node, overrideDisplay }: PropsFromGeneri
           required={required}
           readOnly={hasSuccessfullyFetched}
           error={
-            (nameErrors?.length && <Lang id={nameErrors.join(' ')} />) ||
+            (nameError?.length && <Lang id={nameError} />) ||
             (hasValidationErrors(bindingValidations?.person_lookup_name) && (
               <ComponentValidations validations={bindingValidations?.person_lookup_name} />
             ))
