24f6e505b13994be075e90e51e74604aef9cdc44
Camilla Marie Dalan <camillamdalan@gmail.com>
2025-01-15 12:35:37 +0100

    adds reject button to panel if user has reject rights

--- src/layout/SigningStatusPanel/SigningPanel.tsx
+++ src/layout/SigningStatusPanel/SigningPanel.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import type { PropsWithChildren, ReactElement } from 'react';
 
-import { ErrorMessage, Heading, Paragraph } from '@digdir/designsystemet-react';
+import { ErrorMessage, Heading, Modal, Paragraph } from '@digdir/designsystemet-react';
 
+import { Button } from 'src/app-components/Button/Button';
 import { Panel } from 'src/app-components/Panel/Panel';
+import { useIsAuthorised } from 'src/features/instance/ProcessContext';
+import { useProcessNavigation } from 'src/features/instance/ProcessNavigationContext';
 import classes from 'src/layout/SigningStatusPanel/SigningStatusPanel.module.css';
-import type { Button } from 'src/app-components/Button/Button';
 import type { PanelProps } from 'src/app-components/Panel/Panel';
 
 type SigningPanelProps = {
@@ -13,7 +15,6 @@ type SigningPanelProps = {
   description?: string;
   variant?: PanelProps['variant'];
   actionButton: ReactElement<typeof Button>;
-  secondaryButton?: ReactElement<typeof Button>;
   errorMessage?: string;
 };
 
@@ -21,11 +22,12 @@ export function SigningPanel({
   heading,
   description,
   variant = 'info',
-  secondaryButton,
   actionButton,
   errorMessage,
   children,
 }: PropsWithChildren<SigningPanelProps>) {
+  const canReject = useIsAuthorised()('reject');
+
   return (
     <Panel
       variant={variant}
@@ -44,12 +46,63 @@ export function SigningPanel({
         {children}
         <div>
           <div className={classes.buttonContainer}>
-            {secondaryButton}
+            {canReject && <Reject />}
             {actionButton}
           </div>
           {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
         </div>
       </div>
     </Panel>
+  );
+}
+
+function Reject() {
+  const modalRef = useRef<HTMLDialogElement>(null);
+  const { next, busy } = useProcessNavigation() ?? {};
+
+  function handleReject() {
+    next?.({ action: 'reject', nodeId: 'reject-button' });
+  }
+
+  return (
+    <Modal.Root>
+      <Modal.Trigger asChild>
+        <Button
+          color='danger'
+          variant='secondary'
+          size='md'
+        >
+          Avbryt signering
+        </Button>
+      </Modal.Trigger>
+      <Modal.Dialog ref={modalRef}>
+        <Modal.Header>
+          <Heading size='xs'>Avbryt signeringsprosessen</Heading>
+        </Modal.Header>
+        <Modal.Content>
+          <Paragraph>
+            Ved å avbryte signeringsprosessen vil alle signaturer bli slettet og alle delegerte tilganger trukket
+            tilbake.
+          </Paragraph>
+        </Modal.Content>
+        <Modal.Footer>
+          <Button
+            color='danger'
+            disabled={busy}
+            size='md'
+            onClick={handleReject}
+          >
+            Avbryt signeringsprosessen
+          </Button>
+          <Button
+            variant='secondary'
+            size='md'
+            onClick={() => modalRef.current?.close()}
+          >
+            Lukk
+          </Button>
+        </Modal.Footer>
+      </Modal.Dialog>
+    </Modal.Root>
   );
 }
