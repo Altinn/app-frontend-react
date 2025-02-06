4f6178e9c8c4c0a7a33f3bcf8922e6eefa35bd98
Adam Haeger <adamgullerud@gmail.com>
2025-02-06 14:46:42 +0100

    wip

--- src/queries/appPrefetcher.ts
+++ src/queries/appPrefetcher.ts
@@ -1,5 +1,8 @@
 import { matchPath } from 'react-router-dom';
 
+import { useQuery } from '@tanstack/react-query';
+import type { UseQueryResult } from '@tanstack/react-query';
+
 import { usePrefetchQuery } from 'src/core/queries/usePrefetchQuery';
 import { getApplicationMetadataQueryDef } from 'src/features/applicationMetadata/ApplicationMetadataProvider';
 import { useApplicationSettingsQueryDef } from 'src/features/applicationSettings/ApplicationSettingsProvider';
@@ -9,6 +12,14 @@ import { getProcessQueryDef } from 'src/features/instance/ProcessContext';
 import { useOrgsQueryDef } from 'src/features/orgs/OrgsProvider';
 import { useCurrentPartyQueryDef, usePartiesQueryDef } from 'src/features/party/PartiesProvider';
 import { useProfileQueryDef } from 'src/features/profile/ProfileProvider';
+import { fetchInitialStateUrl } from 'src/next/App';
+import type { InitialState } from 'src/next/types/InitialState';
+
+export const useInitialStateQuery = (): UseQueryResult<InitialState, Error> =>
+  useQuery<InitialState, Error>({
+    queryKey: ['initialState'],
+    queryFn: fetchInitialStateUrl,
+  });
 
 /**
  * Prefetches requests that require no processed data to determine the url
@@ -30,6 +41,11 @@ export function AppPrefetcher() {
 
   usePrefetchQuery(useInstanceDataQueryDef(false, partyId, instanceGuid));
   usePrefetchQuery(getProcessQueryDef(instanceId));
+
+  usePrefetchQuery({
+    queryKey: ['initialState'],
+    queryFn: fetchInitialStateUrl,
+  });
 
   return null;
 }
