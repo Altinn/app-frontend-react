37801c3991d24a0dc3553137cd88d05c27735038
Adam Haeger <adamgullerud@gmail.com>
2025-02-17 10:33:16 +0100

    create instance button

--- src/next/pages/Instances.tsx
+++ src/next/pages/Instances.tsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 
 import { useStore } from 'zustand/index';
 
+import { useApiClient } from 'src/next/app/ApiClientContext';
+import { APP, ORG } from 'src/next/app/App';
 import { initialStateStore } from 'src/next/stores/settingsStore';
 import { useActiveInstancesQuery } from 'src/next/v1/queries/instanceQuery';
 
@@ -11,6 +13,18 @@ export const Instances = () => {
 
   const { data, error, isLoading } = useActiveInstancesQuery(`${settings.user.partyId}`);
 
+  const apiClient = useApiClient();
+
+  const { user } = useStore(initialStateStore);
+  const createIntance = async () => {
+    const res = await apiClient.org.instancesCreate(ORG, APP, {
+      instanceOwnerPartyId: user.partyId,
+      language: user.profileSettingPreference.language || '',
+    });
+    const data = await res.json();
+    console.log(JSON.stringify(data, null, 2));
+  };
+
   if (isLoading) {
     return <h2>Loading instance, please wait</h2>;
   }
@@ -18,6 +32,8 @@ export const Instances = () => {
   return (
     <div>
       <h1>Active instances</h1>
+
+      <button onClick={createIntance}>New instance</button>
 
       <ul>
         {data?.map((instance) => (
