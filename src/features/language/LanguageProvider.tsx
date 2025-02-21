a31d4d3ba0ea2c4ecb03478c1448d6ddb98bd360
Bjørnar Østtveit <bjosttveit@gmail.com>
2025-02-21 14:18:59 +0100

    comment in language provider

--- src/features/language/LanguageProvider.tsx
+++ src/features/language/LanguageProvider.tsx
@@ -114,7 +114,9 @@ export const SetShouldFetchAppLanguages = () => {
 };
 
 /**
- * AppRoutingContext is not provided yet, so we have to get this manually
+ * AppRoutingContext is not provided yet, so we have to get this manually.
+ * This unfortunately means that the value is not reactive, and will not update
+ * if this query param changes after initial load.
  */
 function getLanguageFromUrl() {
   const params = new URLSearchParams(window.location.hash.split('?')[1]);
