a1b6cf713608395f3885a4f7c91a5f2fdf701b4a
Pål Myran <paulmyran777@gmail.com>
2025-02-25 09:14:16 +0100

    Fix better tests

--- test/e2e/integration/component-library/list.ts
+++ test/e2e/integration/component-library/list.ts
@@ -10,105 +10,47 @@ describe('List component', () => {
     cy.gotoNavPage('Liste (tabell)');
   });
   it('Should be possible to select multiple rows', () => {
-    cy.get('input[name*="ListPage-ListWithCheckboxesComponent"]').should('have.length', 5);
-
-    cy.get('div[data-componentid*="ListPage-ListWithCheckboxesComponent"]').contains('td', 'Johanne').parent().click();
-    cy.get('div[data-componentid*="ListPage-ListWithCheckboxesComponent"]').contains('td', 'Kari').parent().click();
-    cy.get('div[data-componentid*="ListPage-ListWithCheckboxesComponent"]')
-      .contains('td', 'Johanne')
-      .parent()
-      .findByRole('checkbox')
-      .should('be.checked');
-    cy.get('div[data-componentid*="ListPage-ListWithCheckboxesComponent"]')
-      .contains('td', 'Kari')
-      .parent()
-      .findByRole('checkbox')
-      .should('be.checked');
+    cy.findAllByRole('cell', { name: 'Johanne' }).last().parent().click(); /*[1].parent().click();*/
+    cy.findAllByRole('cell', { name: 'Kari' }).last().parent().click();
+    cy.findAllByRole('cell', { name: 'Johanne' }).last().parent().findByRole('checkbox').should('be.checked');
+    cy.findAllByRole('cell', { name: 'Kari' }).last().parent().findByRole('checkbox').should('be.checked');
   });
 
   it('Should be possible to deselect rows', () => {
-    cy.get('input[name*="ListPage-ListWithCheckboxesComponent"]').should('have.length', 5);
-
-    cy.get('div[data-componentid*="ListPage-ListWithCheckboxesComponent"]').contains('td', 'Johanne').parent().click();
-    cy.get('div[data-componentid*="ListPage-ListWithCheckboxesComponent"]').contains('td', 'Kari').parent().click();
-    cy.get('div[data-componentid*="ListPage-ListWithCheckboxesComponent"]').contains('td', 'Johanne').parent().click();
-    cy.get('div[data-componentid*="ListPage-ListWithCheckboxesComponent"]')
-      .contains('td', 'Johanne')
-      .parent()
-      .findByRole('checkbox')
-      .should('not.be.checked');
-    cy.get('div[data-componentid*="ListPage-ListWithCheckboxesComponent"]')
-      .contains('td', 'Kari')
-      .parent()
-      .findByRole('checkbox')
-      .should('be.checked');
+    cy.findAllByRole('cell', { name: 'Johanne' }).last().parent().click();
+    cy.findAllByRole('cell', { name: 'Kari' }).last().parent().click();
+    cy.findAllByRole('cell', { name: 'Johanne' }).last().parent().click();
+    cy.findAllByRole('cell', { name: 'Kari' }).last().parent().findByRole('checkbox').should('be.checked');
+    cy.findAllByRole('cell', { name: 'Johanne' }).last().parent().findByRole('checkbox').should('not.be.checked');
   });
 
   it('Selections in list should apply to RepeatingGroup', () => {
-    cy.get('input[name*="ListPage-ListWithCheckboxesComponent"]').should('have.length', 5);
+    cy.findAllByRole('cell', { name: 'Johanne' }).last().parent().click();
+    cy.findAllByRole('cell', { name: 'Kari' }).last().parent().click();
+    cy.findAllByRole('cell', { name: 'Johanne' }).last().parent().click();
 
-    cy.get('div[data-componentid*="ListPage-ListWithCheckboxesComponent"]').contains('td', 'Johanne').parent().click();
-    cy.get('div[data-componentid*="ListPage-ListWithCheckboxesComponent"]').contains('td', 'Kari').parent().click();
-    cy.get('div[data-componentid*="ListPage-ListWithCheckboxesComponent"]').contains('td', 'Johanne').parent().click();
-
-    cy.get('div[data-componentid*="RepeatingGroupListWithCheckboxes"]').contains('td', 'Kari');
+    cy.findAllByRole('cell', { name: 'Kari' }).last().should('exist');
   });
 
   it('Removing from RepeatingGroup should deselect from List', () => {
-    cy.get('input[name*="ListPage-ListWithCheckboxesComponent"]').should('have.length', 5);
+    cy.findAllByRole('cell', { name: 'Kari' }).last().parent().click();
+    cy.findAllByRole('cell', { name: 'Kari' }).last().parent().findByRole('checkbox').should('be.checked');
 
-    cy.get('div[data-componentid*="ListPage-ListWithCheckboxesComponent"]').contains('td', 'Kari').parent().click();
+    cy.findAllByRole('cell', { name: 'Kari' }).last().parent().contains('td', 'Slett').findByRole('button').click();
 
-    cy.get('div[data-componentid*="ListPage-ListWithCheckboxesComponent"]')
-      .contains('td', 'Kari')
-      .parent()
-      .findByRole('checkbox')
-      .should('be.checked');
-
-    cy.get('div[data-componentid*="RepeatingGroupListWithCheckboxes"]')
-      .contains('td', 'Kari')
-      .parent()
-      .contains('td', 'Slett')
-      .findByRole('button')
-      .click();
-
-    cy.get('div[data-componentid*="ListPage-ListWithCheckboxesComponent"]')
-      .contains('td', 'Kari')
-      .parent()
-      .findByRole('checkbox')
-      .should('not.be.checked');
+    cy.findAllByRole('cell', { name: 'Kari' }).last().parent().findByRole('checkbox').should('not.be.checked');
   });
 
   it('Deselecting in List should remove from RepeatingGroup', () => {
-    cy.get('input[name*="ListPage-ListWithCheckboxesComponent"]').should('have.length', 5);
-
-    cy.get('div[data-componentid*="ListPage-ListWithCheckboxesComponent"]').contains('td', 'Kari').parent().click();
-
-    cy.get('div[data-componentid*="ListPage-ListWithCheckboxesComponent"]')
-      .contains('td', 'Kari')
-      .parent()
-      .findByRole('checkbox')
-      .should('be.checked');
-
-    cy.get('div[data-componentid*="RepeatingGroupListWithCheckboxes"]')
-      .contains('td', 'Kari')
-      .parent()
-      .contains('td', 'Rediger')
-      .findByRole('button')
-      .click();
-
+    cy.findAllByRole('cell', { name: 'Kari' }).last().parent().click();
+    cy.findAllByRole('cell', { name: 'Kari' }).last().parent().findByRole('checkbox').should('be.checked');
+    cy.findAllByRole('cell', { name: 'Kari' }).last().parent().contains('td', 'Rediger').findByRole('button').click();
     cy.findByRole('textbox', { name: /Surname/ }).type('Olsen');
     cy.findAllByRole('button', { name: /Lagre og lukk/ })
       .first()
       .click();
-
-    cy.get('div[data-componentid*="RepeatingGroupListWithCheckboxes"]')
-      .contains('td', 'Kari')
-      .parent()
-      .contains('td', 'Olsen');
-
-    cy.get('div[data-componentid*="ListPage-ListWithCheckboxesComponent"]').contains('td', 'Kari').parent().click();
-
+    cy.findAllByRole('cell', { name: 'Kari' }).last().parent().contains('td', 'Olsen');
+    cy.findAllByRole('cell', { name: 'Kari' }).last().parent().click();
     cy.get('div[data-componentid*="group-RepeatingGroupListWithCheckboxes"]').should('not.exist');
   });
 });
