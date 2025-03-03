0a59a7fca5186452086f08a3c14cea4a683ffe68
Bjørnar Østtveit <bjosttveit@gmail.com>
2025-03-03 19:21:00 +0100

    add subform to navigation

--- src/features/navigation/AppNavigation.tsx
+++ src/features/navigation/AppNavigation.tsx
@@ -17,7 +17,9 @@ import cn from 'classnames';
 
 import { ContextNotProvided } from 'src/core/contexts/context';
 import { useIsProcessing } from 'src/core/contexts/processingContext';
+import { useDataTypeFromLayoutSet } from 'src/features/form/layout/LayoutsContext';
 import { usePageGroups, usePageSettings } from 'src/features/form/layoutSettings/LayoutSettingsContext';
+import { useStrictDataElements } from 'src/features/instance/InstanceContext';
 import { useGetAltinnTaskType } from 'src/features/instance/ProcessContext';
 import { useProcessTaskId } from 'src/features/instance/useProcessTaskId';
 import { Lang } from 'src/features/language/Lang';
@@ -30,8 +32,19 @@ import {
   useValidationsForPages,
   useVisiblePages,
 } from 'src/features/navigation/utils';
-import { useIsReceiptPage, useIsSubformPage, useNavigationParam } from 'src/features/routing/AppRoutingContext';
+import {
+  useIsReceiptPage,
+  useIsSubformPage,
+  useNavigate,
+  useNavigationParam,
+} from 'src/features/routing/AppRoutingContext';
+import { isSubformValidation } from 'src/features/validation';
+import { useComponentValidationsForNode } from 'src/features/validation/selectors/componentValidationsForNode';
 import { useNavigatePage } from 'src/hooks/useNavigatePage';
+import { getSubformEntryName, useSubformDataSources } from 'src/layout/Subform/utils';
+import { NodesInternal, useNode } from 'src/utils/layout/NodesContext';
+import { useNodeItem } from 'src/utils/layout/useNodeItem';
+import type { ExprVal, ExprValToActualOrExpr } from 'src/features/expressions/types';
 import type {
   NavigationPageGroup,
   NavigationPageGroupMultiple,
@@ -39,6 +52,7 @@ import type {
   NavigationReceipt,
   NavigationTask,
 } from 'src/layout/common.generated';
+import type { IData } from 'src/types/shared';
 
 export function AppNavigation({ onNavigate }: { onNavigate?: () => void }) {
   const pageGroups = usePageGroups();
@@ -293,23 +307,21 @@ function PageGroupMultiple({
           className={cn(classes.groupChevron, { [classes.groupChevronOpen]: isOpen })}
         />
       </button>
-      {isOpen && (
-        <ul
-          id={listId}
-          aria-labelledby={buttonId}
-          className={classes.pageList}
-        >
-          {visiblePages.map((page) => (
-            <Page
-              key={page}
-              page={page}
-              onNavigate={onNavigate}
-              hasErrors={validations !== ContextNotProvided && validations.hasErrors.pages[page]}
-              isComplete={validations !== ContextNotProvided && validations.isCompleted.pages[page]}
-            />
-          ))}
-        </ul>
-      )}
+      <ul
+        id={listId}
+        aria-labelledby={buttonId}
+        className={cn(classes.pageList, { [classes.hidden]: !isOpen })}
+      >
+        {visiblePages.map((page) => (
+          <Page
+            key={page}
+            page={page}
+            onNavigate={onNavigate}
+            hasErrors={validations !== ContextNotProvided && validations.hasErrors.pages[page]}
+            isComplete={validations !== ContextNotProvided && validations.isCompleted.pages[page]}
+          />
+        ))}
+      </ul>
     </li>
   );
 }
@@ -424,6 +436,7 @@ function Page({
           <Lang id={page} />
         </span>
       </button>
+      <SubformsForPage pageKey={page} />
     </li>
   );
 }
@@ -472,5 +485,120 @@ function PageSymbol({
         />
       )}
     </div>
+  );
+}
+
+function SubformsForPage({ pageKey }: { pageKey: string }) {
+  const subformIds = NodesInternal.useLaxMemoSelector(({ nodeData }) =>
+    Object.values(nodeData)
+      .filter((node) => node.pageKey === pageKey && node.layout.type === 'Subform')
+      .map((node) => node.layout.id),
+  );
+
+  if (subformIds === ContextNotProvided || !subformIds.length) {
+    return null;
+  }
+
+  return subformIds.map((nodeId) => (
+    <SubformGroup
+      key={nodeId}
+      nodeId={nodeId}
+    />
+  ));
+}
+
+function SubformGroup({ nodeId }: { nodeId: string }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const node = useNode(nodeId);
+  if (!node?.isType('Subform')) {
+    // This should never happen, @see SubformsForPage
+    throw new Error(`Navigation expected component: "${nodeId}" to exist and be of type: "Subform"`);
+  }
+  const subformIdsWithError = useComponentValidationsForNode(node).find(isSubformValidation)?.subformDataElementIds;
+  const { layoutSet, textResourceBindings, entryName } = useNodeItem(node);
+  const dataType = useDataTypeFromLayoutSet(layoutSet);
+  if (!dataType) {
+    throw new Error(`Unable to find data type for subform with id ${nodeId}`);
+  }
+  const dataElements = useStrictDataElements(dataType);
+
+  if (!dataElements.length || !entryName) {
+    return null;
+  }
+
+  const buttonId = `navigation-button-${dataType}`;
+  const listId = `navigation-subform-list-${dataType}`;
+
+  return (
+    <div className={classes.subformContainer}>
+      <button
+        id={buttonId}
+        aria-expanded={isOpen}
+        aria-owns={listId}
+        onClick={() => setIsOpen((o) => !o)}
+        className={cn(classes.subformExpandButton, 'fds-focus')}
+      >
+        <span>
+          <Lang id={textResourceBindings?.title} />
+          &nbsp;({dataElements.length})
+        </span>
+        <ChevronDownIcon
+          aria-hidden
+          className={cn(classes.subformExpandChevron, { [classes.subformExpandChevronOpen]: isOpen })}
+        />
+      </button>
+      <ul
+        id={listId}
+        aria-labelledby={buttonId}
+        className={cn(classes.subformList, { [classes.hidden]: !isOpen })}
+      >
+        {dataElements.map((dataElement) => (
+          <SubformLink
+            key={dataElement.id}
+            entryName={entryName}
+            nodeId={nodeId}
+            dataElement={dataElement}
+            hasErrors={Boolean(subformIdsWithError?.includes(dataElement.id))}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function SubformLink({
+  entryName,
+  nodeId,
+  dataElement,
+  hasErrors,
+}: {
+  entryName: ExprValToActualOrExpr<ExprVal.String>;
+  nodeId: string;
+  dataElement: IData;
+  hasErrors: boolean;
+}) {
+  const { isAnyProcessing: disabled } = useIsProcessing();
+  const navigate = useNavigate();
+  const { subformDataSources, isSubformDataFetching, subformDataError } = useSubformDataSources(dataElement);
+
+  const subformEntryName =
+    !isSubformDataFetching && !subformDataError
+      ? getSubformEntryName(entryName, subformDataSources, { type: 'node', id: nodeId })
+      : null;
+
+  if (!subformEntryName) {
+    return null;
+  }
+
+  return (
+    <li>
+      <button
+        disabled={disabled}
+        className={cn(classes.subformLink, 'fds-focus')}
+        onClick={() => navigate(`${nodeId}/${dataElement.id}${hasErrors ? '?validate=true' : ''}`)}
+      >
+        {subformEntryName}
+      </button>
+    </li>
   );
 }
