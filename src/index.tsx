b65f59db49861277ed604dd57ee077637bf6427c
Adam Haeger <adamgullerud@gmail.com>
2025-03-12 09:28:34 +0100

    clean

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import 'src/wdyr';
 import 'src/features/baseurlinjection';
 import 'src/features/toggles';
 import 'src/features/logging';
