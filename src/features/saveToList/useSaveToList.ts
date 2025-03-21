e49e4820c2f87b6f65394b53f0bac7ddb5362c10
Pål Myran <paulmyran777@gmail.com>
2025-03-21 14:14:45 +0100

    Add functionality for toggling checkbox and hide row.

--- src/features/saveToList/useSaveToList.ts
+++ src/features/saveToList/useSaveToList.ts
@@ -12,15 +12,30 @@ type Row = Record<string, string | number | boolean>;
 export const useSaveToList = (node) => {
   const bindings = useNodeItem(node, (i) => i.dataModelBindings) as IDataModelBindingsForSaveTolistCheckbox;
   const isDeleted = bindings.isDeleted;
+  const isDeletedKey = isDeleted?.field.split('.').pop();
   const setLeafValue = FD.useSetLeafValue();
   const { formData } = useDataModelBindings(bindings, DEFAULT_DEBOUNCE_TIMEOUT, 'raw');
   const appendToList = FD.useAppendToList();
   const removeFromList = FD.useRemoveIndexFromList();
 
-  function isRowChecked(row: Row): boolean {
-    return (formData?.saveToList as Row[]).some((selectedRow) =>
+  const getObjectFromFormDataRow = (row: Row): Row | undefined =>
+    (formData?.saveToList as Row[]).find((selectedRow) =>
       Object.keys(row).every((key) => Object.hasOwn(selectedRow, key) && row[key] === selectedRow[key]),
     );
+
+  const getIndexFromFormDataRow = (row: Row) =>
+    (formData?.saveToList as Row[]).findIndex((selectedRow) => {
+      const { altinnRowId: _ } = selectedRow;
+      return Object.keys(row).every((key) => Object.hasOwn(selectedRow, key) && row[key] === selectedRow[key]);
+    });
+
+  function isRowChecked(row: Row): boolean {
+    const formDataObject = getObjectFromFormDataRow(row);
+
+    if (isDeletedKey) {
+      return !!formDataObject && !formDataObject[isDeletedKey];
+    }
+    return !!formDataObject;
   }
 
   const setList = (row) => {
@@ -28,17 +43,14 @@ export const useSaveToList = (node) => {
       return;
     }
     if (isRowChecked(row)) {
-      const index = (formData?.saveToList as Row[]).findIndex((selectedRow) => {
-        const { altinnRowId: _ } = selectedRow;
-        return Object.keys(row).every((key) => Object.hasOwn(selectedRow, key) && row[key] === selectedRow[key]);
-      });
+      const index = getIndexFromFormDataRow(row);
       if (isDeleted) {
         const pathSegments = bindings.isDeleted?.field.split('.');
         const lastElement = pathSegments?.pop();
         const newPath = `${pathSegments?.join('.')}[${index}].${lastElement}`;
         setLeafValue({
           reference: { ...bindings.isDeleted, field: newPath } as IDataModelReference,
-          newValue: !!isDeleted,
+          newValue: true,
         });
       } else {
         if (index >= 0) {
@@ -49,23 +61,35 @@ export const useSaveToList = (node) => {
         }
       }
     } else {
-      const uuid = uuidv4();
-      const next: Row = { [ALTINN_ROW_ID]: uuid };
-      if (isDeleted) {
-        next[isDeleted.field.split('.').reverse()[0]] = false;
-      }
-      for (const binding of Object.keys(bindings)) {
-        if (binding === 'simpleBinding') {
-          const propertyName = bindings.simpleBinding.field.split('.')[1];
-          next[propertyName] = row[propertyName];
-        } else if (binding !== 'saveToList' && binding !== 'label' && binding !== 'metadata') {
-          next[binding] = row[binding];
+      const formDataObject = getObjectFromFormDataRow(row);
+      if (formDataObject && isDeletedKey && formDataObject[isDeletedKey]) {
+        const index = getIndexFromFormDataRow(row);
+        const pathSegments = bindings.isDeleted?.field.split('.');
+        const lastElement = pathSegments?.pop();
+        const newPath = `${pathSegments?.join('.')}[${index}].${lastElement}`;
+        setLeafValue({
+          reference: { ...bindings.isDeleted, field: newPath } as IDataModelReference,
+          newValue: false,
+        });
+      } else {
+        const uuid = uuidv4();
+        const next: Row = { [ALTINN_ROW_ID]: uuid };
+        if (isDeletedKey) {
+          next[isDeletedKey] = false;
         }
+        for (const binding of Object.keys(bindings)) {
+          if (binding === 'simpleBinding') {
+            const propertyName = bindings.simpleBinding.field.split('.')[1];
+            next[propertyName] = row[propertyName];
+          } else if (binding !== 'saveToList' && binding !== 'label' && binding !== 'metadata') {
+            next[binding] = row[binding];
+          }
+        }
+        appendToList({
+          reference: bindings.saveToList,
+          newValue: next,
+        });
       }
-      appendToList({
-        reference: bindings.saveToList,
-        newValue: next,
-      });
     }
   };
 
