da6e0349d3d183fb64e41ff822b255ea59f402c6
HauklandJ <hauklandjohannes@gmail.com>
2025-03-26 14:18:01 +0100

    update the org check logic

--- src/layout/SigningStatusPanel/PanelAwaitingCurrentUserSignature.tsx
+++ src/layout/SigningStatusPanel/PanelAwaitingCurrentUserSignature.tsx
@@ -116,7 +116,7 @@ export function AwaitingCurrentUserSignaturePanel({
       description={<Lang id={checkboxDescription} />}
       errorMessage={error ? <Lang id='signing.error_signing' /> : undefined}
     >
-      {authorizedOrganisationDetails && authorizedOrganisationDetails.organisations.length > 0 && (
+      {!!authorizedOrganisationDetails?.organisations?.length && (
         <Radio.Group
           legend='Hvem ønsker du å signere på vegne av?'
           hideLegend={false}
