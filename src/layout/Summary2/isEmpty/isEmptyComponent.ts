5fbbf896c9bba3fb2534d3e233d26982a22e3f44
Ole Martin Handeland <git@olemartin.org>
2025-05-05 21:20:25 +0200

    A much better empty-check will read the raw data and also consider rows/objects as non-empty

--- src/layout/Summary2/isEmpty/isEmptyComponent.ts
+++ src/layout/Summary2/isEmpty/isEmptyComponent.ts
@@ -1,13 +1,13 @@
-import { useDataModelBindings } from 'src/features/formData/useDataModelBindings';
+import { FD } from 'src/features/formData/FormDataWrite';
 import { useNodeItem } from 'src/utils/layout/useNodeItem';
 import type { LayoutNode } from 'src/utils/layout/LayoutNode';
 
 export function useHasDataInBindings(node: LayoutNode) {
   const dataModelBindings = useNodeItem(node, (i) => i.dataModelBindings);
-  const { formData } = useDataModelBindings(dataModelBindings);
+  const formData = FD.useFreshBindings(dataModelBindings, 'raw');
 
-  // Checks if there is data in any of the string-convertable data model binding
-  return Object.values(formData).some((value) => value?.length < 1);
+  // Checks if there is data in any of the data model binding
+  return Object.values(formData).some((value) => value !== undefined && value !== null && value !== '');
 }
 
 export function useHasNoDataInBindings(node: LayoutNode) {
