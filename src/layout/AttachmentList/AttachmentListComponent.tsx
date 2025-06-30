9711345fd021d1cc66b07ca92dd58732b6bec433
Camilla Marie Dalan <camillamdalan@gmail.com>
2025-06-30 15:25:50 +0200

    don't create a title component if supplied title does not exist or is an empty string

--- src/layout/AttachmentList/AttachmentListComponent.tsx
+++ src/layout/AttachmentList/AttachmentListComponent.tsx
@@ -68,12 +68,14 @@ export function AttachmentListComponent({ node }: IAttachmentListProps) {
 
   const displayAttachments = toDisplayAttachments([...pdfAttachments, ...filteredAttachments]);
 
+  const title = textResourceBindings?.title ? <Lang id={textResourceBindings?.title} /> : undefined;
+
   return (
     <ComponentStructureWrapper node={node}>
       {groupAttachments ? (
         <AttachmentGroupings
           attachments={displayAttachments}
-          title={<Lang id={textResourceBindings?.title} />}
+          title={title}
           hideCollapsibleCount={true}
           showLinks={showLinks}
           showDescription={showDescription}
@@ -81,7 +83,7 @@ export function AttachmentListComponent({ node }: IAttachmentListProps) {
       ) : (
         <AltinnAttachments
           attachments={displayAttachments}
-          title={<Lang id={textResourceBindings?.title} />}
+          title={title}
           showLinks={showLinks}
           showDescription={showDescription}
         />
