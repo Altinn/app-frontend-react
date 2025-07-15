288e0ea8c25886d933e37e4c9d1ea0534888083a
Ole Martin Handeland <git@olemartin.org>
2025-07-15 10:31:39 +0200

    Fixing build setup

--- webpack.common.js
+++ webpack.common.js
@@ -21,10 +21,8 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?/,
+        exclude: /node_modules/,
         use: [
-          {
-            loader: 'babel-loader',
-          },
           {
             loader: reactCompilerLoader,
             options: defineReactCompilerLoaderOption({}),
