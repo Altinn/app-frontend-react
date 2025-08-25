8ce9731ad9e53e9ab74c7e8ddbb3601a6efbd156
Ole Martin Handeland <git@olemartin.org>
2025-08-25 15:12:31 +0200

    Deduplicating some code, adding a test to make sure both automatic and custom PDFs work for a given subform element

--- test/e2e/integration/subform-test/pdf.ts
+++ test/e2e/integration/subform-test/pdf.ts
@@ -5,32 +5,36 @@ import type { ILayoutCollection } from 'src/layout/layout';
 
 const appFrontend = new AppFrontend();
 
+function fillTwoSubforms() {
+  cy.findByRole('textbox', { name: /navn/i }).type('Per');
+  cy.findByRole('textbox', { name: /alder/i }).type('28');
+
+  cy.findByRole('button', { name: /legg til moped/i }).clickAndGone();
+  cy.findByRole('textbox', { name: /registreringsnummer/i }).type('ABC123');
+  cy.findByRole('textbox', { name: /merke/i }).type('Digdir');
+  cy.findByRole('textbox', { name: /modell/i }).type('Scooter2000');
+  cy.findByRole('textbox', { name: /produksjonsår/i }).type('2024');
+  cy.findByRole('button', { name: /ferdig/i }).clickAndGone();
+
+  cy.findByRole('button', { name: /legg til moped/i }).clickAndGone();
+  cy.findByRole('textbox', { name: /registreringsnummer/i }).type('XYZ987');
+  cy.findByRole('textbox', { name: /merke/i }).type('Altinn');
+  cy.findByRole('textbox', { name: /modell/i }).type('3.0');
+  cy.findByRole('textbox', { name: /produksjonsår/i }).type('2030');
+  cy.findByRole('button', { name: /ferdig/i }).clickAndGone();
+}
+
 describe('Subform test', () => {
   beforeEach(() => {
     cy.startAppInstance(appFrontend.apps.subformTest, { authenticationLevel: '1' });
   });
 
-  it('PDF should include subforms', () => {
-    cy.findByRole('textbox', { name: /navn/i }).type('Per');
-    cy.findByRole('textbox', { name: /alder/i }).type('28');
-
-    cy.findByRole('button', { name: /legg til moped/i }).clickAndGone();
-    cy.findByRole('textbox', { name: /registreringsnummer/i }).type('ABC123');
-    cy.findByRole('textbox', { name: /merke/i }).type('Digdir');
-    cy.findByRole('textbox', { name: /modell/i }).type('Scooter2000');
-    cy.findByRole('textbox', { name: /produksjonsår/i }).type('2024');
-    cy.findByRole('button', { name: /ferdig/i }).clickAndGone();
-
-    cy.findByRole('button', { name: /legg til moped/i }).clickAndGone();
-    cy.findByRole('textbox', { name: /registreringsnummer/i }).type('XYZ987');
-    cy.findByRole('textbox', { name: /merke/i }).type('Altinn');
-    cy.findByRole('textbox', { name: /modell/i }).type('3.0');
-    cy.findByRole('textbox', { name: /produksjonsår/i }).type('2030');
-    cy.findByRole('button', { name: /ferdig/i }).clickAndGone();
-
+  it('PDF should include subforms + single-subform PDFs should work', () => {
+    fillTwoSubforms();
     cy.testPdf({
       snapshotName: 'subform',
       enableResponseFuzzing: true,
+      returnToForm: true,
       callback: () => {
         cy.getSummary('Navn').should('contain.text', 'Per');
         cy.getSummary('Alder').should('contain.text', '28 år');
@@ -44,6 +48,55 @@ describe('Subform test', () => {
         cy.getSummary('Merke').eq(1).should('contain.text', 'Altinn');
         cy.getSummary('Modell').eq(1).should('contain.text', '3.0');
         cy.getSummary('Produksjonsår').eq(1).should('contain.text', '2030');
+      },
+    });
+
+    cy.findAllByRole('button', { name: /endre/i }).first().clickAndGone();
+    cy.findByRole('textbox', { name: /registreringsnummer/i }).should('have.value', 'ABC123');
+
+    cy.testPdf({
+      snapshotName: 'single-subform',
+      enableResponseFuzzing: true,
+      returnToForm: true,
+      buildUrl: buildUrlForSingleSubform,
+      callback: () => {
+        cy.get('#moped-blurb').should(
+          'contain.text',
+          'Fyll inn dette skjemaet for å registrere en moped i din garasje',
+        );
+        cy.getSummary('Navn').should('not.exist');
+        cy.getSummary('Alder').should('not.exist');
+
+        cy.getSummary('Registreringsnummer').should('contain.text', 'ABC123');
+        cy.getSummary('Registreringsnummer').should('not.contain.text', 'XYZ987');
+        cy.getSummary('Merke').should('contain.text', 'Digdir');
+        cy.getSummary('Modell').should('contain.text', 'Scooter2000');
+        cy.getSummary('Produksjonsår').should('contain.text', '2024');
+      },
+    });
+
+    cy.intercept('GET', '**/api/layoutsettings/moped-subform', (req) => {
+      req.on('response', (res) => {
+        const body = JSON.parse(res.body) as ILayoutSettings;
+        body.pages.pdfLayoutName = 'moped-pdf'; // Forces PDF engine to use a tailor-made layout
+        res.send(body);
+      });
+    }).as('settings');
+
+    cy.testPdf({
+      snapshotName: 'single-subform-custom',
+      enableResponseFuzzing: true,
+      buildUrl: buildUrlForSingleSubform,
+      callback: () => {
+        cy.get('#moped-blurb').should('not.exist');
+        cy.getSummary('Navn').should('not.exist');
+        cy.getSummary('Alder').should('not.exist');
+
+        cy.getSummary('Registreringsnummer').should('contain.text', 'ABC123');
+        cy.getSummary('Registreringsnummer').should('not.contain.text', 'XYZ987');
+        cy.getSummary('Merke').should('contain.text', 'Digdir');
+        cy.getSummary('Modell').should('contain.text', 'Scooter2000');
+        cy.getSummary('Produksjonsår').should('contain.text', '2024');
       },
     });
   });
@@ -97,27 +150,12 @@ describe('Subform test', () => {
     );
 
     cy.waitUntilSaved();
-
-    cy.findByRole('textbox', { name: /navn/i }).type('Per');
-    cy.findByRole('textbox', { name: /alder/i }).type('28');
-
-    cy.findByRole('button', { name: /legg til moped/i }).clickAndGone();
-    cy.findByRole('textbox', { name: /registreringsnummer/i }).type('ABC123');
-    cy.findByRole('textbox', { name: /merke/i }).type('Digdir');
-    cy.findByRole('textbox', { name: /modell/i }).type('Scooter2000');
-    cy.findByRole('textbox', { name: /produksjonsår/i }).type('2024');
-    cy.findByRole('button', { name: /ferdig/i }).clickAndGone();
-
-    cy.findByRole('button', { name: /legg til moped/i }).clickAndGone();
-    cy.findByRole('textbox', { name: /registreringsnummer/i }).type('XYZ987');
-    cy.findByRole('textbox', { name: /merke/i }).type('Altinn');
-    cy.findByRole('textbox', { name: /modell/i }).type('3.0');
-    cy.findByRole('textbox', { name: /produksjonsår/i }).type('2030');
-    cy.findByRole('button', { name: /ferdig/i }).clickAndGone();
+    fillTwoSubforms();
 
     cy.testPdf({
       snapshotName: 'subform',
       enableResponseFuzzing: true,
+      returnToForm: true,
       callback: () => {
         cy.getSummary('Navn').should('contain.text', 'Per');
         cy.getSummary('Alder').should('contain.text', '28 år');
@@ -134,3 +172,17 @@ describe('Subform test', () => {
     });
   });
 });
+
+function buildUrlForSingleSubform(href: string) {
+  if (!href.includes('/utfylling/') && !href.includes('/whatever/')) {
+    // We replace this with 'whatever' to make sure we can still load the PDF whatever the main page
+    // name is. It should not matter inside this subform.
+    throw new Error('Expected URL to contain /utfylling/ but it was not found');
+  }
+
+  if (!href.endsWith('/moped-utfylling')) {
+    throw new Error('Expected URL to end with /moped-utfylling but it was not found');
+  }
+
+  return href.replace('/utfylling/', '/whatever/').replace('/moped-utfylling', '/?pdf=1');
+}
