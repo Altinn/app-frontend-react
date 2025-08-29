7a2e26b7bb232252a2b9e53297d16fbe0cf4773c
Magnus Revheim Martinsen <mrmartinsen.96@gmail.com>
2025-08-29 10:40:47 +0200

    cropper first iteration

---
+++ src/layout/ImageUpload/config.ts
@@ -0,0 +1,21 @@
+import { CG } from 'src/codegen/CG';
+import { CompCategory } from 'src/layout/common';
+
+export const Config = new CG.component({
+  category: CompCategory.Presentation,
+  capabilities: {
+    renderInTable: true,
+    renderInButtonGroup: false,
+    renderInAccordion: true,
+    renderInAccordionGroup: false,
+    renderInTabs: true,
+    renderInCards: true,
+    renderInCardsMedia: false,
+  },
+  functionality: {
+    customExpressions: true,
+  },
+})
+  .makeSummarizable()
+  .extendTextResources(CG.common('TRBLabel'))
+  .addSummaryOverrides();
