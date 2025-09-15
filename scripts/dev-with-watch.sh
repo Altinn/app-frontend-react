ece92a0632fa2df7c59a0e798d52f6988a84f4de
Ole Martin Handeland <git@olemartin.org>
2025-09-15 16:45:59 +0200

    No need to run yarn gen twice

--- scripts/dev-with-watch.sh
+++ scripts/dev-with-watch.sh
@@ -6,7 +6,7 @@ cleanup() {
 }
 
 trap cleanup SIGINT SIGTERM EXIT
-yarn gen
 yarn gen:watch &
 cross-env NODE_ENV=development webpack-dev-server --config webpack.config.development.js --mode development --progress
 cleanup
+]
