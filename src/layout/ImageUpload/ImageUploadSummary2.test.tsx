5be31873237412d889c2c2dad2026bd60a60cced
lassopicasso <lars.walderhaug@digdir.no>
2025-10-07 08:37:15 +0200

    update tests for new summary of image upload

--- src/layout/ImageUpload/ImageUploadSummary2.test.tsx
+++ src/layout/ImageUpload/ImageUploadSummary2.test.tsx
@@ -3,35 +3,41 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 
 import { getAttachmentsMock } from 'src/__mocks__/getAttachmentsMock';
-import * as summaryHooks from 'src/layout/FileUpload/Summary/summary';
+import { UploadedAttachment } from 'src/features/attachments';
 import { ImageUploadSummary2 } from 'src/layout/ImageUpload/ImageUploadSummary2';
+import * as useImageFile from 'src/layout/ImageUpload/useImageFile';
 import { renderGenericComponentTest, RenderGenericComponentTestProps } from 'src/test/renderWithProviders';
 
 const targetBaseComponentId = 'mock-id';
-const mockAttachment = getAttachmentsMock({ count: 1, fileSize: 500 })[0];
 
 describe('ImageUploadSummary2', () => {
   beforeEach(() => jest.restoreAllMocks());
 
   it('renders label', async () => {
     await renderImageUploadSummary2();
-    expect(screen.getByText('mock.label')).toBeInTheDocument();
-  });
-
-  it('renders required symbol when required', async () => {
-    await renderImageUploadSummary2({ required: true });
-    expect(screen.getByText('*')).toBeInTheDocument();
+    expect(screen.getByText('mock.label', { selector: 'span' })).toBeInTheDocument();
   });
 
   it('renders empty value text when no attachment', async () => {
     await renderImageUploadSummary2();
-    expect(screen.getByText('Du har ikke lagt inn informasjon her')).toBeInTheDocument();
+    expect(screen.getByText('Du har ikke lastet opp noe bilde')).toBeInTheDocument();
   });
 
-  it('renders attachment table when attachment exists', async () => {
-    jest.spyOn(summaryHooks, 'useUploaderSummaryData').mockReturnValue([mockAttachment]);
+  it('renders image when attachment exists', async () => {
+    const mockAttachment = getAttachmentsMock({ count: 1, fileSize: 500 })[0] as UploadedAttachment;
+    jest.spyOn(useImageFile, 'useImageFile').mockReturnValue({
+      storedImage: mockAttachment,
+      imageUrl: 'https://mock.url/image.png',
+      saveImage: jest.fn(),
+      deleteImage: jest.fn(),
+    });
+
     await renderImageUploadSummary2();
-    expect(screen.getByText(mockAttachment.data.filename!)).toBeInTheDocument();
+
+    const img = screen.getByRole('img') as HTMLImageElement;
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe('https://mock.url/image.png');
+    expect(img.alt).toBe(mockAttachment.data.filename);
   });
 });
 
