05defd2f38e585b7bdea64a32b7a217e76aa0dca
Ole Martin Handeland <git@olemartin.org>
2025-12-02 16:07:09 +0100

    Checking if we're in the current task (i.e. that we're not navigating to another one)

--- src/layout/SigningActions/ValidateSigningTaskType.tsx
+++ src/layout/SigningActions/ValidateSigningTaskType.tsx
@@ -1,7 +1,9 @@
 import { useEffect } from 'react';
 
-import { useTaskTypeFromBackend } from 'src/features/instance/useProcessQuery';
+import { useTaskOverrides } from 'src/core/contexts/TaskOverrides';
+import { useProcessQuery, useTaskTypeFromBackend } from 'src/features/instance/useProcessQuery';
 import { useLanguage } from 'src/features/language/useLanguage';
+import { useNavigationParam } from 'src/hooks/navigation';
 import { ProcessTaskType } from 'src/types';
 import { NodesInternal } from 'src/utils/layout/NodesContext';
 import type { NodeValidationProps } from 'src/layout/layout';
@@ -10,16 +12,25 @@ type Props = NodeValidationProps<'SigningActions' | 'SigningDocumentList' | 'Sig
 
 export function ValidateSigningTaskType(props: Props) {
   const taskType = useTaskTypeFromBackend();
+  const isInProcessTask = useIsInProcessTask();
   const addError = NodesInternal.useAddError();
   const { langAsString } = useLanguage();
   const error = langAsString('signing.wrong_task_error', [props.intermediateItem.type]);
 
   useEffect(() => {
-    if (taskType !== ProcessTaskType.Signing) {
+    if (taskType !== ProcessTaskType.Signing && isInProcessTask) {
       addError(error, props.intermediateItem.id, 'node');
       window.logErrorOnce(`Validation error for '${props.intermediateItem.id}': ${error}`);
     }
-  }, [addError, error, props.intermediateItem.id, props.intermediateItem.type, taskType]);
+  }, [addError, error, isInProcessTask, props.intermediateItem.id, props.intermediateItem.type, taskType]);
 
   return null;
+}
+
+function useIsInProcessTask() {
+  const overriddenTaskId = useTaskOverrides()?.taskId;
+  const processTaskId = useProcessQuery().data?.currentTask?.elementId;
+  const urlTaskId = useNavigationParam('taskId');
+
+  return (overriddenTaskId ?? urlTaskId) === processTaskId && processTaskId !== undefined;
 }
