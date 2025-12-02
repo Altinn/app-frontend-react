ef690f0b52cccd8c4f45dcfafe7f9de5e9ba86e8
Ole Martin Handeland <git@olemartin.org>
2025-12-02 15:59:01 +0100

    Extracting duplicated code snippet to common component

---
+++ src/layout/SigningActions/ValidateSigningTaskType.tsx
@@ -0,0 +1,21 @@
+import { useTaskTypeFromBackend } from 'src/features/instance/useProcessQuery';
+import { useLanguage } from 'src/features/language/useLanguage';
+import { ProcessTaskType } from 'src/types';
+import { NodesInternal } from 'src/utils/layout/NodesContext';
+import type { NodeValidationProps } from 'src/layout/layout';
+
+type Props = NodeValidationProps<'SigningActions' | 'SigningDocumentList' | 'SigneeList'>;
+
+export function ValidateSigningTaskType(props: Props) {
+  const taskType = useTaskTypeFromBackend();
+  const addError = NodesInternal.useAddError();
+  const { langAsString } = useLanguage();
+
+  if (taskType !== ProcessTaskType.Signing) {
+    const error = langAsString('signing.wrong_task_error', [props.intermediateItem.type]);
+    addError(error, props.intermediateItem.id, 'node');
+    window.logErrorOnce(`Validation error for '${props.intermediateItem.id}': ${error}`);
+  }
+
+  return null;
+}
