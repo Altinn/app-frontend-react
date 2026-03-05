cd3f8eb54113024a55c453312093825963268e13
Magnus Revheim Martinsen <mrmartinsen.96@gmail.com>
2026-03-05 13:50:05 +0100

    dont auto call process next regardless of backend version

--- src/features/payment/PaymentProvider.tsx
+++ src/features/payment/PaymentProvider.tsx
@@ -4,7 +4,6 @@ import type { ReactNode } from 'react';
 import type { AxiosError } from 'axios';
 
 import { Loader } from 'src/core/loading/Loader';
-import { useApplicationMetadata } from 'src/features/applicationMetadata/ApplicationMetadataProvider';
 import { useProcessNext } from 'src/features/instance/useProcessNext';
 import { usePaymentInformation } from 'src/features/payment/PaymentInformationProvider';
 import { PaymentStatus } from 'src/features/payment/types';
@@ -14,7 +13,6 @@ import { useNavigationParam } from 'src/hooks/navigation';
 import { useIsPdf } from 'src/hooks/useIsPdf';
 import { useOurEffectEvent } from 'src/hooks/useOurEffectEvent';
 import { useShallowMemo } from 'src/hooks/useShallowMemo';
-import { appSupportsPaymentWebhook } from 'src/utils/versioning/versions';
 
 type PaymentContextProps = {
   performPayment: () => void;
@@ -62,9 +60,6 @@ function PaymentNavigation() {
   const { performPayment, skipPayment } = usePayment();
   const instanceGuid = useNavigationParam('instanceGuid');
 
-  const applicationMetadata = useApplicationMetadata();
-  const supportsPaymentWebhook = appSupportsPaymentWebhook(applicationMetadata?.altinnNugetVersion);
-
   const paymentDoesNotExist = paymentInfo?.status === PaymentStatus.Uninitialized;
   const isPaymentProcess = useIsPayment();
 
@@ -86,15 +81,6 @@ function PaymentNavigation() {
       paymentInitiatedForInstance.delete(instanceGuid);
     }
   }, [isPaymentProcess, paymentDoesNotExist, performPayment, isPdf, instanceGuid, paymentInfo?.status]);
-
-  const paymentCompleted = paymentInfo?.status === PaymentStatus.Paid || paymentInfo?.status === PaymentStatus.Skipped;
-
-  // If when landing on payment task, PaymentStatus is Paid or Skipped, and is not handled by webhook go to next task
-  useEffect(() => {
-    if (isPaymentProcess && paymentCompleted && !isPdf && !supportsPaymentWebhook) {
-      skipPayment();
-    }
-  }, [isPaymentProcess, paymentCompleted, skipPayment, isPdf, supportsPaymentWebhook]);
 
   return null;
 }
