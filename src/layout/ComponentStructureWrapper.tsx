599ca8f4e9ccf620c5df1c433e8b56cce9c7e6cb
Jamal Alabdullah <jamal.mustafa.alabdullah@digdir.no>
2026-03-26 10:23:00 +0100

    refactor

--- src/layout/ComponentStructureWrapper.tsx
+++ src/layout/ComponentStructureWrapper.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import type { PropsWithChildren } from 'react';
 
-import cn from 'classnames';
-
 import { Flex } from 'src/app-components/Flex/Flex';
 import { Label } from 'src/components/label/Label';
 import { AllComponentValidations } from 'src/features/validation/ComponentValidations';
-import classes from 'src/layout/ComponentStructureWrapper.module.css';
 import { useFormComponentCtx } from 'src/layout/FormComponentContext';
 import { getComponentDef } from 'src/layout/index';
 import { useIndexedId } from 'src/utils/layout/DataModelLocation';
@@ -15,13 +12,6 @@ import type { LabelProps } from 'src/components/label/Label';
 import type { IGridSize, IGridStyling } from 'src/layout/common.generated';
 
 const GRID_BREAKPOINT_KEYS = ['xs', 'sm', 'md', 'lg', 'xl'] as const;
-
-const EXACT_WIDTH_CLASS_BY_BREAKPOINT: Partial<Record<(typeof GRID_BREAKPOINT_KEYS)[number], string>> = {
-  xs: classes.useXs,
-  sm: classes.useSm,
-  md: classes.useMd,
-  lg: classes.useLg,
-};
 
 function mergeMaxGridSizePair(
   innerSize: IGridSize | undefined,
@@ -59,49 +49,6 @@ function mergeMaxGridStyling(
   return result;
 }
 
-function inputGridSizeAndExactWidth(
-  inner: IGridStyling | undefined,
-  outer: IGridStyling,
-): { size: IGridStyling; exactPercentWidthStyle: React.CSSProperties; exactClassName: string | undefined } {
-  const base: IGridStyling = { xs: 12, ...inner };
-  const result: IGridStyling = { ...base };
-  const cssVars: Record<string, string> = {};
-
-  for (const bp of GRID_BREAKPOINT_KEYS) {
-    const innerVal = inner?.[bp];
-    const outerVal = outer[bp];
-    if (typeof innerVal === 'number' && typeof outerVal === 'number') {
-      if (innerVal === outerVal) {
-        result[bp] = 12 as IGridStyling[typeof bp];
-      } else if (innerVal < outerVal) {
-        if (bp === 'xl') {
-          const scaled = Math.min(12, Math.max(1, Math.round((12 * innerVal) / outerVal)));
-          result[bp] = scaled as IGridStyling[typeof bp];
-        } else {
-          delete result[bp];
-          cssVars[`--input-pct-${bp}`] = `${(innerVal / outerVal) * 100}%`;
-        }
-      }
-    }
-  }
-
-  const hasExact = Object.keys(cssVars).length > 0;
-  const exactClassName = hasExact
-    ? cn(
-        classes.inputFlexExact,
-        ...GRID_BREAKPOINT_KEYS.map((bp) =>
-          cssVars[`--input-pct-${bp}`] ? EXACT_WIDTH_CLASS_BY_BREAKPOINT[bp] : null,
-        ),
-      )
-    : undefined;
-
-  return {
-    size: result,
-    exactPercentWidthStyle: cssVars as React.CSSProperties,
-    exactClassName,
-  };
-}
-
 type ComponentStructureWrapperProps = {
   baseComponentId: string;
   label?: LabelProps;
@@ -126,16 +73,6 @@ export function ComponentStructureWrapper({
   const innerGrid = grid?.innerGrid;
   const validationGrid = grid?.validationGrid ?? innerGrid;
   const contentGridSize: IGridStyling = mergeMaxGridStyling(innerGrid, validationGrid);
-  const {
-    size: inputGridSize,
-    exactPercentWidthStyle,
-    exactClassName,
-  } = inputGridSizeAndExactWidth(innerGrid, contentGridSize);
-  const {
-    size: validationGridSize,
-    exactPercentWidthStyle: validationExactPercentWidthStyle,
-    exactClassName: validationExactClassName,
-  } = inputGridSizeAndExactWidth(validationGrid, contentGridSize);
 
   const componentWithValidations = (
     <Flex
@@ -150,18 +87,14 @@ export function ComponentStructureWrapper({
     >
       <Flex
         item
-        className={exactClassName}
-        size={inputGridSize}
-        style={exactPercentWidthStyle}
+        size={{ xs: 12, ...innerGrid }}
       >
         {children}
       </Flex>
       {showValidationMessages && (
         <Flex
           item
-          className={validationExactClassName}
-          size={validationGridSize}
-          style={validationExactPercentWidthStyle}
+          size={{ xs: 12, ...validationGrid }}
         >
           <AllComponentValidations baseComponentId={baseComponentId} />
         </Flex>
