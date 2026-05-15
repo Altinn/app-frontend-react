172a15116c202c9cb76fa002940e0d2b68a87851
Philip Bruvoll <philip.johannes.bruvoll@digdir.no>
2026-05-15 13:07:13 +0200

    improve test

--- src/features/instantiate/containers/UnknownError.test.tsx
+++ src/features/instantiate/containers/UnknownError.test.tsx
@@ -11,6 +11,11 @@ import { renderWithMinimalProviders } from 'src/test/renderWithProviders';
 jest.unmock('axios');
 
 describe('Unknown error', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.restoreAllMocks();
+  });
+
   it('should be able to render with minimal providers', async () => {
     const user = userEvent.setup({ delay: null });
     jest.spyOn(console, 'error').mockImplementation(() => {});
@@ -29,5 +34,12 @@ describe('Unknown error', () => {
     const showDetailsButton = screen.getByRole('button', { name: 'Vis detaljer om feilen' });
     await user.click(showDetailsButton);
     expect(screen.getByText('Error test message')).toBeInTheDocument();
+
+    const writeTextMock = jest.spyOn(navigator.clipboard, 'writeText').mockResolvedValue();
+
+    const copyButton = screen.getByRole('button', { name: 'Kopier' });
+    await user.click(copyButton);
+    expect(writeTextMock).toHaveBeenCalledWith(expect.stringContaining('Error test message'));
+    expect(copyButton).toHaveAccessibleName('Kopiert');
   });
 });
