b3a73dacf8c53cad5f3298770243ed4fa21b01fd
Jamal Alabdullah <jamal.mustafa.alabdullah@digdir.no>
2026-06-16 10:22:12 +0200

    feedback fixing

--- src/layout/Text/TextComponent.tsx
+++ src/layout/Text/TextComponent.tsx
@@ -4,14 +4,13 @@ import cn from 'classnames';
 
 import { DisplayText } from 'src/app-components/Text/DisplayText';
 import classes from 'src/app-components/Text/Text.module.css';
-import { getLabelId } from 'src/components/label/Label';
 import { useLanguage } from 'src/features/language/useLanguage';
 import { ComponentStructureWrapper } from 'src/layout/ComponentStructureWrapper';
 import { useItemWhenType } from 'src/utils/layout/useNodeItem';
 import type { PropsFromGenericComponent } from 'src/layout';
 
 export const TextComponent = ({ baseComponentId }: PropsFromGenericComponent<'Text'>) => {
-  const { id, textResourceBindings, value, icon, direction: _direction } = useItemWhenType(baseComponentId, 'Text');
+  const { textResourceBindings, value, icon, direction: _direction } = useItemWhenType(baseComponentId, 'Text');
   const direction = _direction ?? 'horizontal';
   const { langAsString } = useLanguage();
 
@@ -32,16 +31,11 @@ export const TextComponent = ({ baseComponentId }: PropsFromGenericComponent<'Te
         ),
       }}
     >
-      <div
-        role='group'
-        aria-labelledby={getLabelId(id)}
-      >
-        <DisplayText
-          value={value}
-          iconUrl={icon}
-          iconAltText={langAsString(textResourceBindings.title)}
-        />
-      </div>
+      <DisplayText
+        value={value}
+        iconUrl={icon}
+        iconAltText={langAsString(textResourceBindings.title)}
+      />
     </ComponentStructureWrapper>
   );
 };
